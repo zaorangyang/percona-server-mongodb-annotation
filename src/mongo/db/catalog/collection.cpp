zaorangyang/percona-server-mongodb-annotation

commit 6d00a0f43290cbf255abb9af758c11ec95b745f1

SERVER-14668 Move getIndexSizeForCollection to be a method on Collection
Former-commit-id: e671a2c31f6b555254e4ad8265f66578f8c805aa

src/mongo/db/catalog/collection.cpp
@@ -441,6 +441,31 @@ namespace mongo {
         return _recordStore->dataSize( txn );
     }
 
+    uint64_t Collection::getIndexSize(OperationContext* opCtx,
+                                      BSONObjBuilder* details,
+                                      int scale) {
+
+        IndexCatalog* idxCatalog = getIndexCatalog();
+
+        IndexCatalog::IndexIterator ii = idxCatalog->getIndexIterator(opCtx, true);
+
+        long long totalSize = 0;
+
+        while (ii.more()) {
+            IndexDescriptor* d = ii.next();
+            IndexAccessMethod* iam = idxCatalog->getIndex(d);
+
+            long long ds = iam->getSpaceUsedBytes(opCtx);
+
+            totalSize += ds;
+            if (details) {
+                details->appendNumber(d->indexName(), ds / scale);
+            }
+        }
+
+        return totalSize;
+    }
+
     /**
      * order will be:
      * 1) store index specs

src/mongo/db/catalog/collection.h
@@ -257,6 +257,10 @@ namespace mongo {
             return static_cast<int>( dataSize( txn ) / n );
         }
 
+        uint64_t getIndexSize(OperationContext* opCtx,
+                              BSONObjBuilder* details = NULL,
+                              int scale = 1);
+
         // --- end suspect things
 
     private:

src/mongo/db/catalog/database.cpp
@@ -240,34 +240,6 @@ namespace mongo {
         return true;
     }
 
-    long long Database::getIndexSizeForCollection(OperationContext* opCtx,
-                                                  Collection* coll,
-                                                  BSONObjBuilder* details,
-                                                  int scale ) {
-        if ( !coll )
-            return 0;
-
-        IndexCatalog* idxCatalog = coll->getIndexCatalog();
-
-        IndexCatalog::IndexIterator ii = idxCatalog->getIndexIterator( opCtx, true );
-
-        long long totalSize = 0;
-
-        while ( ii.more() ) {
-            IndexDescriptor* d = ii.next();
-            IndexAccessMethod* iam = idxCatalog->getIndex( d );
-
-            long long ds = iam->getSpaceUsedBytes( opCtx );
-
-            totalSize += ds;
-            if ( details ) {
-                details->appendNumber( d->indexName(), ds / scale );
-            }
-        }
-
-        return totalSize;
-    }
-
     void Database::getStats( OperationContext* opCtx, BSONObjBuilder* output, double scale ) {
         list<string> collections;
         _dbEntry->getCollectionNamespaces( &collections );
@@ -296,7 +268,7 @@ namespace mongo {
             numExtents += temp.obj()["numExtents"].numberInt(); // XXX
 
             indexes += collection->getIndexCatalog()->numIndexesTotal( opCtx );
-            indexSize += getIndexSizeForCollection(opCtx, collection);
+            indexSize += collection->getIndexSize(opCtx);
         }
 
         output->append      ( "db" , _name );

src/mongo/db/catalog/database.h
@@ -91,11 +91,6 @@ namespace mongo {
 
         void getStats( OperationContext* opCtx, BSONObjBuilder* output, double scale = 1 );
 
-        long long getIndexSizeForCollection( OperationContext* opCtx,
-                                             Collection* collections,
-                                             BSONObjBuilder* details = NULL,
-                                             int scale = 1 );
-
         const DatabaseCatalogEntry* getDatabaseCatalogEntry() const;
 
         Status dropCollection( OperationContext* txn, const StringData& fullns );

src/mongo/db/dbcommands.cpp
@@ -951,10 +951,9 @@ namespace mongo {
             collection->getRecordStore()->appendCustomStats( txn, &result, scale );
 
             BSONObjBuilder indexSizes;
-            result.appendNumber( "totalIndexSize" , db->getIndexSizeForCollection(txn,
-                                                                                  collection,
-                                                                                  &indexSizes,
-                                                                                  scale) / scale );
+            result.appendNumber("totalIndexSize", collection->getIndexSize(txn,
+                                                                           &indexSizes,
+                                                                           scale) / scale);
             result.append("indexSizes", indexSizes.obj());
 
             return true;

commit 0792bd71c55e082f6a377ed838b630f4375200d1

SERVER-14668 Fix compile break on 32-bit and Clang
Introduced by commit 6d00a0f43290cbf255abb9af758c11ec95b745f1 [formerly e671a2c31f6b555254e4ad8265f66578f8c805aa]


Former-commit-id: bcea622441dc2f689c4651981f9d7929a1dd75b3

src/mongo/db/catalog/collection.cpp
@@ -441,7 +441,7 @@ namespace mongo {
         return _recordStore->dataSize( txn );
     }
 
-    uint64_t Collection::getIndexSize(OperationContext* opCtx,
+    size_t Collection::getIndexSize(OperationContext* opCtx,
                                       BSONObjBuilder* details,
                                       int scale) {
 

src/mongo/db/catalog/collection.h
@@ -257,7 +257,7 @@ namespace mongo {
             return static_cast<int>( dataSize( txn ) / n );
         }
 
-        uint64_t getIndexSize(OperationContext* opCtx,
+        size_t getIndexSize(OperationContext* opCtx,
                               BSONObjBuilder* details = NULL,
                               int scale = 1);
 

src/mongo/db/catalog/database.cpp
@@ -250,7 +250,7 @@ namespace mongo {
         long long storageSize = 0;
         long long numExtents = 0;
         long long indexes = 0;
-        long long indexSize = 0;
+        size_t indexSize = 0;
 
         for (list<string>::const_iterator it = collections.begin(); it != collections.end(); ++it) {
             const string ns = *it;