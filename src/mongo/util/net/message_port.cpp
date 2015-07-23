xiexiaoy/eloqdoc

SERVER-19389: Remove wire level endianness check

src/mongo/util/net/message_port.cpp
@@ -174,7 +174,9 @@ MessagingPort::~MessagingPort() {
 
 bool MessagingPort::recv(Message& m) {
     try {
+#ifdef MONGO_CONFIG_SSL
     again:
+#endif
         // mmm( log() << "*  recv() sock:" << this->sock << endl; )
         MSGHEADER::Value header;
         int headerLen = sizeof(MSGHEADER::Value);
@@ -193,13 +195,6 @@ bool MessagingPort::recv(Message& m) {
             string s = ss.str();
             send(s.c_str(), s.size(), "http");
             return false;
-        } else if (len == -1) {
-            // Endian check from the client, after connecting, to see what mode
-            // server is running in.
-            unsigned foo = 0x10203040;
-            send((char*)&foo, 4, "endian");
-            psock->setHandshakeReceived();
-            goto again;
         }
         // If responseTo is not 0 or -1 for first packet assume SSL
         else if (psock->isAwaitingHandshake()) {

zaorangyang/percona-server-mongodb-annotation

Former-commit-id: 0884ec0aa5465a82d30f0098702be2ae33784ff3

src/mongo/util/net/message_port.cpp
@@ -175,7 +175,9 @@ namespace mongo {
     
     bool MessagingPort::recv(Message& m) {
         try {
+#ifdef MONGO_SSL
 again:
+#endif
             //mmm( log() << "*  recv() sock:" << this->sock << endl; )
             MSGHEADER::Value header;
             int headerLen = sizeof(MSGHEADER::Value);
@@ -191,13 +193,6 @@ again:
                 string s = ss.str();
                 send( s.c_str(), s.size(), "http" );
                 return false;
-            }
-            else if ( len == -1 ) {
-                // Endian check from the client, after connecting, to see what mode server is running in.
-                unsigned foo = 0x10203040;
-                send( (char *) &foo, 4, "endian" );
-                psock->setHandshakeReceived();
-                goto again;
             }
             // If responseTo is not 0 or -1 for first packet assume SSL
             else if (psock->isAwaitingHandshake()) {