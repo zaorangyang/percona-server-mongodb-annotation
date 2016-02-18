xiexiaoy/eloqdoc

SERVER-22287 added replica set ID validation to heartbeat response processing, replSetReconfigure and replica set quorum check.

src/mongo/db/repl/replication_coordinator_impl_reconfig_test.cpp
@@ -157,6 +157,40 @@ TEST_F(ReplCoordTest, NodeReturnsInvalidReplicaSetConfigWhenReconfigReceivedWith
     ASSERT_TRUE(result.obj().isEmpty());
 }
 
+TEST_F(ReplCoordTest, NodeReturnsInvalidReplicaSetConfigWhenReconfigReceivedWithIncorrectSetId) {
+    // start up, become primary, receive config with incorrect replset name
+    OperationContextNoop txn;
+    assertStartSuccess(BSON("_id"
+                            << "mySet"
+                            << "version" << 2 << "members"
+                            << BSON_ARRAY(BSON("_id" << 1 << "host"
+                                                     << "node1:12345")
+                                          << BSON("_id" << 2 << "host"
+                                                        << "node2:12345")) << "settings"
+                            << BSON("replicaSetId" << OID::gen())),
+                       HostAndPort("node1", 12345));
+    ASSERT(getReplCoord()->setFollowerMode(MemberState::RS_SECONDARY));
+    getReplCoord()->setMyLastAppliedOpTime(OpTime(Timestamp(100, 0), 0));
+    getReplCoord()->setMyLastDurableOpTime(OpTime(Timestamp(100, 0), 0));
+    simulateSuccessfulV1Election();
+
+    BSONObjBuilder result;
+    ReplSetReconfigArgs args;
+    args.force = false;
+    args.newConfigObj = BSON("_id"
+                             << "mySet"
+                             << "version" << 3 << "members"
+                             << BSON_ARRAY(BSON("_id" << 1 << "host"
+                                                      << "node1:12345")
+                                           << BSON("_id" << 2 << "host"
+                                                         << "node2:12345")) << "settings"
+                             << BSON("replicaSetId" << OID::gen()));
+
+    ASSERT_EQUALS(ErrorCodes::NewReplicaSetConfigurationIncompatible,
+                  getReplCoord()->processReplSetReconfig(&txn, args, &result));
+    ASSERT_TRUE(result.obj().isEmpty());
+}
+
 TEST_F(ReplCoordTest,
        NodeReturnsNewReplicaSetConfigurationIncompatibleWhenANewConfigFailsToValidate) {
     // start up, become primary, validate fails
@@ -210,6 +244,7 @@ void doReplSetReconfig(ReplicationCoordinatorImpl* replCoord, Status* status) {
     BSONObjBuilder garbage;
     ReplSetReconfigArgs args;
     args.force = false;
+    // Replica set id will be copied from existing configuration.
     args.newConfigObj = BSON("_id"
                              << "mySet"
                              << "version" << 3 << "members"
@@ -377,7 +412,8 @@ TEST_F(ReplCoordTest, PrimaryNodeAcceptsNewConfigWhenReceivingAReconfigWithAComp
                             << BSON_ARRAY(BSON("_id" << 1 << "host"
                                                      << "node1:12345")
                                           << BSON("_id" << 2 << "host"
-                                                        << "node2:12345"))),
+                                                        << "node2:12345")) << "settings"
+                            << BSON("replicaSetId" << OID::gen())),
                        HostAndPort("node1", 12345));
     ASSERT(getReplCoord()->setFollowerMode(MemberState::RS_SECONDARY));
     getReplCoord()->setMyLastAppliedOpTime(OpTime(Timestamp(100, 0), 0));

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit db6af08f34febd0ae025d0db55ca9e8ebee8dfe6 [formerly 0bc96b1ede73c82d45b6d6c2e0b9a33e613ca7c3])
(cherry picked from commit a83a5f6e7db6b35346295cadfc05acd9eac1f7b8 [formerly 8633f9c7290e025182c1ea6a42d1c5c44224506f])
(cherry picked from commit db99358a22c045f53495131dfbc9028d07826b94 [formerly 3e96008ad946a5423806f823e8ee3744c49537be])
(cherry picked from commit 4c230b40b0a0938af2238d08662ed6cbd7b9bc44 [formerly 38cd69968a54a3a72b9c83f0bcdbc5acac03785a])
Former-commit-id: 8033364795d3b4453ddbbfedb5edd8a75f18c85b

src/mongo/db/repl/replication_coordinator_impl_reconfig_test.cpp
@@ -154,6 +154,39 @@ TEST_F(ReplCoordTest, NodeReturnsInvalidReplicaSetConfigWhenReconfigReceivedWith
     ASSERT_TRUE(result.obj().isEmpty());
 }
 
+TEST_F(ReplCoordTest, NodeReturnsInvalidReplicaSetConfigWhenReconfigReceivedWithIncorrectSetId) {
+    // start up, become primary, receive config with incorrect replset name
+    OperationContextNoop txn;
+    assertStartSuccess(BSON("_id"
+                            << "mySet"
+                            << "version" << 2 << "members"
+                            << BSON_ARRAY(BSON("_id" << 1 << "host"
+                                                     << "node1:12345")
+                                          << BSON("_id" << 2 << "host"
+                                                        << "node2:12345")) << "settings"
+                            << BSON("replicaSetId" << OID::gen())),
+                       HostAndPort("node1", 12345));
+    ASSERT(getReplCoord()->setFollowerMode(MemberState::RS_SECONDARY));
+    getReplCoord()->setMyLastOptime(OpTime(Timestamp(100, 0), 0));
+    simulateSuccessfulV1Election();
+
+    BSONObjBuilder result;
+    ReplSetReconfigArgs args;
+    args.force = false;
+    args.newConfigObj = BSON("_id"
+                             << "mySet"
+                             << "version" << 3 << "members"
+                             << BSON_ARRAY(BSON("_id" << 1 << "host"
+                                                      << "node1:12345")
+                                           << BSON("_id" << 2 << "host"
+                                                         << "node2:12345")) << "settings"
+                             << BSON("replicaSetId" << OID::gen()));
+
+    ASSERT_EQUALS(ErrorCodes::NewReplicaSetConfigurationIncompatible,
+                  getReplCoord()->processReplSetReconfig(&txn, args, &result));
+    ASSERT_TRUE(result.obj().isEmpty());
+}
+
 TEST_F(ReplCoordTest,
        NodeReturnsNewReplicaSetConfigurationIncompatibleWhenANewConfigFailsToValidate) {
     // start up, become primary, validate fails
@@ -206,6 +239,7 @@ void doReplSetReconfig(ReplicationCoordinatorImpl* replCoord, Status* status) {
     BSONObjBuilder garbage;
     ReplSetReconfigArgs args;
     args.force = false;
+    // Replica set id will be copied from existing configuration.
     args.newConfigObj = BSON("_id"
                              << "mySet"
                              << "version" << 3 << "members"
@@ -369,7 +403,8 @@ TEST_F(ReplCoordTest, PrimaryNodeAcceptsNewConfigWhenReceivingAReconfigWithAComp
                             << BSON_ARRAY(BSON("_id" << 1 << "host"
                                                      << "node1:12345")
                                           << BSON("_id" << 2 << "host"
-                                                        << "node2:12345"))),
+                                                        << "node2:12345")) << "settings"
+                            << BSON("replicaSetId" << OID::gen())),
                        HostAndPort("node1", 12345));
     ASSERT(getReplCoord()->setFollowerMode(MemberState::RS_SECONDARY));
     getReplCoord()->setMyLastOptime(OpTime(Timestamp(100, 0), 0));