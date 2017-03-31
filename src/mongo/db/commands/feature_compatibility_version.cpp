xiexiaoy/eloqdoc

SERVER-27834 FeatureCompatibilityVersion creates the admin.system.version collection if missing.
This avoids relying on the implicit collection behavior in IndexBuilder.

src/mongo/db/commands/feature_compatibility_version.cpp
@@ -220,6 +220,13 @@ void FeatureCompatibilityVersion::set(OperationContext* opCtx, StringData versio
                     repl::ReplicationCoordinator::get(opCtx->getServiceContext())
                         ->canAcceptWritesFor(opCtx, nss));
 
+            // If the "admin.system.version" collection has not been created yet, explicitly create
+            // it to hold the v=2 index.
+            if (!autoDB.getDb()->getCollection(nss)) {
+                uassertStatusOK(
+                    repl::StorageInterface::get(opCtx)->createCollection(opCtx, nss, {}));
+            }
+
             IndexBuilder builder(k32IncompatibleIndexSpec, false);
             auto status = builder.buildInForeground(opCtx, autoDB.getDb());
             uassertStatusOK(status);
@@ -303,6 +310,12 @@ void FeatureCompatibilityVersion::setIfCleanStartup(OperationContext* opCtx,
 
         {
             AutoGetOrCreateDb autoDB(opCtx, nss.db(), MODE_X);
+
+            // We reached this point because the only database that exists on the server is "local"
+            // and we have just created an empty "admin" database.
+            // Therefore, it is safe to create the "admin.system.version" collection.
+            invariant(autoDB.justCreated());
+            uassertStatusOK(storageInterface->createCollection(opCtx, nss, {}));
 
             IndexBuilder builder(k32IncompatibleIndexSpec, false);
             auto status = builder.buildInForeground(opCtx, autoDB.getDb());

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit 3029714d2c75692cf23db2e89a9b397eb1a10527 [formerly 9bc30836f60a888c44de09c55a45c278b215a02b])
Former-commit-id: 5eed47fa931d4093c6c3b5e60ef3d01ab25cf3cb

src/mongo/db/commands/feature_compatibility_version.cpp
@@ -221,6 +221,12 @@ void FeatureCompatibilityVersion::set(OperationContext* txn, StringData version)
                     repl::ReplicationCoordinator::get(txn->getServiceContext())
                         ->canAcceptWritesFor(nss));
 
+            // If the "admin.system.version" collection has not been created yet, explicitly create
+            // it to hold the v=2 index.
+            if (!autoDB.getDb()->getCollection(nss)) {
+                uassertStatusOK(repl::StorageInterface::get(txn)->createCollection(txn, nss, {}));
+            }
+
             IndexBuilder builder(k32IncompatibleIndexSpec, false);
             auto status = builder.buildInForeground(txn, autoDB.getDb());
             uassertStatusOK(status);
@@ -304,6 +310,12 @@ void FeatureCompatibilityVersion::setIfCleanStartup(OperationContext* txn,
         {
             ScopedTransaction transaction(txn, MODE_IX);
             AutoGetOrCreateDb autoDB(txn, nss.db(), MODE_X);
+
+            // We reached this point because the only database that exists on the server is "local"
+            // and we have just created an empty "admin" database.
+            // Therefore, it is safe to create the "admin.system.version" collection.
+            invariant(autoDB.justCreated());
+            uassertStatusOK(storageInterface->createCollection(txn, nss, {}));
 
             IndexBuilder builder(k32IncompatibleIndexSpec, false);
             auto status = builder.buildInForeground(txn, autoDB.getDb());