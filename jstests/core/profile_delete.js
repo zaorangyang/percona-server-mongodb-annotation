tessavitabile/mongo

SERVER-28575 Profile entry for update/delete should contain entire raw update/delete

jstests/core/profile_delete.js
@@ -31,7 +31,7 @@
     assert.eq(profileObj.ns, coll.getFullName(), tojson(profileObj));
     assert.eq(profileObj.op, "remove", tojson(profileObj));
     if (db.getMongo().writeMode() === "commands") {
-        assert.eq(profileObj.collation, {locale: "fr"}, tojson(profileObj));
+        assert.eq(profileObj.query.collation, {locale: "fr"}, tojson(profileObj));
     }
     assert.eq(profileObj.ndeleted, 1, tojson(profileObj));
     assert.eq(profileObj.keysExamined, 1, tojson(profileObj));

jstests/core/profile_update.js
@@ -21,17 +21,19 @@
     }
     assert.commandWorked(coll.createIndex({a: 1}));
 
-    assert.writeOK(
-        coll.update({a: {$gte: 2}},
-                    {$set: {c: 1}, $inc: {a: -10}},
-                    db.getMongo().writeMode() === "commands" ? {collation: {locale: "fr"}} : {}));
+    assert.writeOK(coll.update({a: {$gte: 2}},
+                               {$set: {c: 1}, $inc: {a: -10}},
+                               db.getMongo().writeMode() === "commands"
+                                   ? {collation: {locale: "fr"}, arrayFilters: [{i: 0}]}
+                                   : {}));
 
     var profileObj = getLatestProfilerEntry(testDB);
 
     assert.eq(profileObj.ns, coll.getFullName(), tojson(profileObj));
     assert.eq(profileObj.op, "update", tojson(profileObj));
     if (db.getMongo().writeMode() === "commands") {
-        assert.eq(profileObj.collation, {locale: "fr"}, tojson(profileObj));
+        assert.eq(profileObj.query.collation, {locale: "fr"}, tojson(profileObj));
+        assert.eq(profileObj.query.arrayFilters, [{i: 0}], tojson(profileObj));
     }
     assert.eq(profileObj.keysExamined, 1, tojson(profileObj));
     assert.eq(profileObj.docsExamined, 1, tojson(profileObj));
@@ -80,8 +82,9 @@
     assert.writeOK(coll.update({_id: "new value", a: 4}, {$inc: {b: 1}}, {upsert: true}));
     profileObj = getLatestProfilerEntry(testDB);
 
-    assert.eq(profileObj.query, {_id: "new value", a: 4}, tojson(profileObj));
-    assert.eq(profileObj.updateobj, {$inc: {b: 1}}, tojson(profileObj));
+    assert.eq(profileObj.query,
+              {q: {_id: "new value", a: 4}, u: {$inc: {b: 1}}, multi: false, upsert: true},
+              tojson(profileObj));
     assert.eq(profileObj.keysExamined, 0, tojson(profileObj));
     assert.eq(profileObj.docsExamined, 0, tojson(profileObj));
     assert.eq(profileObj.keysInserted, 2, tojson(profileObj));

zaorangyang/percona-server-mongodb-annotation

Former-commit-id: f56ae945275aa9360d2d6eae192b64d558ede20b

jstests/core/profile_delete.js
@@ -31,7 +31,7 @@
     assert.eq(profileObj.ns, coll.getFullName(), tojson(profileObj));
     assert.eq(profileObj.op, "remove", tojson(profileObj));
     if (db.getMongo().writeMode() === "commands") {
-        assert.eq(profileObj.collation, {locale: "fr"}, tojson(profileObj));
+        assert.eq(profileObj.command.collation, {locale: "fr"}, tojson(profileObj));
     }
     assert.eq(profileObj.ndeleted, 1, tojson(profileObj));
     assert.eq(profileObj.keysExamined, 1, tojson(profileObj));

jstests/core/profile_update.js
@@ -21,17 +21,19 @@
     }
     assert.commandWorked(coll.createIndex({a: 1}));
 
-    assert.writeOK(
-        coll.update({a: {$gte: 2}},
-                    {$set: {c: 1}, $inc: {a: -10}},
-                    db.getMongo().writeMode() === "commands" ? {collation: {locale: "fr"}} : {}));
+    assert.writeOK(coll.update({a: {$gte: 2}},
+                               {$set: {c: 1}, $inc: {a: -10}},
+                               db.getMongo().writeMode() === "commands"
+                                   ? {collation: {locale: "fr"}, arrayFilters: [{i: 0}]}
+                                   : {}));
 
     var profileObj = getLatestProfilerEntry(testDB);
 
     assert.eq(profileObj.ns, coll.getFullName(), tojson(profileObj));
     assert.eq(profileObj.op, "update", tojson(profileObj));
     if (db.getMongo().writeMode() === "commands") {
-        assert.eq(profileObj.collation, {locale: "fr"}, tojson(profileObj));
+        assert.eq(profileObj.command.collation, {locale: "fr"}, tojson(profileObj));
+        assert.eq(profileObj.command.arrayFilters, [{i: 0}], tojson(profileObj));
     }
     assert.eq(profileObj.keysExamined, 1, tojson(profileObj));
     assert.eq(profileObj.docsExamined, 1, tojson(profileObj));
@@ -80,8 +82,9 @@
     assert.writeOK(coll.update({_id: "new value", a: 4}, {$inc: {b: 1}}, {upsert: true}));
     profileObj = getLatestProfilerEntry(testDB);
 
-    assert.eq(profileObj.query, {_id: "new value", a: 4}, tojson(profileObj));
-    assert.eq(profileObj.updateobj, {$inc: {b: 1}}, tojson(profileObj));
+    assert.eq(profileObj.command,
+              {q: {_id: "new value", a: 4}, u: {$inc: {b: 1}}, multi: false, upsert: true},
+              tojson(profileObj));
     assert.eq(profileObj.keysExamined, 0, tojson(profileObj));
     assert.eq(profileObj.docsExamined, 0, tojson(profileObj));
     assert.eq(profileObj.keysInserted, 2, tojson(profileObj));