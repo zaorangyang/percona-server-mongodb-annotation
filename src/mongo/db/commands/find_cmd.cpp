xiexiaoy/eloqdoc

SERVER-29327: Prevent DBDirectClient from storing MaxTimeMS on cursors

src/mongo/db/commands/find_cmd.cpp
@@ -401,7 +401,12 @@ public:
             cursorExec->saveState();
             cursorExec->detachFromOperationContext();
 
-            pinnedCursor.getCursor()->setLeftoverMaxTimeMicros(opCtx->getRemainingMaxTimeMicros());
+            // We assume that cursors created through a DBDirectClient are always used from their
+            // original OperationContext, so we do not need to move time to and from the cursor.
+            if (!opCtx->getClient()->isInDirectClient()) {
+                pinnedCursor.getCursor()->setLeftoverMaxTimeMicros(
+                    opCtx->getRemainingMaxTimeMicros());
+            }
             pinnedCursor.getCursor()->setPos(numResults);
 
             // Fill out curop based on the results.

src/mongo/db/commands/getmore_cmd.cpp
@@ -284,20 +284,16 @@ public:
 
         const bool hasOwnMaxTime = opCtx->hasDeadline();
 
-        if (!hasOwnMaxTime) {
+        // We assume that cursors created through a DBDirectClient are always used from their
+        // original OperationContext, so we do not need to move time to and from the cursor.
+        if (!hasOwnMaxTime && !opCtx->getClient()->isInDirectClient()) {
             // There is no time limit set directly on this getMore command. If the cursor is
             // awaitData, then we supply a default time of one second. Otherwise we roll over
             // any leftover time from the maxTimeMS of the operation that spawned this cursor,
             // applying it to this getMore.
             if (isCursorAwaitData(cursor)) {
-                uassert(40117,
-                        "Illegal attempt to set operation deadline within DBDirectClient",
-                        !opCtx->getClient()->isInDirectClient());
                 opCtx->setDeadlineAfterNowBy(Seconds{1});
             } else if (cursor->getLeftoverMaxTimeMicros() < Microseconds::max()) {
-                uassert(40118,
-                        "Illegal attempt to set operation deadline within DBDirectClient",
-                        !opCtx->getClient()->isInDirectClient());
                 opCtx->setDeadlineAfterNowBy(cursor->getLeftoverMaxTimeMicros());
             }
         }

src/mongo/db/query/find.cpp
@@ -481,9 +481,13 @@ Message getMore(OperationContext* opCtx,
 
             *exhaust = cc->queryOptions() & QueryOption_Exhaust;
 
-            // If the getmore had a time limit, remaining time is "rolled over" back to the
-            // cursor (for use by future getmore ops).
-            cc->setLeftoverMaxTimeMicros(opCtx->getRemainingMaxTimeMicros());
+            // We assume that cursors created through a DBDirectClient are always used from their
+            // original OperationContext, so we do not need to move time to and from the cursor.
+            if (!opCtx->getClient()->isInDirectClient()) {
+                // If the getmore had a time limit, remaining time is "rolled over" back to the
+                // cursor (for use by future getmore ops).
+                cc->setLeftoverMaxTimeMicros(opCtx->getRemainingMaxTimeMicros());
+            }
         }
     }
 
@@ -684,9 +688,13 @@ std::string runQuery(OperationContext* opCtx,
 
         pinnedCursor.getCursor()->setPos(numResults);
 
-        // If the query had a time limit, remaining time is "rolled over" to the cursor (for
-        // use by future getmore ops).
-        pinnedCursor.getCursor()->setLeftoverMaxTimeMicros(opCtx->getRemainingMaxTimeMicros());
+        // We assume that cursors created through a DBDirectClient are always used from their
+        // original OperationContext, so we do not need to move time to and from the cursor.
+        if (!opCtx->getClient()->isInDirectClient()) {
+            // If the query had a time limit, remaining time is "rolled over" to the cursor (for
+            // use by future getmore ops).
+            pinnedCursor.getCursor()->setLeftoverMaxTimeMicros(opCtx->getRemainingMaxTimeMicros());
+        }
 
         endQueryOp(opCtx, collection, *pinnedCursor.getCursor()->getExecutor(), numResults, ccId);
     } else {

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit 426f12a43e02bdfc5159f19241098bd65bb4c37f [formerly 7d5b5cd998df4a8532748c2a59fce06c305122d8])
Former-commit-id: 57069e87a5237c9026e985b01f8fef504de3f62f

src/mongo/db/commands/find_cmd.cpp
@@ -408,7 +408,11 @@ public:
             cursorExec->saveState();
             cursorExec->detachFromOperationContext();
 
-            cursor->setLeftoverMaxTimeMicros(txn->getRemainingMaxTimeMicros());
+            // We assume that cursors created through a DBDirectClient are always used from their
+            // original OperationContext, so we do not need to move time to and from the cursor.
+            if (!txn->getClient()->isInDirectClient()) {
+                cursor->setLeftoverMaxTimeMicros(txn->getRemainingMaxTimeMicros());
+            }
             cursor->setPos(numResults);
 
             // Fill out curop based on the results.

src/mongo/db/commands/getmore_cmd.cpp
@@ -301,20 +301,16 @@ public:
 
         const bool hasOwnMaxTime = txn->hasDeadline();
 
-        if (!hasOwnMaxTime) {
+        // We assume that cursors created through a DBDirectClient are always used from their
+        // original OperationContext, so we do not need to move time to and from the cursor.
+        if (!hasOwnMaxTime && !txn->getClient()->isInDirectClient()) {
             // There is no time limit set directly on this getMore command. If the cursor is
             // awaitData, then we supply a default time of one second. Otherwise we roll over
             // any leftover time from the maxTimeMS of the operation that spawned this cursor,
             // applying it to this getMore.
             if (isCursorAwaitData(cursor)) {
-                uassert(40117,
-                        "Illegal attempt to set operation deadline within DBDirectClient",
-                        !txn->getClient()->isInDirectClient());
                 txn->setDeadlineAfterNowBy(Seconds{1});
             } else if (cursor->getLeftoverMaxTimeMicros() < Microseconds::max()) {
-                uassert(40118,
-                        "Illegal attempt to set operation deadline within DBDirectClient",
-                        !txn->getClient()->isInDirectClient());
                 txn->setDeadlineAfterNowBy(cursor->getLeftoverMaxTimeMicros());
             }
         }

src/mongo/db/query/find.cpp
@@ -478,9 +478,13 @@ Message getMore(OperationContext* txn,
 
             *exhaust = cc->queryOptions() & QueryOption_Exhaust;
 
-            // If the getmore had a time limit, remaining time is "rolled over" back to the
-            // cursor (for use by future getmore ops).
-            cc->setLeftoverMaxTimeMicros(txn->getRemainingMaxTimeMicros());
+            // We assume that cursors created through a DBDirectClient are always used from their
+            // original OperationContext, so we do not need to move time to and from the cursor.
+            if (!txn->getClient()->isInDirectClient()) {
+                // If the getmore had a time limit, remaining time is "rolled over" back to the
+                // cursor (for use by future getmore ops).
+                cc->setLeftoverMaxTimeMicros(txn->getRemainingMaxTimeMicros());
+            }
         }
     }
 
@@ -689,9 +693,13 @@ std::string runQuery(OperationContext* txn,
 
         cc->setPos(numResults);
 
-        // If the query had a time limit, remaining time is "rolled over" to the cursor (for
-        // use by future getmore ops).
-        cc->setLeftoverMaxTimeMicros(txn->getRemainingMaxTimeMicros());
+        // We assume that cursors created through a DBDirectClient are always used from their
+        // original OperationContext, so we do not need to move time to and from the cursor.
+        if (!txn->getClient()->isInDirectClient()) {
+            // If the query had a time limit, remaining time is "rolled over" to the cursor (for
+            // use by future getmore ops).
+            cc->setLeftoverMaxTimeMicros(txn->getRemainingMaxTimeMicros());
+        }
 
         endQueryOp(txn, collection, *cc->getExecutor(), numResults, ccId);
     } else {