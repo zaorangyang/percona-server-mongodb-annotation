xiexiaoy/eloqdoc

SERVER-22512 Remove unnecessary calls to stopBalancer

jstests/aggregation/bugs/server6118.js
@@ -1,12 +1,12 @@
 // SERVER-6118: support for sharded sorts
 (function() {
+    'use strict';
 
-    var s = new ShardingTest({name: "aggregation_sort1", shards: 2, mongos: 1});
-    s.stopBalancer();
+    var s = new ShardingTest({shards: 2});
 
-    s.adminCommand({enablesharding: "test"});
+    assert.commandWorked(s.s0.adminCommand({enablesharding: "test"}));
     s.ensurePrimaryShard('test', 'shard0001');
-    s.adminCommand({shardcollection: "test.data", key: {_id: 1}});
+    assert.commandWorked(s.s0.adminCommand({shardcollection: "test.data", key: {_id: 1}}));
 
     var d = s.getDB("test");
 
@@ -20,12 +20,12 @@
     bulkOp.execute();
 
     // Split the data into 3 chunks
-    s.adminCommand({split: "test.data", middle: {_id: 33}});
-    s.adminCommand({split: "test.data", middle: {_id: 66}});
+    assert.commandWorked(s.s0.adminCommand({split: "test.data", middle: {_id: 33}}));
+    assert.commandWorked(s.s0.adminCommand({split: "test.data", middle: {_id: 66}}));
 
     // Migrate the middle chunk to another shard
-    s.adminCommand(
-        {movechunk: "test.data", find: {_id: 50}, to: s.getOther(s.getPrimaryShard("test")).name});
+    assert.commandWorked(s.s0.adminCommand(
+        {movechunk: "test.data", find: {_id: 50}, to: s.getOther(s.getPrimaryShard("test")).name}));
 
     // Check that the results are in order.
     var result = d.data.aggregate({$sort: {_id: 1}}).toArray();
@@ -36,5 +36,4 @@
     }
 
     s.stop();
-
 })();

jstests/aggregation/bugs/server6179.js
@@ -1,12 +1,12 @@
 // SERVER-6179: support for two $groups in sharded agg
 (function() {
+    'use strict';
 
-    var s = new ShardingTest({name: "aggregation_multiple_group", shards: 2, mongos: 1});
-    s.stopBalancer();
+    var s = new ShardingTest({shards: 2});
 
-    s.adminCommand({enablesharding: "test"});
+    assert.commandWorked(s.s0.adminCommand({enablesharding: "test"}));
     s.ensurePrimaryShard('test', 'shard0001');
-    s.adminCommand({shardcollection: "test.data", key: {_id: 1}});
+    assert.commandWorked(s.s0.adminCommand({shardcollection: "test.data", key: {_id: 1}}));
 
     var d = s.getDB("test");
 
@@ -20,12 +20,12 @@
     bulkOp.execute();
 
     // Split the data into 3 chunks
-    s.adminCommand({split: "test.data", middle: {_id: 33}});
-    s.adminCommand({split: "test.data", middle: {_id: 66}});
+    assert.commandWorked(s.s0.adminCommand({split: "test.data", middle: {_id: 33}}));
+    assert.commandWorked(s.s0.adminCommand({split: "test.data", middle: {_id: 66}}));
 
     // Migrate the middle chunk to another shard
-    s.adminCommand(
-        {movechunk: "test.data", find: {_id: 50}, to: s.getOther(s.getPrimaryShard("test")).name});
+    assert.commandWorked(s.s0.adminCommand(
+        {movechunk: "test.data", find: {_id: 50}, to: s.getOther(s.getPrimaryShard("test")).name}));
 
     // Check that we get results rather than an error
     var result = d.data
@@ -33,7 +33,7 @@
                                 {$group: {_id: '$i', avg_id: {$avg: '$_id'}}},
                                 {$sort: {_id: 1}})
                      .toArray();
-    expected = [
+    var expected = [
         {"_id": 0, "avg_id": 45},
         {"_id": 1, "avg_id": 46},
         {"_id": 2, "avg_id": 47},
@@ -49,5 +49,4 @@
     assert.eq(result, expected);
 
     s.stop();
-
 })();

zaorangyang/percona-server-mongodb-annotation

Modified some to work in v3.2 -- e.g. removed getShardPrimary which doesn't exist in v3.2
(cherry picked from commit d5b904522232df40404b7ff77de3be6b3cb15b96 [formerly 2477b8c33b2e8f26fcde47c38c19c3fbb8b99839])
Former-commit-id: 41798bb7ecfa6d5d491f02f9dba68681da486355

jstests/aggregation/bugs/server6118.js
@@ -1,12 +1,12 @@
 // SERVER-6118: support for sharded sorts
 (function() {
+    'use strict';
 
-    var s = new ShardingTest({name: "aggregation_sort1", shards: 2, mongos: 1});
-    s.stopBalancer();
+    var s = new ShardingTest({shards: 2});
 
-    s.adminCommand({enablesharding: "test"});
+    assert.commandWorked(s.s0.adminCommand({enablesharding: "test"}));
     s.ensurePrimaryShard('test', 'shard0001');
-    s.adminCommand({shardcollection: "test.data", key: {_id: 1}});
+    assert.commandWorked(s.s0.adminCommand({shardcollection: "test.data", key: {_id: 1}}));
 
     var d = s.getDB("test");
 
@@ -20,12 +20,12 @@
     bulkOp.execute();
 
     // Split the data into 3 chunks
-    s.adminCommand({split: "test.data", middle: {_id: 33}});
-    s.adminCommand({split: "test.data", middle: {_id: 66}});
+    assert.commandWorked(s.s0.adminCommand({split: "test.data", middle: {_id: 33}}));
+    assert.commandWorked(s.s0.adminCommand({split: "test.data", middle: {_id: 66}}));
 
     // Migrate the middle chunk to another shard
-    s.adminCommand(
-        {movechunk: "test.data", find: {_id: 50}, to: s.getOther(s.getServer("test")).name});
+    assert.commandWorked(s.s0.adminCommand(
+        {movechunk: "test.data", find: {_id: 50}, to: s.getOther(s.getServer("test")).name}));
 
     // Check that the results are in order.
     var result = d.data.aggregate({$sort: {_id: 1}}).toArray();
@@ -36,5 +36,4 @@
     }
 
     s.stop();
-
 })();

jstests/aggregation/bugs/server6179.js
@@ -1,12 +1,12 @@
 // SERVER-6179: support for two $groups in sharded agg
 (function() {
+    'use strict';
 
-    var s = new ShardingTest({name: "aggregation_multiple_group", shards: 2, mongos: 1});
-    s.stopBalancer();
+    var s = new ShardingTest({shards: 2});
 
-    s.adminCommand({enablesharding: "test"});
+    assert.commandWorked(s.s0.adminCommand({enablesharding: "test"}));
     s.ensurePrimaryShard('test', 'shard0001');
-    s.adminCommand({shardcollection: "test.data", key: {_id: 1}});
+    assert.commandWorked(s.s0.adminCommand({shardcollection: "test.data", key: {_id: 1}}));
 
     var d = s.getDB("test");
 
@@ -20,18 +20,18 @@
     bulkOp.execute();
 
     // Split the data into 3 chunks
-    s.adminCommand({split: "test.data", middle: {_id: 33}});
-    s.adminCommand({split: "test.data", middle: {_id: 66}});
+    assert.commandWorked(s.s0.adminCommand({split: "test.data", middle: {_id: 33}}));
+    assert.commandWorked(s.s0.adminCommand({split: "test.data", middle: {_id: 66}}));
 
     // Migrate the middle chunk to another shard
-    s.adminCommand(
-        {movechunk: "test.data", find: {_id: 50}, to: s.getOther(s.getServer("test")).name});
+    assert.commandWorked(s.s0.adminCommand(
+        {movechunk: "test.data", find: {_id: 50}, to: s.getOther(s.getServer("test")).name}));
 
     // Check that we get results rather than an error
     var result = d.data.aggregate({$group: {_id: '$_id', i: {$first: '$i'}}},
                                   {$group: {_id: '$i', avg_id: {$avg: '$_id'}}},
                                   {$sort: {_id: 1}}).toArray();
-    expected = [
+    var expected = [
         {"_id": 0, "avg_id": 45},
         {"_id": 1, "avg_id": 46},
         {"_id": 2, "avg_id": 47},
@@ -47,5 +47,4 @@
     assert.eq(result, expected);
 
     s.stop();
-
 })();