xiexiaoy/eloqdoc

SERVER-30406 Test view catalog invalidation due to renaming to/from system.views

src/mongo/db/op_observer_impl.cpp
@@ -447,11 +447,10 @@ void OpObserverImpl::onRenameCollection(OperationContext* opCtx,
     BSONObj cmdObj = builder.done();
 
     repl::logOp(opCtx, "c", cmdNss, uuid, cmdObj, nullptr, false, kUninitializedStmtId);
-    if (fromCollection.coll() == DurableViewCatalog::viewsCollectionName() ||
-        toCollection.coll() == DurableViewCatalog::viewsCollectionName()) {
-        DurableViewCatalog::onExternalChange(
-            opCtx, NamespaceString(DurableViewCatalog::viewsCollectionName()));
-    }
+    if (fromCollection.isSystemDotViews())
+        DurableViewCatalog::onExternalChange(opCtx, fromCollection);
+    if (toCollection.isSystemDotViews())
+        DurableViewCatalog::onExternalChange(opCtx, toCollection);
 
     getGlobalAuthorizationManager()->logOp(opCtx, "c", cmdNss, cmdObj, nullptr);
 

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit c8eaa8f3139b89a66c70c901d8c4d81cce0ee82a [formerly e3bdbd02624f5dce42aada11b84f28ff14f578da])
Conflicts:
        src/mongo/db/op_observer_impl.cpp
Former-commit-id: 8d99028a306bfb1fe5a7734e2f1926c06e2defbe

src/mongo/db/op_observer_impl.cpp
@@ -276,11 +276,10 @@ void OpObserverImpl::onRenameCollection(OperationContext* txn,
                                 << dropTarget);
 
     repl::logOp(txn, "c", dbName.c_str(), cmdObj, nullptr, false);
-    if (fromCollection.coll() == DurableViewCatalog::viewsCollectionName() ||
-        toCollection.coll() == DurableViewCatalog::viewsCollectionName()) {
-        DurableViewCatalog::onExternalChange(
-            txn, NamespaceString(DurableViewCatalog::viewsCollectionName()));
-    }
+    if (fromCollection.isSystemDotViews())
+        DurableViewCatalog::onExternalChange(txn, fromCollection);
+    if (toCollection.isSystemDotViews())
+        DurableViewCatalog::onExternalChange(txn, toCollection);
 
     getGlobalAuthorizationManager()->logOp(txn, "c", dbName.c_str(), cmdObj, nullptr);
     logOpForDbHash(txn, dbName.c_str());