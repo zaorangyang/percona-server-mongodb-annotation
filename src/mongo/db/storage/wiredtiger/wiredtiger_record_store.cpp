xiexiaoy/eloqdoc

SERVER-31590 Release unused cursors in the cache before awaiting oplog deletion

src/mongo/db/storage/wiredtiger/wiredtiger_record_store.cpp
@@ -1034,7 +1034,9 @@ bool WiredTigerRecordStore::yieldAndAwaitOplogDeletionRequest(OperationContext* 
 
     // The top-level locks were freed, so also release any potential low-level (storage engine)
     // locks that might be held.
-    opCtx->recoveryUnit()->abandonSnapshot();
+    WiredTigerRecoveryUnit* recoveryUnit = (WiredTigerRecoveryUnit*)opCtx->recoveryUnit();
+    recoveryUnit->abandonSnapshot();
+    recoveryUnit->beginIdle();
 
     // Wait for an oplog deletion request, or for this record store to have been destroyed.
     oplogStones->awaitHasExcessStonesOrDead();

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit 90b63d5166e609c7f290f1d93e01183b99ee03fb [formerly 2a8818d4fd7b833b82997c9996aca9eb14471f09])
Former-commit-id: 657bd4c2285367278f43992a33942268ebf6919e

src/mongo/db/storage/wiredtiger/wiredtiger_record_store.cpp
@@ -1192,7 +1192,9 @@ bool WiredTigerRecordStore::yieldAndAwaitOplogDeletionRequest(OperationContext* 
 
     // The top-level locks were freed, so also release any potential low-level (storage engine)
     // locks that might be held.
-    txn->recoveryUnit()->abandonSnapshot();
+    WiredTigerRecoveryUnit* recoveryUnit = (WiredTigerRecoveryUnit*)txn->recoveryUnit();
+    recoveryUnit->abandonSnapshot();
+    recoveryUnit->beginIdle();
 
     // Wait for an oplog deletion request, or for this record store to have been destroyed.
     oplogStones->awaitHasExcessStonesOrDead();