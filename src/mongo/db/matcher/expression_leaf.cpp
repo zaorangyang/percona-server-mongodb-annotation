tessavitabile/mongo

SERVER-30189 Reduce calls to allocator for large $in expressions

src/mongo/db/matcher/expression_leaf.cpp
@@ -595,27 +595,33 @@ void InMatchExpression::_doSetCollator(const CollatorInterface* collator) {
     _collator = collator;
 
     // We need to re-compute '_equalitySet', since our set comparator has changed.
-    BSONElementSet equalitiesWithNewComparator(
+    BSONElementFlatSet equalitiesWithNewComparator(
         _originalEqualityVector.begin(), _originalEqualityVector.end(), collator);
     _equalitySet = std::move(equalitiesWithNewComparator);
 }
 
-Status InMatchExpression::addEquality(const BSONElement& elt) {
-    if (elt.type() == BSONType::RegEx) {
-        return Status(ErrorCodes::BadValue, "InMatchExpression equality cannot be a regex");
-    }
-    if (elt.type() == BSONType::Undefined) {
-        return Status(ErrorCodes::BadValue, "InMatchExpression equality cannot be undefined");
-    }
+Status InMatchExpression::setEqualities(std::vector<BSONElement> equalities) {
+    for (auto&& equality : equalities) {
+        if (equality.type() == BSONType::RegEx) {
+            return Status(ErrorCodes::BadValue, "InMatchExpression equality cannot be a regex");
+        }
+        if (equality.type() == BSONType::Undefined) {
+            return Status(ErrorCodes::BadValue, "InMatchExpression equality cannot be undefined");
+        }
 
-    if (elt.type() == BSONType::jstNULL) {
-        _hasNull = true;
+        if (equality.type() == BSONType::jstNULL) {
+            _hasNull = true;
+        }
+        if (equality.type() == BSONType::Array && equality.Obj().isEmpty()) {
+            _hasEmptyArray = true;
+        }
     }
-    if (elt.type() == BSONType::Array && elt.Obj().isEmpty()) {
-        _hasEmptyArray = true;
-    }
-    _equalitySet.insert(elt);
-    _originalEqualityVector.push_back(elt);
+    _originalEqualityVector = std::move(equalities);
+
+    BSONElementFlatSet equalitySet(
+        _originalEqualityVector.begin(), _originalEqualityVector.end(), _collator);
+    _equalitySet = std::move(equalitySet);
+
     return Status::OK();
 }
 

src/mongo/db/matcher/expression_leaf.h
@@ -28,6 +28,7 @@
 
 #pragma once
 
+#include "mongo/bson/bsonelement_comparator_interface.h"
 #include "mongo/bson/bsonmisc.h"
 #include "mongo/bson/bsonobj.h"
 #include "mongo/db/matcher/expression.h"
@@ -317,11 +318,11 @@ public:
      */
     virtual void _doSetCollator(const CollatorInterface* collator);
 
-    Status addEquality(const BSONElement& elt);
+    Status setEqualities(std::vector<BSONElement> equalities);
 
     Status addRegex(std::unique_ptr<RegexMatchExpression> expr);
 
-    const BSONElementSet& getEqualities() const {
+    const BSONElementFlatSet& getEqualities() const {
         return _equalitySet;
     }
 
@@ -353,7 +354,7 @@ private:
 
     // Set of equality elements associated with this expression. '_collator' is used as a comparator
     // for this set.
-    BSONElementSet _equalitySet;
+    BSONElementFlatSet _equalitySet;
 
     // Original container of equality elements, including duplicates. Needed for re-computing
     // '_equalitySet' in case '_collator' changes after elements have been added.

src/mongo/db/matcher/expression_parser.cpp
@@ -723,6 +723,7 @@ Status MatchExpressionParser::_parseInExpression(InMatchExpression* inExpression
                                                  const BSONObj& theArray,
                                                  const CollatorInterface* collator) {
     inExpression->setCollator(collator);
+    std::vector<BSONElement> equalities;
     BSONObjIterator i(theArray);
     while (i.more()) {
         BSONElement e = i.next();
@@ -741,12 +742,10 @@ Status MatchExpressionParser::_parseInExpression(InMatchExpression* inExpression
             if (!s.isOK())
                 return s;
         } else {
-            Status s = inExpression->addEquality(e);
-            if (!s.isOK())
-                return s;
+            equalities.push_back(e);
         }
     }
-    return Status::OK();
+    return inExpression->setEqualities(std::move(equalities));
 }
 
 StatusWith<std::unique_ptr<TypeMatchExpression>> MatchExpressionParser::parseTypeFromAlias(

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit ce805323a736fb6adf90bb8ff41ffe4c70aac690 [formerly 50a1afafc816097ed57804ff7033dffd85dbe160])
(cherry picked from commit 725e09dfc2387b6412b573ad238253b1d331e9e4 [formerly ef408a20e6fd71ffc97f3602dd65ef5b03de6a45])
Former-commit-id: d131ca7d6a2b0442620346561d27c6b00f832bf0

src/mongo/db/matcher/expression_leaf.cpp
@@ -534,22 +534,24 @@ ArrayFilterEntries::~ArrayFilterEntries() {
     _regexes.clear();
 }
 
-Status ArrayFilterEntries::addEquality(const BSONElement& e) {
-    if (e.type() == RegEx)
-        return Status(ErrorCodes::BadValue, "ArrayFilterEntries equality cannot be a regex");
+Status ArrayFilterEntries::setEqualities(std::vector<BSONElement> equalities) {
+    for (auto&& equality : equalities) {
+        if (equality.type() == BSONType::RegEx) {
+            return Status(ErrorCodes::BadValue, "ArrayFilterEntries equality cannot be a regex");
+        }
 
-    if (e.type() == Undefined) {
-        return Status(ErrorCodes::BadValue, "ArrayFilterEntries equality cannot be undefined");
+        if (equality.type() == BSONType::Undefined) {
+            return Status(ErrorCodes::BadValue, "ArrayFilterEntries equality cannot be undefined");
+        }
+
+        if (equality.type() == BSONType::jstNULL) {
+            _hasNull = true;
+        } else if (equality.type() == BSONType::Array && equality.Obj().isEmpty()) {
+            _hasEmptyArray = true;
+        }
     }
 
-    if (e.type() == jstNULL) {
-        _hasNull = true;
-    }
-
-    if (e.type() == Array && e.Obj().isEmpty())
-        _hasEmptyArray = true;
-
-    _equalities.insert(e);
+    _equalities = BSONElementFlatSet(equalities.begin(), equalities.end());
     return Status::OK();
 }
 
@@ -582,8 +584,8 @@ void ArrayFilterEntries::copyTo(ArrayFilterEntries& toFillIn) const {
 
 void ArrayFilterEntries::debugString(StringBuilder& debug) const {
     debug << "[ ";
-    for (BSONElementSet::const_iterator it = _equalities.begin(); it != _equalities.end(); ++it) {
-        debug << it->toString(false) << " ";
+    for (auto&& equality : _equalities) {
+        debug << equality.toString(false) << " ";
     }
     for (size_t i = 0; i < _regexes.size(); ++i) {
         _regexes[i]->shortDebugString(debug);
@@ -593,8 +595,8 @@ void ArrayFilterEntries::debugString(StringBuilder& debug) const {
 }
 
 void ArrayFilterEntries::toBSON(BSONArrayBuilder* out) const {
-    for (BSONElementSet::const_iterator it = _equalities.begin(); it != _equalities.end(); ++it) {
-        out->append(*it);
+    for (auto&& equality : _equalities) {
+        out->append(equality);
     }
     for (size_t i = 0; i < _regexes.size(); ++i) {
         BSONObjBuilder regexBob;

src/mongo/db/matcher/expression_leaf.h
@@ -301,10 +301,10 @@ public:
     ArrayFilterEntries();
     ~ArrayFilterEntries();
 
-    Status addEquality(const BSONElement& e);
+    Status setEqualities(std::vector<BSONElement> equalities);
     Status addRegex(RegexMatchExpression* expr);
 
-    const BSONElementSet& equalities() const {
+    const BSONElementFlatSet& equalities() const {
         return _equalities;
     }
     bool contains(const BSONElement& elem) const {
@@ -342,7 +342,7 @@ public:
 private:
     bool _hasNull;  // if _equalities has a jstNULL element in it
     bool _hasEmptyArray;
-    BSONElementSet _equalities;
+    BSONElementFlatSet _equalities;
     std::vector<RegexMatchExpression*> _regexes;
 };
 

src/mongo/db/matcher/expression_parser.cpp
@@ -588,6 +588,7 @@ StatusWithMatchExpression MatchExpressionParser::_parseRegexDocument(const char*
 
 Status MatchExpressionParser::_parseArrayFilterEntries(ArrayFilterEntries* entries,
                                                        const BSONObj& theArray) {
+    std::vector<BSONElement> equalities;
     BSONObjIterator i(theArray);
     while (i.more()) {
         BSONElement e = i.next();
@@ -606,12 +607,10 @@ Status MatchExpressionParser::_parseArrayFilterEntries(ArrayFilterEntries* entri
             if (!s.isOK())
                 return s;
         } else {
-            Status s = entries->addEquality(e);
-            if (!s.isOK())
-                return s;
+            equalities.push_back(e);
         }
     }
-    return Status::OK();
+    return entries->setEqualities(std::move(equalities));
 }
 
 StatusWithMatchExpression MatchExpressionParser::_parseType(const char* name,