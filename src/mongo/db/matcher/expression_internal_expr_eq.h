xiexiaoy/eloqdoc

SERVER-31760 Add index support for InternalExprEqMatchExpression.

src/mongo/db/matcher/expression_internal_expr_eq.h
@@ -44,46 +44,30 @@ namespace mongo {
  * - Equality to null matches literal nulls, but not documents in which the field path is missing or
  *   undefined.
  *
- * - Equality to undefined is legal, and matches either literal undefined, or documents in which the
- *   field path is missing.
+ * - Equality to an array is illegal. It is invalid usage to construct a
+ *   InternalExprEqMatchExpression node which compares to an array.
  */
-class InternalExprEqMatchExpression final : public LeafMatchExpression {
+class InternalExprEqMatchExpression final : public ComparisonMatchExpressionBase {
 public:
     static constexpr StringData kName = "$_internalExprEq"_sd;
 
     InternalExprEqMatchExpression(StringData path, BSONElement value)
-        : LeafMatchExpression(MatchType::INTERNAL_EXPR_EQ,
-                              path,
-                              ElementPath::LeafArrayBehavior::kNoTraversal,
-                              ElementPath::NonLeafArrayBehavior::kMatchSubpath),
-          _rhsElem(value) {}
+        : ComparisonMatchExpressionBase(MatchType::INTERNAL_EXPR_EQ,
+                                        path,
+                                        value,
+                                        ElementPath::LeafArrayBehavior::kNoTraversal,
+                                        ElementPath::NonLeafArrayBehavior::kMatchSubpath) {
+        invariant(_rhs.type() != BSONType::Undefined);
+        invariant(_rhs.type() != BSONType::Array);
+    }
+
+    StringData name() const final {
+        return kName;
+    }
 
     bool matchesSingleElement(const BSONElement&, MatchDetails*) const final;
 
-    void debugString(StringBuilder& debug, int level) const final;
-
-    void serialize(BSONObjBuilder* out) const final;
-
-    bool equivalent(const MatchExpression* other) const final;
-
     std::unique_ptr<MatchExpression> shallowClone() const final;
-
-protected:
-    /**
-     * 'collator' must outlive the InternalExprEqMatchExpression and any clones made of it.
-     */
-    void _doSetCollator(const CollatorInterface* collator) final {
-        _collator = collator;
-    }
-
-    // Collator used to compare elements. By default, simple binary comparison will be used.
-    const CollatorInterface* _collator = nullptr;
-
-    BSONElement _rhsElem;
-
-private:
-    ExpressionOptimizerFunc getOptimizer() const final {
-        return [](std::unique_ptr<MatchExpression> expression) { return expression; };
-    }
 };
+
 }  // namespace mongo

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit 8170a94870aaf854ee4055fa1112f2199ae2c613 [formerly 6699621bfb54174c7ee082ee85c62211788942c3])
Conflicts:
       src/mongo/db/matcher/expression_internal_expr_eq.cpp
       src/mongo/db/matcher/expression_internal_expr_eq.h
       src/mongo/db/matcher/expression_internal_expr_eq_test.cpp
       src/mongo/db/matcher/expression_leaf.cpp
       src/mongo/db/matcher/expression_leaf.h
       src/mongo/db/matcher/expression_parser.cpp
Former-commit-id: 779beeca39066f939dc4fdbcb05f2e6ed05e99fa

src/mongo/db/matcher/expression_internal_expr_eq.h
@@ -44,49 +44,33 @@ namespace mongo {
  * - Equality to null matches literal nulls, but not documents in which the field path is missing or
  *   undefined.
  *
- * - Equality to undefined is legal, and matches either literal undefined, or documents in which the
- *   field path is missing.
+ * - Equality to an array is illegal. It is invalid usage to construct a
+ *   InternalExprEqMatchExpression node which compares to an array.
  */
-class InternalExprEqMatchExpression final : public LeafMatchExpression {
+class InternalExprEqMatchExpression final : public ComparisonMatchExpressionBase {
 public:
     static constexpr StringData kName = "$_internalExprEq"_sd;
 
     InternalExprEqMatchExpression()
-        : LeafMatchExpression(MatchType::INTERNAL_EXPR_EQ,
-                              ElementPath::LeafArrayBehavior::kNoTraversal,
-                              ElementPath::NonLeafArrayBehavior::kMatchSubpath) {}
+        : ComparisonMatchExpressionBase(MatchType::INTERNAL_EXPR_EQ,
+                                        ElementPath::LeafArrayBehavior::kNoTraversal,
+                                        ElementPath::NonLeafArrayBehavior::kMatchSubpath) {}
 
     Status init(StringData path, BSONElement value) {
-        _rhsElem = value;
+        invariant(value);
+        invariant(value.type() != BSONType::Undefined);
+        invariant(value.type() != BSONType::Array);
+        _rhs = value;
         return setPath(path);
+    }
+
+    StringData name() const final {
+        return kName;
     }
 
     bool matchesSingleElement(const BSONElement&, MatchDetails*) const final;
 
-    void debugString(StringBuilder& debug, int level) const final;
-
-    void serialize(BSONObjBuilder* out) const final;
-
-    bool equivalent(const MatchExpression* other) const final;
-
     std::unique_ptr<MatchExpression> shallowClone() const final;
-
-protected:
-    /**
-     * 'collator' must outlive the InternalExprEqMatchExpression and any clones made of it.
-     */
-    void _doSetCollator(const CollatorInterface* collator) final {
-        _collator = collator;
-    }
-
-    // Collator used to compare elements. By default, simple binary comparison will be used.
-    const CollatorInterface* _collator = nullptr;
-
-    BSONElement _rhsElem;
-
-private:
-    ExpressionOptimizerFunc getOptimizer() const final {
-        return [](std::unique_ptr<MatchExpression> expression) { return expression; };
-    }
 };
+
 }  // namespace mongo