xiexiaoy/eloqdoc

SERVER-28830 check for closed conn in cursorHandleFromId
The old implementation of DBCommandCursor used when running the shell
with --readMode compatibility calls getConnection(which checks if the
connection has been closed) to construct a DBClientCursor which it
passes to setCursor. The new variant used with --readMode command calls
setCursorHandle with the cursor ID. Check that we aren't closed in the
new variant.

jstests/noPassthroughWithMongod/dbcommand_cursor_throws_on_closed_conn.js
@@ -1,0 +1,17 @@
+(function() {
+    "use strict";
+
+    var testDB = db.getSiblingDB('dbcommand_cursor_throws_on_closed_conn');
+    testDB.dropDatabase();
+    var coll = testDB.collection;
+    var conn = testDB.getMongo();
+    conn.forceReadMode("commands");
+    assert.commandWorked(coll.save({}));
+    var res = assert.commandWorked(testDB.runCommand({
+        find: coll.getName(),
+        batchSize: 0,
+    }));
+
+    conn.close();
+    assert.throws(() => new DBCommandCursor(testDB, res));
+}());

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit 4e34ef207d31d286883b2dc88abd67f75668c724 [formerly bb2a2ccf260929e8a97080f81cd696340d429d57])
Former-commit-id: c3c9a3a04c47e08054b224e167373c6f46112f8c

jstests/noPassthroughWithMongod/dbcommand_cursor_throws_on_closed_conn.js
@@ -1,0 +1,17 @@
+(function() {
+    "use strict";
+
+    var testDB = db.getSiblingDB('dbcommand_cursor_throws_on_closed_conn');
+    testDB.dropDatabase();
+    var coll = testDB.collection;
+    var conn = testDB.getMongo();
+    conn.forceReadMode("commands");
+    assert.writeOK(coll.save({}));
+    var res = assert.commandWorked(testDB.runCommand({
+        find: coll.getName(),
+        batchSize: 0,
+    }));
+
+    conn.close();
+    assert.throws(() => new DBCommandCursor(testDB, res));
+}());