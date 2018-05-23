mongodb/mongo

SERVER-34661 Return early when the vote request response has an error. Fixed by making VoteRequester::Algorithm::processResponseto to check  ok field in the vote response message.

src/mongo/db/repl/vote_requester_test.cpp
@@ -31,6 +31,7 @@
 #include <memory>
 
 #include "mongo/base/status.h"
+#include "mongo/db/commands.h"
 #include "mongo/db/jsobj.h"
 #include "mongo/db/repl/repl_set_request_votes_args.h"
 #include "mongo/db/repl/vote_requester.h"
@@ -112,7 +113,16 @@ protected:
 
     void processResponse(const RemoteCommandRequest& request,
                          const RemoteCommandResponse& response) {
-        _requester->processResponse(request, response);
+        if (!response.isOK()) {
+            _requester->processResponse(request, response);
+            return;
+        }
+        BSONObjBuilder builder(response.data);
+        // Appends ok:1.0 (status ok) to response data if 'ok' field is missing.
+        CommandHelpers::appendCommandStatusNoThrow(builder, Status::OK());
+        RemoteCommandResponse responseWithCmdStatus = response;
+        responseWithCmdStatus.data = builder.obj();
+        _requester->processResponse(request, responseWithCmdStatus);
     }
 
     int getNumResponders() {
@@ -140,6 +150,18 @@ protected:
         response.setVoteGranted(true);
         response.setTerm(1);
         return RemoteCommandResponse(response.toBSON(), BSONObj(), Milliseconds(10));
+    }
+
+    RemoteCommandResponse votedYesStatusNotOkBecauseFailedToStoreLastVote() {
+        ReplSetRequestVotesResponse response;
+        BSONObjBuilder result;
+        response.setVoteGranted(true);
+        response.setTerm(1);
+        response.addToBSON(&result);
+        auto status =
+            Status(ErrorCodes::InterruptedDueToReplStateChange, "operation was interrupted");
+        CommandHelpers::appendCommandStatusNoThrow(result, status);
+        return RemoteCommandResponse(result.obj(), BSONObj(), Milliseconds(10));
     }
 
     RemoteCommandResponse votedNoBecauseConfigVersionDoesNotMatch() {
@@ -270,6 +292,19 @@ TEST_F(VoteRequesterTest, ImmediateGoodResponseWinElection) {
     ASSERT_TRUE(hasReceivedSufficientResponses());
     ASSERT(VoteRequester::Result::kSuccessfullyElected == getResult());
     ASSERT_EQUALS(1, getNumResponders());
+}
+
+TEST_F(VoteRequesterTest, VoterFailedToStoreLastVote) {
+    startCapturingLogMessages();
+    ASSERT_FALSE(hasReceivedSufficientResponses());
+    processResponse(requestFrom("host1"), votedYesStatusNotOkBecauseFailedToStoreLastVote());
+    ASSERT_FALSE(hasReceivedSufficientResponses());
+    ASSERT_EQUALS(1, countLogLinesContaining("received an invalid response from host1:27017"));
+    processResponse(requestFrom("host2"), votedYes());
+    ASSERT_TRUE(hasReceivedSufficientResponses());
+    ASSERT(VoteRequester::Result::kSuccessfullyElected == getResult());
+    ASSERT_EQUALS(2, getNumResponders());
+    stopCapturingLogMessages();
 }
 
 TEST_F(VoteRequesterTest, BadConfigVersionWinElection) {

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit f48946bbbe7cbb3d4c6fbaf05c0dda15ec4f6209 [formerly e4e2162c489c1faa569463f51058ebc09368a5f9])
Former-commit-id: 7f3e69a63c3ab61bfea2a563ded9d555fb017c8a

src/mongo/db/repl/vote_requester_test.cpp
@@ -31,6 +31,7 @@
 #include <memory>
 
 #include "mongo/base/status.h"
+#include "mongo/db/commands.h"
 #include "mongo/db/jsobj.h"
 #include "mongo/db/repl/repl_set_request_votes_args.h"
 #include "mongo/db/repl/replication_executor.h"
@@ -109,7 +110,17 @@ protected:
     }
 
     void processResponse(const RemoteCommandRequest& request, const ResponseStatus& response) {
-        _requester->processResponse(request, response);
+        if (!response.isOK()) {
+            _requester->processResponse(request, response);
+            return;
+        }
+        BSONObjBuilder builder;
+        builder.appendElements(response.data);
+        // Appends ok:1.0 (status ok) to response data if 'ok' field is missing.
+        Command::appendCommandStatus(builder, Status::OK());
+        ResponseStatus responseWithCmdStatus = response;
+        responseWithCmdStatus.data = builder.obj();
+        _requester->processResponse(request, responseWithCmdStatus);
     }
 
     int getNumResponders() {
@@ -138,6 +149,19 @@ protected:
         response.setTerm(1);
         return ResponseStatus(
             NetworkInterfaceMock::Response(response.toBSON(), BSONObj(), Milliseconds(10)));
+    }
+
+    ResponseStatus votedYesStatusNotOkBecauseFailedToStoreLastVote() {
+        ReplSetRequestVotesResponse response;
+        BSONObjBuilder result;
+        response.setVoteGranted(true);
+        response.setTerm(1);
+        response.addToBSON(&result);
+        auto status =
+            Status(ErrorCodes::InterruptedDueToReplStateChange, "operation was interrupted");
+        Command::appendCommandStatus(result, status);
+        return ResponseStatus(
+            NetworkInterfaceMock::Response(result.obj(), BSONObj(), Milliseconds(10)));
     }
 
     ResponseStatus votedNoBecauseConfigVersionDoesNotMatch() {
@@ -234,6 +258,19 @@ TEST_F(VoteRequesterTest, ImmediateGoodResponseWinElection) {
     ASSERT_TRUE(hasReceivedSufficientResponses());
     ASSERT(VoteRequester::Result::kSuccessfullyElected == getResult());
     ASSERT_EQUALS(1, getNumResponders());
+}
+
+TEST_F(VoteRequesterTest, VoterFailedToStoreLastVote) {
+    startCapturingLogMessages();
+    ASSERT_FALSE(hasReceivedSufficientResponses());
+    processResponse(requestFrom("host1"), votedYesStatusNotOkBecauseFailedToStoreLastVote());
+    ASSERT_FALSE(hasReceivedSufficientResponses());
+    ASSERT_EQUALS(1, countLogLinesContaining("received an invalid response from host1:27017"));
+    processResponse(requestFrom("host2"), votedYes());
+    ASSERT_TRUE(hasReceivedSufficientResponses());
+    ASSERT(VoteRequester::Result::kSuccessfullyElected == getResult());
+    ASSERT_EQUALS(2, getNumResponders());
+    stopCapturingLogMessages();
 }
 
 TEST_F(VoteRequesterTest, BadConfigVersionWinElection) {