xiexiaoy/eloqdoc

SERVER-34900 add ReplicationCoordinatorExternalState::getInitialSyncBatchLimits()

src/mongo/db/repl/replication_coordinator_external_state_impl.h
@@ -107,6 +107,7 @@ public:
     virtual bool isReadCommittedSupportedByStorageEngine(OperationContext* opCtx) const;
     virtual bool isReadConcernSnapshotSupportedByStorageEngine(OperationContext* opCtx) const;
     virtual std::size_t getOplogFetcherMaxFetcherRestarts() const override;
+    OplogApplier::BatchLimits getInitialSyncBatchLimits() const final;
 
     // Methods from JournalListener.
     virtual JournalListener::Token getToken();

src/mongo/db/repl/replication_coordinator_external_state_mock.h
@@ -96,6 +96,7 @@ public:
     virtual bool isReadCommittedSupportedByStorageEngine(OperationContext* opCtx) const;
     virtual bool isReadConcernSnapshotSupportedByStorageEngine(OperationContext* opCtx) const;
     virtual std::size_t getOplogFetcherMaxFetcherRestarts() const override;
+    OplogApplier::BatchLimits getInitialSyncBatchLimits() const final;
 
     /**
      * Adds "host" to the list of hosts that this mock will match when responding to "isSelf"

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit 1f500c2bb039d58978fd88b6b132afb1bf399009 [formerly 3bee8adabe9841da544a7e46788cbf5143552bb6])
Former-commit-id: e3771d986d7987e25be0c8e67adfdf8315c1deb6

src/mongo/db/repl/replication_coordinator_external_state_impl.h
@@ -117,6 +117,7 @@ public:
     virtual std::unique_ptr<OplogBuffer> makeSteadyStateOplogBuffer(
         OperationContext* opCtx) const override;
     virtual std::size_t getOplogFetcherMaxFetcherRestarts() const override;
+    SyncTail::BatchLimits getInitialSyncBatchLimits() const final;
 
     // Methods from JournalListener.
     virtual JournalListener::Token getToken();

src/mongo/db/repl/replication_coordinator_external_state_mock.h
@@ -105,6 +105,7 @@ public:
     virtual std::unique_ptr<OplogBuffer> makeSteadyStateOplogBuffer(
         OperationContext* opCtx) const override;
     virtual std::size_t getOplogFetcherMaxFetcherRestarts() const override;
+    SyncTail::BatchLimits getInitialSyncBatchLimits() const final;
 
     /**
      * Adds "host" to the list of hosts that this mock will match when responding to "isSelf"