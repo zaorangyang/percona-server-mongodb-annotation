mongodb/mongo

SERVER-35401: Whole-db and whole-cluster change streams do not filter operations to "system" collections

src/mongo/db/pipeline/document_source_change_stream.cpp
@@ -310,11 +310,11 @@ std::string DocumentSourceChangeStream::getNsRegexForChangeStream(const Namespac
         case ChangeStreamType::kSingleDatabase:
             // Match all namespaces that start with db name, followed by ".", then NOT followed by
             // '$' or 'system.'
-            return "^" + nss.db() + kRegexAllCollections;
+            return "^" + nss.db() + "\\." + kRegexAllCollections;
         case ChangeStreamType::kAllChangesForCluster:
             // Match all namespaces that start with any db name other than admin, config, or local,
-            // followed by ".", then NOT followed by '$' or 'system.'
-            return "^" + kRegexAllDBs + kRegexAllCollections;
+            // followed by ".", then NOT followed by '$' or 'system.'.
+            return kRegexAllDBs + "\\." + kRegexAllCollections;
         default:
             MONGO_UNREACHABLE;
     }
@@ -346,18 +346,19 @@ BSONObj DocumentSourceChangeStream::buildMatchFilter(
                 BSON("o.create" << nss.coll() << "o.collation" << BSON("$exists" << true)));
         }
     } else {
-        // For change streams on an entire database, include notifications for individual collection
-        // drops and renames which will not invalidate the stream. Also include the 'dropDatabase'
-        // command which will invalidate the stream.
-        relevantCommands.append(BSON("o.drop" << BSON("$exists" << true)));
+        // For change streams on an entire database, include notifications for drops and renames of
+        // non-system collections which will not invalidate the stream. Also include the
+        // 'dropDatabase' command which will invalidate the stream.
+        relevantCommands.append(BSON("o.drop" << BSONRegEx("^" + kRegexAllCollections)));
         relevantCommands.append(BSON("o.dropDatabase" << BSON("$exists" << true)));
-        relevantCommands.append(BSON("o.renameCollection" << BSON("$exists" << true)));
+        relevantCommands.append(
+            BSON("o.renameCollection" << BSONRegEx(getNsRegexForChangeStream(nss))));
     }
 
     // For cluster-wide $changeStream, match the command namespace of any database other than admin,
     // config, or local. Otherwise, match only against the target db's command namespace.
     auto cmdNsFilter = (sourceType == ChangeStreamType::kAllChangesForCluster
-                            ? BSON("ns" << BSONRegEx("^" + kRegexAllDBs + kRegexCmdColl))
+                            ? BSON("ns" << BSONRegEx(kRegexAllDBs + "\\." + kRegexCmdColl))
                             : BSON("ns" << nss.getCommandNS().ns()));
 
     // 1.1) Commands that are on target db(s) and one of the above supported commands.
@@ -365,9 +366,7 @@ BSONObj DocumentSourceChangeStream::buildMatchFilter(
         BSON("$and" << BSON_ARRAY(cmdNsFilter << BSON("$or" << relevantCommands.arr())));
 
     // 1.2) Supported commands that have arbitrary db namespaces in "ns" field.
-    auto renameDropTarget = (sourceType == ChangeStreamType::kAllChangesForCluster
-                                 ? BSON("o.to" << BSON("$exists" << true))
-                                 : BSON("o.to" << nss.ns()));
+    auto renameDropTarget = BSON("o.to" << BSONRegEx(getNsRegexForChangeStream(nss)));
 
     // All supported commands that are either (1.1) or (1.2).
     BSONObj commandMatch = BSON("op"

src/mongo/db/pipeline/document_source_change_stream_test.cpp
@@ -1133,6 +1133,41 @@ TEST_F(ChangeStreamStageTest, DocumentKeyShouldNotIncludeShardKeyIfResumeTokenDo
         40645);
 }
 
+TEST_F(ChangeStreamStageTest, RenameFromSystemToUserCollectionShouldIncludeNotification) {
+    // Renaming to a non-system collection will include a notification in the stream.
+    NamespaceString systemColl(nss.db() + ".system.users");
+    OplogEntry rename =
+        createCommand(BSON("renameCollection" << systemColl.ns() << "to" << nss.ns()), testUuid());
+
+    // Note that the collection rename does *not* have the queued invalidated field.
+    Document expectedRename{
+        {DSChangeStream::kRenameTargetNssField, D{{"db", nss.db()}, {"coll", nss.coll()}}},
+        {DSChangeStream::kIdField, makeResumeToken(kDefaultTs, testUuid())},
+        {DSChangeStream::kOperationTypeField, DSChangeStream::kRenameCollectionOpType},
+        {DSChangeStream::kClusterTimeField, kDefaultTs},
+        {DSChangeStream::kNamespaceField, D{{"db", systemColl.db()}, {"coll", systemColl.coll()}}},
+    };
+    checkTransformation(rename, expectedRename);
+}
+
+TEST_F(ChangeStreamStageTest, RenameFromUserToSystemCollectionShouldIncludeNotification) {
+    // Renaming to a system collection will include a notification in the stream.
+    NamespaceString systemColl(nss.db() + ".system.users");
+    OplogEntry rename =
+        createCommand(BSON("renameCollection" << nss.ns() << "to" << systemColl.ns()), testUuid());
+
+    // Note that the collection rename does *not* have the queued invalidated field.
+    Document expectedRename{
+        {DSChangeStream::kRenameTargetNssField,
+         D{{"db", systemColl.db()}, {"coll", systemColl.coll()}}},
+        {DSChangeStream::kIdField, makeResumeToken(kDefaultTs, testUuid())},
+        {DSChangeStream::kOperationTypeField, DSChangeStream::kRenameCollectionOpType},
+        {DSChangeStream::kClusterTimeField, kDefaultTs},
+        {DSChangeStream::kNamespaceField, D{{"db", nss.db()}, {"coll", nss.coll()}}},
+    };
+    checkTransformation(rename, expectedRename);
+}
+
 //
 // Test class for change stream of a single database.
 //
@@ -1377,33 +1412,55 @@ TEST_F(ChangeStreamStageDBTest, TransformDropDatabase) {
     checkTransformation(dropDB, expectedInvalidateDropDatabase);
 }
 
-TEST_F(ChangeStreamStageDBTest, SystemCollectionsDrop) {
+TEST_F(ChangeStreamStageDBTest, MatchFiltersOperationsOnSystemCollections) {
     NamespaceString systemColl(nss.db() + ".system.users");
+    OplogEntry insert = makeOplogEntry(OpTypeEnum::kInsert, systemColl, BSON("_id" << 1));
+    checkTransformation(insert, boost::none);
+
     OplogEntry dropColl = createCommand(BSON("drop" << systemColl.coll()), testUuid());
-    // Note that the collection drop does *not* have the queued invalidated field.
-    Document expectedDrop{
-        {DSChangeStream::kIdField, makeResumeToken(kDefaultTs, testUuid())},
-        {DSChangeStream::kOperationTypeField, DSChangeStream::kDropCollectionOpType},
-        {DSChangeStream::kClusterTimeField, kDefaultTs},
-        {DSChangeStream::kNamespaceField, D{{"db", systemColl.db()}, {"coll", systemColl.coll()}}},
-    };
-    checkTransformation(dropColl, expectedDrop);
-}
+    checkTransformation(dropColl, boost::none);
 
-TEST_F(ChangeStreamStageDBTest, SystemCollectionsRename) {
-    NamespaceString systemColl(nss.db() + ".system.users");
-    NamespaceString renamedSystemColl(nss.db() + ".system.users_new");
-
+    // Rename from a 'system' collection to another 'system' collection should not include a
+    // notification.
+    NamespaceString renamedSystemColl(nss.db() + ".system.views");
     OplogEntry rename = createCommand(
         BSON("renameCollection" << systemColl.ns() << "to" << renamedSystemColl.ns()), testUuid());
+    checkTransformation(rename, boost::none);
+}
+
+TEST_F(ChangeStreamStageDBTest, RenameFromSystemToUserCollectionShouldIncludeNotification) {
+    // Renaming to a non-system collection will include a notification in the stream.
+    NamespaceString systemColl(nss.db() + ".system.users");
+    NamespaceString renamedColl(nss.db() + ".non_system_coll");
+    OplogEntry rename = createCommand(
+        BSON("renameCollection" << systemColl.ns() << "to" << renamedColl.ns()), testUuid());
+
     // Note that the collection rename does *not* have the queued invalidated field.
     Document expectedRename{
         {DSChangeStream::kRenameTargetNssField,
-         D{{"db", renamedSystemColl.db()}, {"coll", renamedSystemColl.coll()}}},
+         D{{"db", renamedColl.db()}, {"coll", renamedColl.coll()}}},
         {DSChangeStream::kIdField, makeResumeToken(kDefaultTs, testUuid())},
         {DSChangeStream::kOperationTypeField, DSChangeStream::kRenameCollectionOpType},
         {DSChangeStream::kClusterTimeField, kDefaultTs},
         {DSChangeStream::kNamespaceField, D{{"db", systemColl.db()}, {"coll", systemColl.coll()}}},
+    };
+    checkTransformation(rename, expectedRename);
+}
+
+TEST_F(ChangeStreamStageDBTest, RenameFromUserToSystemCollectionShouldIncludeNotification) {
+    // Renaming to a system collection will include a notification in the stream.
+    NamespaceString systemColl(nss.db() + ".system.users");
+    OplogEntry rename =
+        createCommand(BSON("renameCollection" << nss.ns() << "to" << systemColl.ns()), testUuid());
+
+    // Note that the collection rename does *not* have the queued invalidated field.
+    Document expectedRename{
+        {DSChangeStream::kRenameTargetNssField,
+         D{{"db", systemColl.db()}, {"coll", systemColl.coll()}}},
+        {DSChangeStream::kIdField, makeResumeToken(kDefaultTs, testUuid())},
+        {DSChangeStream::kOperationTypeField, DSChangeStream::kRenameCollectionOpType},
+        {DSChangeStream::kClusterTimeField, kDefaultTs},
+        {DSChangeStream::kNamespaceField, D{{"db", nss.db()}, {"coll", nss.coll()}}},
     };
     checkTransformation(rename, expectedRename);
 }

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit 6a8b445ef6b8974a5100cf4861d31f04c65aebd1 [formerly 12f7795c52a7d352da1a7e03f71a233624dc05d4])
Former-commit-id: 3c4c5b4da681e6d50744460243f6361fe0328389

src/mongo/db/pipeline/document_source_change_stream.cpp
@@ -285,11 +285,11 @@ std::string DocumentSourceChangeStream::getNsRegexForChangeStream(const Namespac
         case ChangeStreamType::kSingleDatabase:
             // Match all namespaces that start with db name, followed by ".", then NOT followed by
             // '$' or 'system.'
-            return "^" + nss.db() + kRegexAllCollections;
+            return "^" + nss.db() + "\\." + kRegexAllCollections;
         case ChangeStreamType::kAllChangesForCluster:
             // Match all namespaces that start with any db name other than admin, config, or local,
-            // followed by ".", then NOT followed by '$' or 'system.'
-            return "^" + kRegexAllDBs + kRegexAllCollections;
+            // followed by ".", then NOT followed by '$' or 'system.'.
+            return kRegexAllDBs + "\\." + kRegexAllCollections;
         default:
             MONGO_UNREACHABLE;
     }
@@ -320,18 +320,20 @@ BSONObj DocumentSourceChangeStream::buildMatchFilter(
                 BSON("o.create" << nss.coll() << "o.collation" << BSON("$exists" << true)));
         }
     } else {
-        // For change streams on an entire database, the stream is invalidated if any collections in
-        // that database are dropped or renamed. For cluster-wide streams, drops or renames of any
-        // collection in any database (aside from the internal databases admin, config and local)
-        // will invalidate the stream.
-        invalidatingCommands.append(BSON("o.drop" << BSON("$exists" << true)));
-        invalidatingCommands.append(BSON("o.renameCollection" << BSON("$exists" << true)));
+        // For change streams on an entire database, the stream is invalidated if any non-system
+        // collections in that database are dropped or renamed. For cluster-wide streams, drops or
+        // renames of any non-system collection in any database (aside from the internal databases
+        // admin, config and local) will invalidate the stream.
+        invalidatingCommands.append(BSON("o.drop" << BSONRegEx("^" + kRegexAllCollections)));
+        // Note that 'o.renameCollection' contains the full NamespaceString.
+        invalidatingCommands.append(
+            BSON("o.renameCollection" << BSONRegEx(getNsRegexForChangeStream(nss))));
     }
 
     // For cluster-wide $changeStream, match the command namespace of any database other than admin,
     // config, or local. Otherwise, match only against the target db's command namespace.
     auto cmdNsFilter = (sourceType == ChangeStreamType::kAllChangesForCluster
-                            ? BSON("ns" << BSONRegEx("^" + kRegexAllDBs + kRegexCmdColl))
+                            ? BSON("ns" << BSONRegEx(kRegexAllDBs + "\\." + kRegexCmdColl))
                             : BSON("ns" << nss.getCommandNS().ns()));
 
     // 1.1) Commands that are on target db(s) and one of the above invalidating commands.
@@ -339,9 +341,7 @@ BSONObj DocumentSourceChangeStream::buildMatchFilter(
         BSON("$and" << BSON_ARRAY(cmdNsFilter << BSON("$or" << invalidatingCommands.arr())));
 
     // 1.2) Supported commands that have arbitrary db namespaces in "ns" field.
-    auto renameDropTarget = (sourceType == ChangeStreamType::kAllChangesForCluster
-                                 ? BSON("o.to" << BSON("$exists" << true))
-                                 : BSON("o.to" << nss.ns()));
+    auto renameDropTarget = BSON("o.to" << BSONRegEx(getNsRegexForChangeStream(nss)));
 
     // All supported commands that are either (1.1) or (1.2).
     BSONObj commandMatch = BSON("op"

src/mongo/db/pipeline/document_source_change_stream_test.cpp
@@ -1086,6 +1086,32 @@ TEST_F(ChangeStreamStageTest, DocumentKeyShouldNotIncludeShardKeyIfResumeTokenDo
         40645);
 }
 
+TEST_F(ChangeStreamStageTest, RenameFromSystemToUserCollectionShouldIncludeNotification) {
+    // Renaming to a non-system collection will include a notification in the stream.
+    NamespaceString systemColl(nss.db() + ".system.users");
+    OplogEntry rename =
+        createCommand(BSON("renameCollection" << systemColl.ns() << "to" << nss.ns()), testUuid());
+    Document expectedInvalidate{
+        {DSChangeStream::kIdField, makeResumeToken(kDefaultTs, testUuid())},
+        {DSChangeStream::kOperationTypeField, DSChangeStream::kInvalidateOpType},
+        {DSChangeStream::kClusterTimeField, kDefaultTs},
+    };
+    checkTransformation(rename, expectedInvalidate);
+}
+
+TEST_F(ChangeStreamStageTest, RenameFromUserToSystemCollectionShouldIncludeNotification) {
+    // Renaming to a system collection will include a notification in the stream.
+    NamespaceString systemColl(nss.db() + ".system.users");
+    OplogEntry rename =
+        createCommand(BSON("renameCollection" << nss.ns() << "to" << systemColl.ns()), testUuid());
+    Document expectedInvalidate{
+        {DSChangeStream::kIdField, makeResumeToken(kDefaultTs, testUuid())},
+        {DSChangeStream::kOperationTypeField, DSChangeStream::kInvalidateOpType},
+        {DSChangeStream::kClusterTimeField, kDefaultTs},
+    };
+    checkTransformation(rename, expectedInvalidate);
+}
+
 //
 // Test class for change stream of a single database.
 //
@@ -1317,20 +1343,46 @@ TEST_F(ChangeStreamStageDBTest, TransformInvalidate) {
     checkTransformation(dropDB, expectedInvalidateDropDatabase);
 }
 
-TEST_F(ChangeStreamStageDBTest, SystemCollectionsDropOrRenameShouldInvalidate) {
+TEST_F(ChangeStreamStageDBTest, MatchFiltersOperationsOnSystemCollections) {
     NamespaceString systemColl(nss.db() + ".system.users");
-    NamespaceString renamedSystemColl(nss.db() + ".system.users_new");
+    OplogEntry insert = makeOplogEntry(OpTypeEnum::kInsert, systemColl, BSON("_id" << 1));
+    checkTransformation(insert, boost::none);
+
+    OplogEntry dropColl = createCommand(BSON("drop" << systemColl.coll()), testUuid());
+    checkTransformation(dropColl, boost::none);
+
+    // Rename from a 'system' collection to another 'system' collection should not include a
+    // notification.
+    NamespaceString renamedSystemColl(nss.db() + ".system.views");
+    OplogEntry rename = createCommand(
+        BSON("renameCollection" << systemColl.ns() << "to" << renamedSystemColl.ns()), testUuid());
+    checkTransformation(rename, boost::none);
+}
+
+TEST_F(ChangeStreamStageDBTest, RenameFromSystemToUserCollectionShouldIncludeNotification) {
+    // Renaming to a non-system collection will include a notification in the stream.
+    NamespaceString systemColl(nss.db() + ".system.users");
+    NamespaceString renamedColl(nss.db() + ".non_system_coll");
+    OplogEntry rename = createCommand(
+        BSON("renameCollection" << systemColl.ns() << "to" << renamedColl.ns()), testUuid());
     Document expectedInvalidate{
         {DSChangeStream::kIdField, makeResumeToken(kDefaultTs, testUuid())},
         {DSChangeStream::kOperationTypeField, DSChangeStream::kInvalidateOpType},
         {DSChangeStream::kClusterTimeField, kDefaultTs},
     };
+    checkTransformation(rename, expectedInvalidate);
+}
 
-    OplogEntry dropColl = createCommand(BSON("drop" << systemColl.coll()), testUuid());
-    checkTransformation(dropColl, expectedInvalidate);
-
-    OplogEntry rename = createCommand(
-        BSON("renameCollection" << systemColl.ns() << "to" << renamedSystemColl.ns()), testUuid());
+TEST_F(ChangeStreamStageDBTest, RenameFromUserToSystemCollectionShouldIncludeNotification) {
+    // Renaming to a system collection will include a notification in the stream.
+    NamespaceString systemColl(nss.db() + ".system.users");
+    OplogEntry rename =
+        createCommand(BSON("renameCollection" << nss.ns() << "to" << systemColl.ns()), testUuid());
+    Document expectedInvalidate{
+        {DSChangeStream::kIdField, makeResumeToken(kDefaultTs, testUuid())},
+        {DSChangeStream::kOperationTypeField, DSChangeStream::kInvalidateOpType},
+        {DSChangeStream::kClusterTimeField, kDefaultTs},
+    };
     checkTransformation(rename, expectedInvalidate);
 }
 