mongodb/mongo

SERVER-34822: Make role graph ignore irrelevant createIndexes

jstests/auth/user_defined_roles_on_secondaries.js
@@ -189,6 +189,17 @@
               }
             },
             {
+              op: "i",
+              ns: "admin.system.roles",
+              o: {
+                  _id: "db1.t3",
+                  role: "t3",
+                  db: "db1",
+                  roles: [{role: "t1", db: "db1"}, {role: "t2", db: "db1"}],
+                  privileges: []
+              }
+            },
+            {
               op: "u",
               ns: "admin.system.roles",
               o: {$set: {roles: [{role: "readWrite", db: "db1"}]}},
@@ -206,6 +217,16 @@
         var role = node.getDB("db1").getRole("t2");
         assert.eq(1, role.roles.length, tojson(node));
         assertListContainsRole(role.roles, {role: "readWrite", db: "db1"}, node);
+    });
+
+    // Verify that irrelevant index creation doesn't impair graph resolution
+    assert.commandWorked(rstest.getPrimary().getDB("admin").col.save({data: 5}));
+    assert.commandWorked(rstest.getPrimary().getDB("admin").runCommand(
+        {createIndexes: "col", indexes: [{key: {data: 1}, name: "testIndex"}]}));
+    rstest.awaitReplication();
+    rstest.nodes.forEach(function(node) {
+        var role = node.getDB("db1").getRole("t3");
+        assert.eq(4, role.inheritedRoles.length, tojson(node));
     });
 
     rstest.stopSet();

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit c437830d9cb4d70569f5021473582e59cfcc8500 [formerly f0227671de94cd54a3d8e1653400aa1ee9d8b2fa])
Former-commit-id: d82ca47151cdea0b1df326890913e2f0124a8d8d

jstests/auth/user_defined_roles_on_secondaries.js
@@ -188,6 +188,17 @@
               }
             },
             {
+              op: "i",
+              ns: "admin.system.roles",
+              o: {
+                  _id: "db1.t3",
+                  role: "t3",
+                  db: "db1",
+                  roles: [{role: "t1", db: "db1"}, {role: "t2", db: "db1"}],
+                  privileges: []
+              }
+            },
+            {
               op: "u",
               ns: "admin.system.roles",
               o: {$set: {roles: [{role: "readWrite", db: "db1"}]}},
@@ -205,6 +216,16 @@
         var role = node.getDB("db1").getRole("t2");
         assert.eq(1, role.roles.length, node);
         assertListContainsRole(role.roles, {role: "readWrite", db: "db1"}, node);
+    });
+
+    // Verify that irrelevant index creation doesn't impair graph resolution
+    assert.writeOK(rstest.getPrimary().getDB("admin").col.save({data: 5}));
+    assert.commandWorked(rstest.getPrimary().getDB("admin").runCommand(
+        {createIndexes: "col", indexes: [{key: {data: 1}, name: "testIndex"}]}));
+    rstest.awaitReplication();
+    rstest.nodes.forEach(function(node) {
+        var role = node.getDB("db1").getRole("t3");
+        assert.eq(4, role.inheritedRoles.length, tojson(node));
     });
 
     rstest.stopSet();