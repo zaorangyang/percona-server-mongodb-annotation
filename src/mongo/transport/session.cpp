mongodb/mongo

SERVER-34823 Make sure LegacySession ID's match their MessagingPort connection ID

src/mongo/transport/session.cpp
@@ -45,6 +45,8 @@ AtomicUInt64 sessionIdCounter(0);
 
 Session::Session() : _id(sessionIdCounter.addAndFetch(1)), _tags(kPending) {}
 
+Session::Session(Id id) : _id(id), _tags(kPending) {}
+
 Ticket Session::sourceMessage(Message* message, Date_t expiration) {
     return getTransportLayer()->sourceMessage(shared_from_this(), message, expiration);
 }

src/mongo/transport/transport_layer_legacy.cpp
@@ -80,7 +80,8 @@ std::shared_ptr<TransportLayerLegacy::LegacySession> TransportLayerLegacy::Legac
 
 TransportLayerLegacy::LegacySession::LegacySession(std::unique_ptr<AbstractMessagingPort> amp,
                                                    TransportLayerLegacy* tl)
-    : _remote(amp->remoteAddr()),
+    : Session(amp->connectionId()),
+      _remote(amp->remoteAddr()),
       _local(amp->localAddr()),
       _tl(tl),
       _connection(stdx::make_unique<Connection>(std::move(amp))) {}

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit 70cf29355b46d0638d9f55d3e4ce5648c10a6fc3 [formerly dd7de2240e42ea384d3804634522739618a6da0a])
Former-commit-id: 1f9f74c540e36d6edff36b5ae04c7132d2e65c41

src/mongo/transport/session.cpp
@@ -45,6 +45,8 @@ AtomicUInt64 sessionIdCounter(0);
 
 Session::Session() : _id(sessionIdCounter.addAndFetch(1)), _tags(kEmptyTagMask) {}
 
+Session::Session(Id id) : _id(id), _tags(kEmptyTagMask) {}
+
 Ticket Session::sourceMessage(Message* message, Date_t expiration) {
     return getTransportLayer()->sourceMessage(shared_from_this(), message, expiration);
 }

src/mongo/transport/transport_layer_legacy.cpp
@@ -84,7 +84,8 @@ std::shared_ptr<TransportLayerLegacy::LegacySession> TransportLayerLegacy::Legac
 
 TransportLayerLegacy::LegacySession::LegacySession(std::unique_ptr<AbstractMessagingPort> amp,
                                                    TransportLayerLegacy* tl)
-    : _remote(amp->remote()),
+    : Session(amp->connectionId()),
+      _remote(amp->remote()),
       _local(amp->localAddr().toString(true)),
       _tl(tl),
       _tags(kEmptyTagMask),