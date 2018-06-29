henrikedin/mongo

SERVER-35115 Separate dbclientinterface.h into several parts, one per class.

src/mongo/shell/shell_utils.cpp
@@ -32,7 +32,7 @@
 
 #include "mongo/shell/shell_utils.h"
 
-#include "mongo/client/dbclientinterface.h"
+#include "mongo/client/dbclient_network.h"
 #include "mongo/client/replica_set_monitor.h"
 #include "mongo/db/hasher.h"
 #include "mongo/platform/random.h"

zaorangyang/percona-server-mongodb-annotation

Former-commit-id: cfa96795eefed2061628891c43ccdc2c256bb40a

src/mongo/shell/shell_utils.cpp
@@ -32,7 +32,7 @@
 
 #include "mongo/shell/shell_utils.h"
 
-#include "mongo/client/dbclientinterface.h"
+#include "mongo/client/dbclient_base.h"
 #include "mongo/client/replica_set_monitor.h"
 #include "mongo/db/hasher.h"
 #include "mongo/platform/random.h"