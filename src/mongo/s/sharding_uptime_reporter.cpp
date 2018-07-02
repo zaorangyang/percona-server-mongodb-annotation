xiexiaoy/eloqdoc

SERVER-25746 store advisoryHostFQDNs in config.mongos

src/mongo/s/sharding_uptime_reporter.cpp
@@ -41,6 +41,7 @@
 #include "mongo/util/exit.h"
 #include "mongo/util/log.h"
 #include "mongo/util/mongoutils/str.h"
+#include "mongo/util/net/hostname_canonicalization.h"
 #include "mongo/util/net/sock.h"
 #include "mongo/util/version.h"
 
@@ -49,8 +50,8 @@ namespace {
 
 const Seconds kUptimeReportInterval(10);
 
-std::string constructInstanceIdString() {
-    return str::stream() << getHostNameCached() << ":" << serverGlobalParams.port;
+std::string constructInstanceIdString(const std::string& hostName) {
+    return str::stream() << hostName << ":" << serverGlobalParams.port;
 }
 
 /**
@@ -59,6 +60,7 @@ std::string constructInstanceIdString() {
  */
 void reportStatus(OperationContext* opCtx,
                   const std::string& instanceId,
+                  const std::string& hostName,
                   const Timer& upTimeTimer) {
     MongosType mType;
     mType.setName(instanceId);
@@ -67,6 +69,8 @@ void reportStatus(OperationContext* opCtx,
     // balancer is never active in mongos. Here for backwards compatibility only.
     mType.setWaiting(true);
     mType.setMongoVersion(VersionInfoInterface::instance().version().toString());
+    mType.setAdvisoryHostFQDNs(
+        getHostFQDNs(hostName, HostnameCanonicalizationMode::kForwardAndReverse));
 
     try {
         Grid::get(opCtx)
@@ -98,13 +102,14 @@ void ShardingUptimeReporter::startPeriodicThread() {
     _thread = stdx::thread([] {
         Client::initThread("Uptime reporter");
 
-        const std::string instanceId(constructInstanceIdString());
+        const std::string hostName(getHostNameCached());
+        const std::string instanceId(constructInstanceIdString(hostName));
         const Timer upTimeTimer;
 
         while (!globalInShutdownDeprecated()) {
             {
                 auto opCtx = cc().makeOperationContext();
-                reportStatus(opCtx.get(), instanceId, upTimeTimer);
+                reportStatus(opCtx.get(), instanceId, hostName, upTimeTimer);
 
                 auto status = Grid::get(opCtx.get())
                                   ->getBalancerConfiguration()

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit 5681c1ac784b9d34eaa2d3c684cab579821b8917 [formerly 75c8414afea212e79b27dae42cfd2930bdfd6eea])
Former-commit-id: 0d6a9242c11b99ddadcfb6e86a850b6ba487530a

src/mongo/s/sharding_uptime_reporter.cpp
@@ -42,6 +42,7 @@
 #include "mongo/util/exit.h"
 #include "mongo/util/log.h"
 #include "mongo/util/mongoutils/str.h"
+#include "mongo/util/net/hostname_canonicalization.h"
 #include "mongo/util/net/sock.h"
 #include "mongo/util/version.h"
 
@@ -50,15 +51,18 @@ namespace {
 
 const Seconds kUptimeReportInterval(10);
 
-std::string constructInstanceIdString() {
-    return str::stream() << getHostNameCached() << ":" << serverGlobalParams.port;
+std::string constructInstanceIdString(const std::string& hostName) {
+    return str::stream() << hostName << ":" << serverGlobalParams.port;
 }
 
 /**
  * Reports the uptime status of the current instance to the config.pings collection. This method
  * is best-effort and never throws.
  */
-void reportStatus(OperationContext* txn, const std::string& instanceId, const Timer& upTimeTimer) {
+void reportStatus(OperationContext* txn,
+                  const std::string& instanceId,
+                  const std::string& hostName,
+                  const Timer& upTimeTimer) {
     MongosType mType;
     mType.setName(instanceId);
     mType.setPing(jsTime());
@@ -66,6 +70,8 @@ void reportStatus(OperationContext* txn, const std::string& instanceId, const Ti
     // balancer is never active in mongos. Here for backwards compatibility only.
     mType.setWaiting(true);
     mType.setMongoVersion(VersionInfoInterface::instance().version().toString());
+    mType.setAdvisoryHostFQDNs(
+        getHostFQDNs(hostName, HostnameCanonicalizationMode::kForwardAndReverse));
 
     try {
         Grid::get(txn)->catalogClient(txn)->updateConfigDocument(
@@ -95,13 +101,14 @@ void ShardingUptimeReporter::startPeriodicThread() {
     _thread = stdx::thread([this] {
         Client::initThread("Uptime reporter");
 
-        const std::string instanceId(constructInstanceIdString());
+        const std::string hostName(getHostNameCached());
+        const std::string instanceId(constructInstanceIdString(hostName));
         const Timer upTimeTimer;
 
         while (!inShutdown()) {
             {
                 auto txn = cc().makeOperationContext();
-                reportStatus(txn.get(), instanceId, upTimeTimer);
+                reportStatus(txn.get(), instanceId, hostName, upTimeTimer);
 
                 auto status =
                     Grid::get(txn.get())->getBalancerConfiguration()->refreshAndCheck(txn.get());