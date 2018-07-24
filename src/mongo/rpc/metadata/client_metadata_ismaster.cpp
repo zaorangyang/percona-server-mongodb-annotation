xiexiaoy/eloqdoc

SERVER-34141 Reset client metadata when missing in metadata

src/mongo/rpc/metadata/client_metadata_ismaster.cpp
@@ -65,18 +65,31 @@ const boost::optional<ClientMetadata>& ClientMetadataIsMasterState::getClientMet
     return _clientMetadata;
 }
 
-void ClientMetadataIsMasterState::setClientMetadata(
-    Client* client, boost::optional<ClientMetadata> clientMetadata) {
+void ClientMetadataIsMasterState::setClientMetadata(Client* client,
+                                                    boost::optional<ClientMetadata> clientMetadata,
+                                                    bool setViaMetadata) {
     auto& state = get(client);
 
     stdx::lock_guard<Client> lk(*client);
     state._clientMetadata = std::move(clientMetadata);
+    state._setViaMetadata = setViaMetadata;
 }
 
 
 Status ClientMetadataIsMasterState::readFromMetadata(OperationContext* opCtx,
                                                      BSONElement& element) {
+    auto& clientMetadataIsMasterState = ClientMetadataIsMasterState::get(opCtx->getClient());
+
+    // If client metadata is not present in network requests, reset the in-memory metadata to be
+    // blank so that the wrong
+    // app name is not propagated.
     if (element.eoo()) {
+        auto client = opCtx->getClient();
+
+        if (clientMetadataIsMasterState._setViaMetadata && !client->isInDirectClient()) {
+            clientMetadataIsMasterState.setClientMetadata(client, boost::none, true);
+        }
+
         return Status::OK();
     }
 
@@ -86,10 +99,8 @@ Status ClientMetadataIsMasterState::readFromMetadata(OperationContext* opCtx,
         return swParseClientMetadata.getStatus();
     }
 
-    auto& clientMetadataIsMasterState = ClientMetadataIsMasterState::get(opCtx->getClient());
-
-    clientMetadataIsMasterState.setClientMetadata(opCtx->getClient(),
-                                                  std::move(swParseClientMetadata.getValue()));
+    clientMetadataIsMasterState.setClientMetadata(
+        opCtx->getClient(), std::move(swParseClientMetadata.getValue()), true);
 
     return Status::OK();
 }

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit c6d7aa58e66e19972a0908477e362fc7bd587774 [formerly 52c1fe02a0cd0f64a4d97f7c1e17792b38c67ca4])
Former-commit-id: 43fcf5b663829f9d5624b7089fbf813b42647e2c

src/mongo/rpc/metadata/client_metadata_ismaster.cpp
@@ -65,17 +65,30 @@ const boost::optional<ClientMetadata>& ClientMetadataIsMasterState::getClientMet
     return _clientMetadata;
 }
 
-void ClientMetadataIsMasterState::setClientMetadata(
-    Client* client, boost::optional<ClientMetadata> clientMetadata) {
+void ClientMetadataIsMasterState::setClientMetadata(Client* client,
+                                                    boost::optional<ClientMetadata> clientMetadata,
+                                                    bool setViaMetadata) {
     auto& state = get(client);
 
     stdx::lock_guard<Client> lk(*client);
     state._clientMetadata = std::move(clientMetadata);
+    state._setViaMetadata = setViaMetadata;
 }
 
 
 Status ClientMetadataIsMasterState::readFromMetadata(OperationContext* txn, BSONElement& element) {
+    auto& clientMetadataIsMasterState = ClientMetadataIsMasterState::get(txn->getClient());
+
+    // If client metadata is not present in network requests, reset the in-memory metadata to be
+    // blank so that the wrong
+    // app name is not propagated.
     if (element.eoo()) {
+        auto client = txn->getClient();
+
+        if (clientMetadataIsMasterState._setViaMetadata && !client->isInDirectClient()) {
+            clientMetadataIsMasterState.setClientMetadata(client, boost::none, true);
+        }
+
         return Status::OK();
     }
 
@@ -85,10 +98,8 @@ Status ClientMetadataIsMasterState::readFromMetadata(OperationContext* txn, BSON
         return swParseClientMetadata.getStatus();
     }
 
-    auto& clientMetadataIsMasterState = ClientMetadataIsMasterState::get(txn->getClient());
-
-    clientMetadataIsMasterState.setClientMetadata(txn->getClient(),
-                                                  std::move(swParseClientMetadata.getValue()));
+    clientMetadataIsMasterState.setClientMetadata(
+        txn->getClient(), std::move(swParseClientMetadata.getValue()), true);
 
     return Status::OK();
 }