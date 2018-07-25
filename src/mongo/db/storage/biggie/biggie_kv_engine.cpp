Icantjuddle/mongo

SERVER-35532 Set up record store and pass all unit tests.

src/mongo/db/storage/biggie/biggie_kv_engine.cpp
@@ -26,23 +26,61 @@
  *    it in the license file.
  */
 
+#define MONGO_LOG_DEFAULT_COMPONENT ::mongo::logger::LogComponent::kStorage
+
+#include "mongo/db/storage/biggie/biggie_kv_engine.h"
+
 #include "mongo/base/disallow_copying.h"
 #include "mongo/db/snapshot_window_options.h"
-#include "mongo/db/storage/biggie/biggie_kv_engine.h"
+#include "mongo/db/storage/biggie/biggie_recovery_unit.h"
 #include "mongo/db/storage/record_store.h"
 #include "mongo/db/storage/sorted_data_interface.h"
 #include "mongo/platform/basic.h"
 #include "mongo/stdx/memory.h"
+#include "mongo/util/log.h"
+
 
 namespace mongo {
+namespace biggie {
 
-bool BiggieKVEngine::isCacheUnderPressure(OperationContext* opCtx) const {
-    return false; // ! might be under cache pressure eventually
-}
-void BiggieKVEngine::setCachePressureForTest(int pressure) {
-    // ! implement
+mongo::RecoveryUnit* KVEngine::newRecoveryUnit() {
+    return new RecoveryUnit(this, nullptr);
 }
 
+void KVEngine::setCachePressureForTest(int pressure) {
+    // TODO : implement.
+}
+
+Status KVEngine::createRecordStore(OperationContext* opCtx,
+                                   StringData ns,
+                                   StringData ident,
+                                   const CollectionOptions& options) {
+    log() << "Creating Ident in KVEngine with ident: " << ident;
+    _idents.insert(ident);
+    return Status::OK();
+}
+
+std::unique_ptr<::mongo::RecordStore> KVEngine::getRecordStore(OperationContext* opCtx,
+                                                               StringData ns,
+                                                               StringData ident,
+                                                               const CollectionOptions& options) {
+    // TODO: deal with options.
+    return std::make_unique<RecordStore>(ns, ident);
+}
+
+void KVEngine::setMaster_inlock(std::unique_ptr<StringStore> newMaster) {
+    _master.reset(newMaster.release());
+}
+
+std::shared_ptr<StringStore> KVEngine::getMaster() const {
+    // TODO : later on this needs to be changed to use their copy function.
+    std::lock_guard<std::mutex> lk(_masterLock);
+    return _master;
+}
+
+std::shared_ptr<StringStore> KVEngine::getMaster_inlock() const {
+    return _master;
+}
 
 class EmptyRecordCursor final : public SeekableRecordCursor {
 public:
@@ -57,6 +95,7 @@ public:
         return true;
     }
     void detachFromOperationContext() final {}
-    void reattachToOperationContext(OperationContext* opCtx) final {}    
+    void reattachToOperationContext(OperationContext* opCtx) final {}
 };
+}  // namespace biggie
 }  // namespace mongo

src/mongo/db/storage/biggie/biggie_kv_engine.h
@@ -29,48 +29,41 @@
 #pragma once
 
 #include <memory>
+#include <mutex>
+#include <set>
 
-#include "mongo/db/storage/kv/kv_engine.h"
-#include "mongo/db/storage/recovery_unit_noop.h"
 #include "mongo/db/storage/biggie/biggie_record_store.h"
 #include "mongo/db/storage/biggie/biggie_sorted_impl.h"
+#include "mongo/db/storage/biggie/store.h"
+#include "mongo/db/storage/kv/kv_engine.h"
 
 namespace mongo {
-
+namespace biggie {
 class JournalListener;
-
 /**
  * The biggie storage engine is intended for unit and performance testing.
  */
-class BiggieKVEngine : public KVEngine {
-    std::shared_ptr<BiggieStore> _store; //ALL our data
+class KVEngine : public ::mongo::KVEngine {
+    std::shared_ptr<StringStore> _master = std::make_shared<StringStore>();
+    std::set<StringData> _idents;  // TODO : replace with a query to _master.
+    mutable std::mutex _masterLock;
+
 public:
+    KVEngine() : ::mongo::KVEngine() {}
 
-    BiggieKVEngine() : KVEngine() {}
-   
-    virtual ~BiggieKVEngine() {}
+    virtual ~KVEngine() {}
 
-    virtual RecoveryUnit* newRecoveryUnit() {
-        return new RecoveryUnitNoop();
-    }
+    virtual mongo::RecoveryUnit* newRecoveryUnit();
 
     virtual Status createRecordStore(OperationContext* opCtx,
                                      StringData ns,
                                      StringData ident,
-                                     const CollectionOptions& options) {
-        // TODO: implement
-        return Status::OK();
-    }
+                                     const CollectionOptions& options);
 
-    virtual std::unique_ptr<RecordStore> getRecordStore(OperationContext* opCtx,
-                                                        StringData ns,
-                                                        StringData ident,
-                                                        const CollectionOptions& options) {
-        // The engine passes it a new RecordStore based on the single instance
-        // of the underlying data structure
-        // why is this a unique pointer
-        return std::make_unique<BiggieRecordStore>(ns, _store); // TODO deal with ident, options
-    }
+    virtual std::unique_ptr<::mongo::RecordStore> getRecordStore(OperationContext* opCtx,
+                                                                 StringData ns,
+                                                                 StringData ident,
+                                                                 const CollectionOptions& options);
 
     virtual Status createSortedDataInterface(OperationContext* opCtx,
                                              StringData ident,
@@ -78,10 +71,10 @@ public:
         return Status::OK();
     }
 
-    virtual SortedDataInterface* getSortedDataInterface(OperationContext* opCtx,
-                                                        StringData ident,
-                                                        const IndexDescriptor* desc) {
-        return new BiggieSortedImpl(); // TODO : implement later                                                            
+    virtual mongo::SortedDataInterface* getSortedDataInterface(OperationContext* opCtx,
+                                                               StringData ident,
+                                                               const IndexDescriptor* desc) {
+        return new SortedDataInterface();  // TODO : implement later.
     }
 
     virtual Status dropIdent(OperationContext* opCtx, StringData ident) {
@@ -89,19 +82,19 @@ public:
     }
 
     virtual bool supportsDocLocking() const {
-        return true;
+        return false;  // TODO : do this later.
     }
 
     virtual bool supportsDirectoryPerDB() const {
-        return false;
+        return false;  // TODO : do this later.
     }
 
     virtual bool supportsCappedCollections() const {
-        return false;
+        return false;  // TODO : do this later.
     }
 
     /**
-     * biggie does no journaling, so don't report the engine as durable.
+     * Biggie does not write to disk.
      */
     virtual bool isDurable() const {
         return false;
@@ -111,12 +104,14 @@ public:
         return true;
     }
 
-    virtual bool isCacheUnderPressure(OperationContext* opCtx) const override;
+    virtual bool isCacheUnderPressure(OperationContext* opCtx) const override {
+        return false;
+    }
 
     virtual void setCachePressureForTest(int pressure) override;
 
     virtual int64_t getIdentSize(OperationContext* opCtx, StringData ident) {
-        return 1;
+        return 1;  // TODO : implement.
     }
 
     virtual Status repairIdent(OperationContext* opCtx, StringData ident) {
@@ -133,10 +128,28 @@ public:
 
     virtual void cleanShutdown(){};
 
-    void setJournalListener(JournalListener* jl) final {}
+    void setJournalListener(mongo::JournalListener* jl) final {}
 
     virtual Timestamp getAllCommittedTimestamp() const override {
         return Timestamp();
+    }
+
+    // Biggie Specific
+
+    /**
+     * Used to replace the master branch of the store with an updated copy.
+     * Appropriate lock must be taken externally.
+     */
+    // TODO: should possibly check store version numbers before setting.
+    void setMaster_inlock(std::unique_ptr<StringStore> newMaster);
+
+    std::shared_ptr<StringStore> getMaster() const;
+    std::shared_ptr<StringStore> getMaster_inlock() const;
+    /**
+     * Get the lock around the master branch.
+     */
+    std::mutex& getMasterLock() {
+        return _masterLock;
     }
 
 private:
@@ -146,4 +159,5 @@ private:
 
     BSONObj _dummy;
 };
-}
+}  // namespace biggie
+}  // namespace mongo

src/mongo/db/storage/biggie/biggie_recovery_unit.cpp
@@ -1,0 +1,123 @@
+// biggie_recovery_unit.cpp
+
+/**
+*    Copyright (C) 2014 MongoDB Inc.
+*
+*    This program is free software: you can redistribute it and/or  modify
+*    it under the terms of the GNU Affero General Public License, version 3,
+*    as published by the Free Software Foundation.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU Affero General Public License for more details.
+*
+*    You should have received a copy of the GNU Affero General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*    As a special exception, the copyright holders give permission to link the
+*    code of portions of this program with the OpenSSL library under certain
+*    conditions as described in each individual source file and distribute
+*    linked combinations including the program with the OpenSSL library. You
+*    must comply with the GNU Affero General Public License in all respects for
+*    all of the code used other than as permitted herein. If you modify file(s)
+*    with this exception, you may extend this exception to your version of the
+*    file(s), but you are not obligated to do so. If you do not wish to do so,
+*    delete this exception statement from your version. If you delete this
+*    exception statement from all source files in the program, then also delete
+*    it in the license file.
+*/
+
+#define MONGO_LOG_DEFAULT_COMPONENT ::mongo::logger::LogComponent::kStorage
+
+#include "mongo/db/storage/biggie/biggie_recovery_unit.h"
+
+#include <mutex>
+
+#include "mongo/db/concurrency/write_conflict_exception.h"
+#include "mongo/util/log.h"
+
+namespace mongo {
+namespace biggie {
+
+RecoveryUnit::RecoveryUnit(KVEngine* parentKVEngine, stdx::function<void()> cb)
+    : _waitUntilDurableCallback(cb), _KVEngine(parentKVEngine) {}
+
+void RecoveryUnit::beginUnitOfWork(OperationContext* opCtx) {}
+
+void RecoveryUnit::commitUnitOfWork() {
+    while (true) {
+        std::shared_ptr<StringStore> master = _KVEngine->getMaster();
+        try {
+            _workingCopy->merge3(*_mergeBase, *master);
+        } catch (const merge_conflict_exception&) {
+            throw WriteConflictException();
+        }
+        std::lock_guard<std::mutex> lkOnMaster(_KVEngine->getMasterLock());
+        if (_KVEngine->getMaster_inlock() == master) {
+            _KVEngine->setMaster_inlock(std::move(_workingCopy));
+            _mergeBase.reset();
+            break;
+        }
+    }
+    try {
+        for (Changes::iterator it = _changes.begin(), end = _changes.end(); it != end; ++it) {
+            (*it)->commit(boost::none);
+        }
+        _changes.clear();
+    } catch (...) {
+        std::terminate();
+    }
+}
+
+void RecoveryUnit::abortUnitOfWork() {
+    _workingCopy.reset();
+    _mergeBase.reset();
+    try {
+        for (Changes::reverse_iterator it = _changes.rbegin(), end = _changes.rend(); it != end;
+             ++it) {
+            ChangePtr change = *it;
+            LOG(2) << "CUSTOM ROLLBACK " << demangleName(typeid(*change));
+            change->rollback();
+        }
+        _changes.clear();
+    } catch (...) {
+        std::terminate();
+    }
+}
+
+bool RecoveryUnit::waitUntilDurable() {
+    return true;  // This is an in-memory storage engine.
+}
+
+void RecoveryUnit::abandonSnapshot() {
+    _mergeBase.reset();
+    _workingCopy.reset();
+    // TODO : check if we need to add something later.
+}
+
+void RecoveryUnit::registerChange(Change* change) {
+    _changes.push_back(ChangePtr(change));
+}
+
+SnapshotId RecoveryUnit::getSnapshotId() const {
+    return SnapshotId();
+}
+
+bool RecoveryUnit::forkIfNeeded() {
+    // _workingCopy and _mergeBase either both exist or both don't.
+    invariant((_workingCopy && _mergeBase) || (!_workingCopy && !_mergeBase));
+    if (_mergeBase) {
+        return false;
+    }
+    // TODO : later on this needs to be changed to use their copy function.
+    _mergeBase = _KVEngine->getMaster();
+    // TODO : later on this needs to be changed to use their copy function.
+    _workingCopy = std::make_unique<StringStore>(*_mergeBase);
+    return true;
+}
+
+void RecoveryUnit::setOrderedCommit(bool orderedCommit) {}
+
+}  // namespace biggie
+}  // namespace mongo

zaorangyang/percona-server-mongodb-annotation

Former-commit-id: 1b649cd11a8cd4c3c4fe13f4063a07fd6d7d393e

src/mongo/db/storage/biggie/biggie_kv_engine.cpp
@@ -26,23 +26,61 @@
  *    it in the license file.
  */
 
+#define MONGO_LOG_DEFAULT_COMPONENT ::mongo::logger::LogComponent::kStorage
+
+#include "mongo/db/storage/biggie/biggie_kv_engine.h"
+
 #include "mongo/base/disallow_copying.h"
 #include "mongo/db/snapshot_window_options.h"
-#include "mongo/db/storage/biggie/biggie_kv_engine.h"
+#include "mongo/db/storage/biggie/biggie_recovery_unit.h"
 #include "mongo/db/storage/record_store.h"
 #include "mongo/db/storage/sorted_data_interface.h"
 #include "mongo/platform/basic.h"
 #include "mongo/stdx/memory.h"
+#include "mongo/util/log.h"
+
 
 namespace mongo {
+namespace biggie {
 
-bool BiggieKVEngine::isCacheUnderPressure(OperationContext* opCtx) const {
-    return false; // ! might be under cache pressure eventually
-}
-void BiggieKVEngine::setCachePressureForTest(int pressure) {
-    // ! implement
+mongo::RecoveryUnit* KVEngine::newRecoveryUnit() {
+    return new RecoveryUnit(this, nullptr);
 }
 
+void KVEngine::setCachePressureForTest(int pressure) {
+    // TODO : implement.
+}
+
+Status KVEngine::createRecordStore(OperationContext* opCtx,
+                                   StringData ns,
+                                   StringData ident,
+                                   const CollectionOptions& options) {
+    log() << "Creating Ident in KVEngine with ident: " << ident;
+    _idents.insert(ident);
+    return Status::OK();
+}
+
+std::unique_ptr<::mongo::RecordStore> KVEngine::getRecordStore(OperationContext* opCtx,
+                                                               StringData ns,
+                                                               StringData ident,
+                                                               const CollectionOptions& options) {
+    // TODO: deal with options.
+    return std::make_unique<RecordStore>(ns, ident);
+}
+
+void KVEngine::setMaster_inlock(std::unique_ptr<StringStore> newMaster) {
+    _master.reset(newMaster.release());
+}
+
+std::shared_ptr<StringStore> KVEngine::getMaster() const {
+    // TODO : later on this needs to be changed to use their copy function.
+    stdx::lock_guard<stdx::mutex> lk(_masterLock);
+    return _master;
+}
+
+std::shared_ptr<StringStore> KVEngine::getMaster_inlock() const {
+    return _master;
+}
 
 class EmptyRecordCursor final : public SeekableRecordCursor {
 public:
@@ -57,6 +95,7 @@ public:
         return true;
     }
     void detachFromOperationContext() final {}
-    void reattachToOperationContext(OperationContext* opCtx) final {}    
+    void reattachToOperationContext(OperationContext* opCtx) final {}
 };
+}  // namespace biggie
 }  // namespace mongo

src/mongo/db/storage/biggie/biggie_kv_engine.h
@@ -29,48 +29,41 @@
 #pragma once
 
 #include <memory>
+#include <mutex>
+#include <set>
 
-#include "mongo/db/storage/kv/kv_engine.h"
-#include "mongo/db/storage/recovery_unit_noop.h"
 #include "mongo/db/storage/biggie/biggie_record_store.h"
 #include "mongo/db/storage/biggie/biggie_sorted_impl.h"
+#include "mongo/db/storage/biggie/store.h"
+#include "mongo/db/storage/kv/kv_engine.h"
 
 namespace mongo {
-
+namespace biggie {
 class JournalListener;
-
 /**
  * The biggie storage engine is intended for unit and performance testing.
  */
-class BiggieKVEngine : public KVEngine {
-    std::shared_ptr<BiggieStore> _store; //ALL our data
+class KVEngine : public ::mongo::KVEngine {
+    std::shared_ptr<StringStore> _master = std::make_shared<StringStore>();
+    std::set<StringData> _idents;  // TODO : replace with a query to _master.
+    mutable stdx::mutex _masterLock;
+
 public:
+    KVEngine() : ::mongo::KVEngine() {}
 
-    BiggieKVEngine() : KVEngine() {}
-   
-    virtual ~BiggieKVEngine() {}
+    virtual ~KVEngine() {}
 
-    virtual RecoveryUnit* newRecoveryUnit() {
-        return new RecoveryUnitNoop();
-    }
+    virtual mongo::RecoveryUnit* newRecoveryUnit();
 
     virtual Status createRecordStore(OperationContext* opCtx,
                                      StringData ns,
                                      StringData ident,
-                                     const CollectionOptions& options) {
-        // TODO: implement
-        return Status::OK();
-    }
+                                     const CollectionOptions& options);
 
-    virtual std::unique_ptr<RecordStore> getRecordStore(OperationContext* opCtx,
-                                                        StringData ns,
-                                                        StringData ident,
-                                                        const CollectionOptions& options) {
-        // The engine passes it a new RecordStore based on the single instance
-        // of the underlying data structure
-        // why is this a unique pointer
-        return std::make_unique<BiggieRecordStore>(ns, _store); // TODO deal with ident, options
-    }
+    virtual std::unique_ptr<::mongo::RecordStore> getRecordStore(OperationContext* opCtx,
+                                                                 StringData ns,
+                                                                 StringData ident,
+                                                                 const CollectionOptions& options);
 
     virtual Status createSortedDataInterface(OperationContext* opCtx,
                                              StringData ident,
@@ -78,10 +71,10 @@ public:
         return Status::OK();
     }
 
-    virtual SortedDataInterface* getSortedDataInterface(OperationContext* opCtx,
-                                                        StringData ident,
-                                                        const IndexDescriptor* desc) {
-        return new BiggieSortedImpl(); // TODO : implement later                                                            
+    virtual mongo::SortedDataInterface* getSortedDataInterface(OperationContext* opCtx,
+                                                               StringData ident,
+                                                               const IndexDescriptor* desc) {
+        return new SortedDataInterface();  // TODO : implement later.
     }
 
     virtual Status dropIdent(OperationContext* opCtx, StringData ident) {
@@ -89,19 +82,19 @@ public:
     }
 
     virtual bool supportsDocLocking() const {
-        return true;
+        return false;  // TODO : do this later.
     }
 
     virtual bool supportsDirectoryPerDB() const {
-        return false;
+        return false;  // TODO : do this later.
     }
 
     virtual bool supportsCappedCollections() const {
-        return false;
+        return false;  // TODO : do this later.
     }
 
     /**
-     * biggie does no journaling, so don't report the engine as durable.
+     * Biggie does not write to disk.
      */
     virtual bool isDurable() const {
         return false;
@@ -111,12 +104,14 @@ public:
         return true;
     }
 
-    virtual bool isCacheUnderPressure(OperationContext* opCtx) const override;
+    virtual bool isCacheUnderPressure(OperationContext* opCtx) const override {
+        return false;
+    }
 
     virtual void setCachePressureForTest(int pressure) override;
 
     virtual int64_t getIdentSize(OperationContext* opCtx, StringData ident) {
-        return 1;
+        return 1;  // TODO : implement.
     }
 
     virtual Status repairIdent(OperationContext* opCtx, StringData ident) {
@@ -133,10 +128,28 @@ public:
 
     virtual void cleanShutdown(){};
 
-    void setJournalListener(JournalListener* jl) final {}
+    void setJournalListener(mongo::JournalListener* jl) final {}
 
     virtual Timestamp getAllCommittedTimestamp() const override {
         return Timestamp();
+    }
+
+    // Biggie Specific
+
+    /**
+     * Used to replace the master branch of the store with an updated copy.
+     * Appropriate lock must be taken externally.
+     */
+    // TODO: should possibly check store version numbers before setting.
+    void setMaster_inlock(std::unique_ptr<StringStore> newMaster);
+
+    std::shared_ptr<StringStore> getMaster() const;
+    std::shared_ptr<StringStore> getMaster_inlock() const;
+    /**
+     * Get the lock around the master branch.
+     */
+    stdx::mutex& getMasterLock() {
+        return _masterLock;
     }
 
 private:
@@ -146,4 +159,5 @@ private:
 
     BSONObj _dummy;
 };
-}
+}  // namespace biggie
+}  // namespace mongo

src/mongo/db/storage/biggie/biggie_recovery_unit.cpp
@@ -1,0 +1,123 @@
+// biggie_recovery_unit.cpp
+
+/**
+*    Copyright (C) 2014 MongoDB Inc.
+*
+*    This program is free software: you can redistribute it and/or  modify
+*    it under the terms of the GNU Affero General Public License, version 3,
+*    as published by the Free Software Foundation.
+*
+*    This program is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU Affero General Public License for more details.
+*
+*    You should have received a copy of the GNU Affero General Public License
+*    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*    As a special exception, the copyright holders give permission to link the
+*    code of portions of this program with the OpenSSL library under certain
+*    conditions as described in each individual source file and distribute
+*    linked combinations including the program with the OpenSSL library. You
+*    must comply with the GNU Affero General Public License in all respects for
+*    all of the code used other than as permitted herein. If you modify file(s)
+*    with this exception, you may extend this exception to your version of the
+*    file(s), but you are not obligated to do so. If you do not wish to do so,
+*    delete this exception statement from your version. If you delete this
+*    exception statement from all source files in the program, then also delete
+*    it in the license file.
+*/
+
+#define MONGO_LOG_DEFAULT_COMPONENT ::mongo::logger::LogComponent::kStorage
+
+#include "mongo/db/storage/biggie/biggie_recovery_unit.h"
+
+#include <mutex>
+
+#include "mongo/db/concurrency/write_conflict_exception.h"
+#include "mongo/util/log.h"
+
+namespace mongo {
+namespace biggie {
+
+RecoveryUnit::RecoveryUnit(KVEngine* parentKVEngine, stdx::function<void()> cb)
+    : _waitUntilDurableCallback(cb), _KVEngine(parentKVEngine) {}
+
+void RecoveryUnit::beginUnitOfWork(OperationContext* opCtx) {}
+
+void RecoveryUnit::commitUnitOfWork() {
+    while (true) {
+        std::shared_ptr<StringStore> master = _KVEngine->getMaster();
+        try {
+            _workingCopy->merge3(*_mergeBase, *master);
+        } catch (const merge_conflict_exception&) {
+            throw WriteConflictException();
+        }
+        stdx::lock_guard<stdx::mutex> lkOnMaster(_KVEngine->getMasterLock());
+        if (_KVEngine->getMaster_inlock() == master) {
+            _KVEngine->setMaster_inlock(std::move(_workingCopy));
+            _mergeBase.reset();
+            break;
+        }
+    }
+    try {
+        for (Changes::iterator it = _changes.begin(), end = _changes.end(); it != end; ++it) {
+            (*it)->commit(boost::none);
+        }
+        _changes.clear();
+    } catch (...) {
+        std::terminate();
+    }
+}
+
+void RecoveryUnit::abortUnitOfWork() {
+    _workingCopy.reset();
+    _mergeBase.reset();
+    try {
+        for (Changes::reverse_iterator it = _changes.rbegin(), end = _changes.rend(); it != end;
+             ++it) {
+            ChangePtr change = *it;
+            LOG(2) << "CUSTOM ROLLBACK " << demangleName(typeid(*change));
+            change->rollback();
+        }
+        _changes.clear();
+    } catch (...) {
+        std::terminate();
+    }
+}
+
+bool RecoveryUnit::waitUntilDurable() {
+    return true;  // This is an in-memory storage engine.
+}
+
+void RecoveryUnit::abandonSnapshot() {
+    _mergeBase.reset();
+    _workingCopy.reset();
+    // TODO : check if we need to add something later.
+}
+
+void RecoveryUnit::registerChange(Change* change) {
+    _changes.push_back(ChangePtr(change));
+}
+
+SnapshotId RecoveryUnit::getSnapshotId() const {
+    return SnapshotId();
+}
+
+bool RecoveryUnit::forkIfNeeded() {
+    // _workingCopy and _mergeBase either both exist or both don't.
+    invariant((_workingCopy && _mergeBase) || (!_workingCopy && !_mergeBase));
+    if (_mergeBase) {
+        return false;
+    }
+    // TODO : later on this needs to be changed to use their copy function.
+    _mergeBase = _KVEngine->getMaster();
+    // TODO : later on this needs to be changed to use their copy function.
+    _workingCopy = std::make_unique<StringStore>(*_mergeBase);
+    return true;
+}
+
+void RecoveryUnit::setOrderedCommit(bool orderedCommit) {}
+
+}  // namespace biggie
+}  // namespace mongo