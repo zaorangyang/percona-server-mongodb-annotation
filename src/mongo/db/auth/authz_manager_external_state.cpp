xiexiaoy/eloqdoc

SERVER-34735 Extract structured data from X509 subject names

src/mongo/db/auth/authz_manager_external_state.cpp
@@ -28,6 +28,7 @@
 
 #include "mongo/platform/basic.h"
 
+#include "mongo/config.h"
 #include "mongo/db/auth/authz_manager_external_state.h"
 #include "mongo/db/auth/user_name.h"
 #include "mongo/db/operation_context.h"
@@ -42,11 +43,17 @@ AuthzManagerExternalState::~AuthzManagerExternalState() = default;
 
 bool AuthzManagerExternalState::shouldUseRolesFromConnection(OperationContext* opCtx,
                                                              const UserName& userName) {
-    if (!opCtx || !opCtx->getClient() || !opCtx->getClient()->session())
+#ifdef MONGO_CONFIG_SSL
+    if (!opCtx || !opCtx->getClient() || !opCtx->getClient()->session()) {
         return false;
+    }
+
     auto& sslPeerInfo = SSLPeerInfo::forSession(opCtx->getClient()->session());
-    return sslPeerInfo.subjectName == userName.getUser() && userName.getDB() == "$external" &&
-        !sslPeerInfo.roles.empty();
+    return sslPeerInfo.subjectName.toString() == userName.getUser() &&
+        userName.getDB() == "$external" && !sslPeerInfo.roles.empty();
+#else
+    return false;
+#endif
 }
 
 

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit edf6925547e7f4e6d5fabb0153fa6aa054ea926f [formerly fa3fae04c144048bebf3f4c58e08b7b0c32743e1])
Former-commit-id: b48a9b555693e6f74ee9214779d78ad52929140a

src/mongo/db/auth/authz_manager_external_state.cpp
@@ -28,6 +28,7 @@
 
 #include "mongo/platform/basic.h"
 
+#include "mongo/config.h"
 #include "mongo/db/auth/authz_manager_external_state.h"
 #include "mongo/db/auth/user_name.h"
 #include "mongo/db/operation_context.h"
@@ -42,10 +43,17 @@ AuthzManagerExternalState::~AuthzManagerExternalState() = default;
 
 bool AuthzManagerExternalState::shouldUseRolesFromConnection(OperationContext* txn,
                                                              const UserName& userName) {
-    return txn && txn->getClient() && txn->getClient()->session() &&
-        txn->getClient()->session()->getX509PeerInfo().subjectName == userName.getUser() &&
-        userName.getDB() == "$external" &&
-        !txn->getClient()->session()->getX509PeerInfo().roles.empty();
+#ifdef MONGO_CONFIG_SSL
+    if (!txn || !txn->getClient() || !txn->getClient()->session()) {
+        return false;
+    }
+
+    auto sslPeerInfo = txn->getClient()->session()->getX509PeerInfo();
+    return sslPeerInfo.subjectName.toString() == userName.getUser() &&
+        userName.getDB() == "$external" && !sslPeerInfo.roles.empty();
+#else
+    return false;
+#endif
 }
 
 