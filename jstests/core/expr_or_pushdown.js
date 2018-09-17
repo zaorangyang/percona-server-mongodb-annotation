mongodb/mongo

SERVER-36993 Fix crash due to incorrect $or pushdown for indexed $expr.

jstests/core/expr_or_pushdown.js
@@ -1,0 +1,25 @@
+/**
+ * Test that an $expr predicated which is eligible for being indexed with an $or pushdown executes
+ * as expected.
+ */
+(function() {
+    "use strict";
+
+    const coll = db.expr_or_pushdown;
+    coll.drop();
+    assert.commandWorked(coll.createIndex({"a": 1, "b": 1}));
+    assert.commandWorked(coll.insert({_id: 0, a: "a", b: "b", d: "d"}));
+    assert.commandWorked(coll.insert({_id: 1, a: "a", b: "c", d: "d"}));
+    assert.commandWorked(coll.insert({_id: 2, a: "a", b: "x", d: "d"}));
+    assert.commandWorked(coll.insert({_id: 3, a: "x", b: "b", d: "d"}));
+    assert.commandWorked(coll.insert({_id: 4, a: "a", b: "b", d: "x"}));
+
+    const results = coll.find({
+                            $expr: {$and: [{$eq: ["$d", "d"]}, {$eq: ["$a", "a"]}]},
+                            $or: [{"b": "b"}, {"b": "c"}]
+                        })
+                        .sort({_id: 1})
+                        .toArray();
+
+    assert.eq(results, [{_id: 0, a: "a", b: "b", d: "d"}, {_id: 1, a: "a", b: "c", d: "d"}]);
+}());

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit 05fcb6f580dbc94e48b9a537f5179b01078376e1 [formerly 0aebf209b1467df188b8915d507fc6a2dcf80ef8])
Former-commit-id: 2b4634bb6512c5345de2ab8f698a687c6cec9973

jstests/core/expr_or_pushdown.js
@@ -1,0 +1,25 @@
+/**
+ * Test that an $expr predicated which is eligible for being indexed with an $or pushdown executes
+ * as expected.
+ */
+(function() {
+    "use strict";
+
+    const coll = db.expr_or_pushdown;
+    coll.drop();
+    assert.commandWorked(coll.createIndex({"a": 1, "b": 1}));
+    assert.writeOK(coll.insert({_id: 0, a: "a", b: "b", d: "d"}));
+    assert.writeOK(coll.insert({_id: 1, a: "a", b: "c", d: "d"}));
+    assert.writeOK(coll.insert({_id: 2, a: "a", b: "x", d: "d"}));
+    assert.writeOK(coll.insert({_id: 3, a: "x", b: "b", d: "d"}));
+    assert.writeOK(coll.insert({_id: 4, a: "a", b: "b", d: "x"}));
+
+    const results = coll.find({
+                            $expr: {$and: [{$eq: ["$d", "d"]}, {$eq: ["$a", "a"]}]},
+                            $or: [{"b": "b"}, {"b": "c"}]
+                        })
+                        .sort({_id: 1})
+                        .toArray();
+
+    assert.eq(results, [{_id: 0, a: "a", b: "b", d: "d"}, {_id: 1, a: "a", b: "c", d: "d"}]);
+}());