xiexiaoy/eloqdoc

SERVER-34868 Prohibit legacy queries from running on sessions in shell

jstests/noPassthrough/exhaust_option_disallowed_in_session.js
@@ -1,0 +1,32 @@
+/**
+ * Make sure the 'exhaust' query option is not able to be used in a session.
+ */
+(function() {
+    "use strict";
+
+    let conn = MongoRunner.runMongod();
+
+    const dbName = 'test';
+    const collName = 'coll';
+
+    const session = conn.startSession();
+    const sessionColl = session.getDatabase(dbName).getCollection(collName);
+    const testColl = conn.getDB(dbName).getCollection(collName);
+
+    testColl.drop();
+
+    // Create a collection to query.
+    assert.commandWorked(testColl.insert({_id: 1}));
+
+    // Exhaust outside of session should work.
+    let docs = testColl.find().addOption(DBQuery.Option.exhaust).toArray();
+    assert.docEq([{_id: 1}], docs);
+
+    // Exhaust in session should fail.
+    assert.throws(() => {
+        sessionColl.find().addOption(DBQuery.Option.exhaust).toArray();
+    });
+
+    session.endSession();
+    MongoRunner.stopMongod(conn);
+}());

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit f6c19d852a7f6f2d2aa12882cb4fc8a93de25e13 [formerly b188d762b8920f791906c80570f770dd9fc09731])
Former-commit-id: 241515814f8bd5dbc7ad3a0682dd531a965537cb

jstests/noPassthrough/exhaust_option_disallowed_in_session.js
@@ -1,0 +1,32 @@
+/**
+ * Make sure the 'exhaust' query option is not able to be used in a session.
+ */
+(function() {
+    "use strict";
+
+    let conn = MongoRunner.runMongod();
+
+    const dbName = 'test';
+    const collName = 'coll';
+
+    const session = conn.startSession();
+    const sessionColl = session.getDatabase(dbName).getCollection(collName);
+    const testColl = conn.getDB(dbName).getCollection(collName);
+
+    testColl.drop();
+
+    // Create a collection to query.
+    assert.writeOK(testColl.insert({_id: 1}));
+
+    // Exhaust outside of session should work.
+    let docs = testColl.find().addOption(DBQuery.Option.exhaust).toArray();
+    assert.docEq([{_id: 1}], docs);
+
+    // Exhaust in session should fail.
+    assert.throws(() => {
+        sessionColl.find().addOption(DBQuery.Option.exhaust).toArray();
+    });
+
+    session.endSession();
+    MongoRunner.stopMongod(conn);
+}());