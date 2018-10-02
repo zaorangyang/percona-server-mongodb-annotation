mongodb/mongo

SERVER-26854 LockStats for sub-operations should not include wait time for previous operations

jstests/noPassthrough/lock_stats_suboperation_curop.js
@@ -1,0 +1,80 @@
+/* This test checks that currentOp correctly reports the lockStats for sub-operations. Before
+ * SERVER-26854, the currentOp entry for each sub-operation reports the aggregate lock wait time of
+ * all preceding sub-operations.
+ *
+ * To test the correctness of currentOp entry for sub-operations, we use an aggregation pipeline
+ * which can trigger a series of sub-operations: createCollection, createIndex, listCollections,
+ * listIndexes, renameCollection and dropCollection.
+ *
+ * Test steps:
+ *  1. Lock the database by FsyncLock.
+ *  2. Turn 'hangAfterIndexBuild' failpoint on.
+ *  3. Run the aggregation, which will get blocked on sub-operation: {'create' : 'tmp.agg_out.x'}.
+ *  4. Sleep for 2 seconds.
+ *  5. FsyncUnlock. Now the next sub-operation createIndex gets blocked by the failpoint.
+ *  6. Run 'currentOp' to check the entry for createIndex. The lock wait time should be 0 rather
+ *     than ~2.
+ *
+ * @tags: [requires_fsync]
+ */
+(function() {
+    'use strict';
+
+    const conn = MongoRunner.runMongod();
+    const db = conn.getDB('test');
+    const coll = db.books;
+    const blockedMillis = 2000;
+    assert.commandWorked(coll.insert({title: 'Adventures of Huckleberry'}));
+    assert.commandWorked(coll.insert({title: '1984'}));
+    assert.commandWorked(coll.insert({title: 'Animal Farm'}));
+    // Create the output collection beforehand so that $out will execute a code path which triggers
+    // the index creation sub-operation.
+    db['favorite'].createIndex({foo: 1});
+
+    db.setProfilingLevel(0, -1);
+
+    // Lock the database, and then start an operation that needs the lock, so it blocks.
+    assert.commandWorked(db.fsyncLock());
+
+    // Turn 'hangAfterStartingIndexBuildUnlocked' failpoint on, which blocks any index builds.
+    assert.commandWorked(
+        db.adminCommand({configureFailPoint: 'hangAfterStartingIndexBuild', mode: 'alwaysOn'}));
+
+    // Aggregation with $out which will block on creating the temporary collection due to the
+    // FsyncLock.
+    const dollarOutAggregationShell = startParallelShell(function() {
+        // Simple aggregation which copies a document to the output collection.
+        assert.commandWorked(db.runCommand({
+            aggregate: 'books',
+            pipeline: [{$match: {title: '1984'}}, {$out: 'favorite'}],
+            cursor: {}
+        }));
+    }, conn.port);
+
+    // Wait for sub-operation createCollection to get blocked.
+    assert.soon(function() {
+        let res = db.currentOp({"command.create": {$exists: true}, waitingForLock: true});
+        return res.inprog.length == 1;
+    });
+
+    sleep(blockedMillis);
+
+    // Unlock the database. Sub-operation createCollection can proceed.
+    db.fsyncUnlock();
+
+    // Wait for sub-operation createIndex to get blocked.
+    let res;
+    assert.soon(function() {
+        res = db.currentOp({"command.createIndexes": {$exists: true}});
+        return res.inprog.length == 1;
+    });
+    jsTestLog(tojson(res.inprog[0]));
+    // Assert that sub-operation 'createIndex' has 0 lock wait time. Before SERVER-26854, it
+    // erroneously reported ~2s as it counted the lock wait time for the previous sub-operation.
+    assert(!('timeAcquiringMicros' in res.inprog[0].lockStats.Global));
+
+    assert.commandWorked(
+        db.adminCommand({configureFailPoint: 'hangAfterStartingIndexBuild', mode: 'off'}));
+    dollarOutAggregationShell();
+    MongoRunner.stopMongod(conn);
+})();

jstests/noPassthrough/lock_stats_suboperation_logs.js
@@ -1,0 +1,89 @@
+/* This test checks that mongod correctly logs report the lockStats for sub-operations. Before
+ * SERVER-26854, the log for each sub-operation reported the aggregate lock wait time of all
+ * preceding sub-operations.
+ *
+ * To test the correctness of sub-operation logs, we use an aggregation pipeline which can
+ * trigger a series of sub-operations: createCollection, createIndex, listCollections, listIndexes,
+ * renameCollection and dropCollection.
+ *
+ * Test steps:
+ *  1. Lock the database by FsyncLock.
+ *  2. Run the aggregation, which will get blocked on sub-operation: {'create' : 'tmp.agg_out.x'}.
+ *  3. Sleep for 2 seconds.
+ *  4. FsyncUnlock.
+ *  5. Check the mongod logs: only that sub-operation (createCollection) and the 'parent' operation
+ * (aggregation) should log about the 2-second long wait for the lock.
+ *
+ * @tags: [requires_fsync]
+ */
+(function() {
+    'use strict';
+
+    const conn = MongoRunner.runMongod();
+    const db = conn.getDB('test');
+    const coll = db.books;
+    const blockedMillis = 2000;
+    assert.commandWorked(coll.insert({title: 'Adventures of Huckleberry'}));
+    assert.commandWorked(coll.insert({title: '1984'}));
+    assert.commandWorked(coll.insert({title: 'Animal Farm'}));
+    // The server will log every operation.
+    db.setProfilingLevel(0, -1);
+    // Create the output collection beforehand so that $out will execute a code path which triggers
+    // the index creation sub-operation.
+    db['favorite'].insert({foo: 1});
+
+    // Lock the database, and then start an operation that needs the lock, so it blocks.
+    assert.commandWorked(db.fsyncLock());
+
+    // Aggregation with $out which will block on creating the temporary collection due to the
+    // FsyncLock.
+    const dollarOutAggregationShell = startParallelShell(function() {
+        // Simple aggregation which copies a document to the output collection.
+        assert.commandWorked(db.runCommand({
+            aggregate: 'books',
+            pipeline: [{$match: {title: '1984'}}, {$out: 'favorite'}],
+            cursor: {}
+        }));
+    }, conn.port);
+
+    // Sub-operation createCollection starts to get blocked.
+    assert.soon(function() {
+        let res = db.currentOp({waitingForLock: true});
+        return res.inprog.length == 1;
+    });
+
+    sleep(blockedMillis);
+
+    clearRawMongoProgramOutput();
+    // Unlock the database. Sub-operation createCollection can proceed
+    // and so do all the following sub-operations.
+    db.fsyncUnlock();
+
+    dollarOutAggregationShell();
+    assert.eq(db['favorite'].count(), 1);
+
+    let mongodLogs = rawMongoProgramOutput();
+    let lines = mongodLogs.split('\n');
+    const lockWaitTimeRegex = /timeAcquiringMicros: { [wW]: ([0-9]+)/;
+    let match;
+    let supposedLockWaitTime;
+    let numWaitedForLocks = 0;
+
+    // Only the logs of 'parent' (aggregation with $out) and the first
+    // sub-operation(createCollection) have the information about the long wait for the lock.
+    for (let line of lines) {
+        if ((match = lockWaitTimeRegex.exec(line)) !== null) {
+            let lockWaitTime = match[1];
+            if (supposedLockWaitTime === undefined)
+                supposedLockWaitTime = lockWaitTime;
+            else
+                assert.eq(lockWaitTime, supposedLockWaitTime);  // Should be the same.
+            numWaitedForLocks++;
+            jsTestLog('Operation/Sub-operation log: ');
+            jsTestLog(line);
+        }
+    }
+    assert.eq(numWaitedForLocks, 2);
+
+    MongoRunner.stopMongod(conn);
+})();

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit 23da98593dfba216cd4f75260f059c04622e1718 [formerly b5ca922655a1d0a438c39c38b2c4434016069cb7])
Former-commit-id: d8402f63a60fa45bd24b43dfe6959b5af4905cc7

jstests/noPassthrough/lock_stats_suboperation_curop.js
@@ -1,0 +1,80 @@
+/* This test checks that currentOp correctly reports the lockStats for sub-operations. Before
+ * SERVER-26854, the currentOp entry for each sub-operation reports the aggregate lock wait time of
+ * all preceding sub-operations.
+ *
+ * To test the correctness of currentOp entry for sub-operations, we use an aggregation pipeline
+ * which can trigger a series of sub-operations: createCollection, createIndex, listCollections,
+ * listIndexes, renameCollection and dropCollection.
+ *
+ * Test steps:
+ *  1. Lock the database by FsyncLock.
+ *  2. Turn 'hangAfterIndexBuild' failpoint on.
+ *  3. Run the aggregation, which will get blocked on sub-operation: {'create' : 'tmp.agg_out.x'}.
+ *  4. Sleep for 2 seconds.
+ *  5. FsyncUnlock. Now the next sub-operation createIndex gets blocked by the failpoint.
+ *  6. Run 'currentOp' to check the entry for createIndex. The lock wait time should be 0 rather
+ *     than ~2.
+ *
+ * @tags: [requires_fsync]
+ */
+(function() {
+    'use strict';
+
+    const conn = MongoRunner.runMongod();
+    const db = conn.getDB('test');
+    const coll = db.books;
+    const blockedMillis = 2000;
+    assert.writeOK(coll.insert({title: 'Adventures of Huckleberry'}));
+    assert.writeOK(coll.insert({title: '1984'}));
+    assert.writeOK(coll.insert({title: 'Animal Farm'}));
+    // Create the output collection beforehand so that $out will execute a code path which triggers
+    // the index creation sub-operation.
+    db['favorite'].createIndex({foo: 1});
+
+    db.setProfilingLevel(0, -1);
+
+    // Lock the database, and then start an operation that needs the lock, so it blocks.
+    assert.commandWorked(db.fsyncLock());
+
+    // Turn 'hangAfterStartingIndexBuildUnlocked' failpoint on, which blocks any index builds.
+    assert.commandWorked(
+        db.adminCommand({configureFailPoint: 'hangAfterStartingIndexBuild', mode: 'alwaysOn'}));
+
+    // Aggregation with $out which will block on creating the temporary collection due to the
+    // FsyncLock.
+    const dollarOutAggregationShell = startParallelShell(function() {
+        // Simple aggregation which copies a document to the output collection.
+        assert.commandWorked(db.runCommand({
+            aggregate: 'books',
+            pipeline: [{$match: {title: '1984'}}, {$out: 'favorite'}],
+            cursor: {}
+        }));
+    }, conn.port);
+
+    // Wait for sub-operation createCollection to get blocked.
+    assert.soon(function() {
+        let res = db.currentOp({"command.create": {$exists: true}, waitingForLock: true});
+        return res.inprog.length == 1;
+    });
+
+    sleep(blockedMillis);
+
+    // Unlock the database. Sub-operation createCollection can proceed.
+    db.fsyncUnlock();
+
+    // Wait for sub-operation createIndex to get blocked.
+    let res;
+    assert.soon(function() {
+        res = db.currentOp({"msg": "Index Build"});
+        return res.inprog.length == 1;
+    });
+    jsTestLog(tojson(res.inprog[0]));
+    // Assert that sub-operation 'createIndex' has 0 lock wait time. Before SERVER-26854, it
+    // erroneously reported ~2s as it counted the lock wait time for the previous sub-operation.
+    assert(!('timeAcquiringMicros' in res.inprog[0].lockStats.Global));
+
+    assert.commandWorked(
+        db.adminCommand({configureFailPoint: 'hangAfterStartingIndexBuild', mode: 'off'}));
+    dollarOutAggregationShell();
+    MongoRunner.stopMongod(conn);
+})();

jstests/noPassthrough/lock_stats_suboperation_logs.js
@@ -1,0 +1,89 @@
+/* This test checks that mongod correctly logs report the lockStats for sub-operations. Before
+ * SERVER-26854, the log for each sub-operation reported the aggregate lock wait time of all
+ * preceding sub-operations.
+ *
+ * To test the correctness of sub-operation logs, we use an aggregation pipeline which can
+ * trigger a series of sub-operations: createCollection, createIndex, listCollections, listIndexes,
+ * renameCollection and dropCollection.
+ *
+ * Test steps:
+ *  1. Lock the database by FsyncLock.
+ *  2. Run the aggregation, which will get blocked on sub-operation: {'create' : 'tmp.agg_out.x'}.
+ *  3. Sleep for 2 seconds.
+ *  4. FsyncUnlock.
+ *  5. Check the mongod logs: only that sub-operation (createCollection) and the 'parent' operation
+ * (aggregation) should log about the 2-second long wait for the lock.
+ *
+ * @tags: [requires_fsync]
+ */
+(function() {
+    'use strict';
+
+    const conn = MongoRunner.runMongod();
+    const db = conn.getDB('test');
+    const coll = db.books;
+    const blockedMillis = 2000;
+    assert.writeOK(coll.insert({title: 'Adventures of Huckleberry'}));
+    assert.writeOK(coll.insert({title: '1984'}));
+    assert.writeOK(coll.insert({title: 'Animal Farm'}));
+    // The server will log every operation.
+    db.setProfilingLevel(0, -1);
+    // Create the output collection beforehand so that $out will execute a code path which triggers
+    // the index creation sub-operation.
+    db['favorite'].insert({foo: 1});
+
+    // Lock the database, and then start an operation that needs the lock, so it blocks.
+    assert.commandWorked(db.fsyncLock());
+
+    // Aggregation with $out which will block on creating the temporary collection due to the
+    // FsyncLock.
+    const dollarOutAggregationShell = startParallelShell(function() {
+        // Simple aggregation which copies a document to the output collection.
+        assert.commandWorked(db.runCommand({
+            aggregate: 'books',
+            pipeline: [{$match: {title: '1984'}}, {$out: 'favorite'}],
+            cursor: {}
+        }));
+    }, conn.port);
+
+    // Sub-operation createCollection starts to get blocked.
+    assert.soon(function() {
+        let res = db.currentOp({waitingForLock: true});
+        return res.inprog.length == 1;
+    });
+
+    sleep(blockedMillis);
+
+    clearRawMongoProgramOutput();
+    // Unlock the database. Sub-operation createCollection can proceed
+    // and so do all the following sub-operations.
+    db.fsyncUnlock();
+
+    dollarOutAggregationShell();
+    assert.eq(db['favorite'].count(), 1);
+
+    let mongodLogs = rawMongoProgramOutput();
+    let lines = mongodLogs.split('\n');
+    const lockWaitTimeRegex = /timeAcquiringMicros: { [wW]: ([0-9]+)/;
+    let match;
+    let supposedLockWaitTime;
+    let numWaitedForLocks = 0;
+
+    // Only the logs of 'parent' (aggregation with $out) and the first
+    // sub-operation(createCollection) have the information about the long wait for the lock.
+    for (let line of lines) {
+        if ((match = lockWaitTimeRegex.exec(line)) !== null) {
+            let lockWaitTime = match[1];
+            if (supposedLockWaitTime === undefined)
+                supposedLockWaitTime = lockWaitTime;
+            else
+                assert.eq(lockWaitTime, supposedLockWaitTime);  // Should be the same.
+            numWaitedForLocks++;
+            jsTestLog('Operation/Sub-operation log: ');
+            jsTestLog(line);
+        }
+    }
+    assert.eq(numWaitedForLocks, 2);
+
+    MongoRunner.stopMongod(conn);
+})();