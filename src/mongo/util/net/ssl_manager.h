mongodb/mongo

SERVER-36250 Add support for optionally logging specific negotiated TLS versions

src/mongo/util/net/ssl_manager.h
@@ -159,7 +159,9 @@ public:
      * a StatusWith instead.
      */
     virtual SSLPeerInfo parseAndValidatePeerCertificateDeprecated(
-        const SSLConnectionInterface* conn, const std::string& remoteHost) = 0;
+        const SSLConnectionInterface* conn,
+        const std::string& remoteHost,
+        const HostAndPort& hostForLogging) = 0;
 
     /**
      * Gets the SSLConfiguration containing all information about the current SSL setup
@@ -200,7 +202,9 @@ public:
      * X509 authorization will be returned.
      */
     virtual StatusWith<boost::optional<SSLPeerInfo>> parseAndValidatePeerCertificate(
-        SSLConnectionType ssl, const std::string& remoteHost) = 0;
+        SSLConnectionType ssl,
+        const std::string& remoteHost,
+        const HostAndPort& hostForLogging) = 0;
 };
 
 // Access SSL functions through this instance.
@@ -230,6 +234,27 @@ std::string removeFQDNRoot(std::string name);
  * See "2.4 Converting an AttributeValue from ASN.1 to a String" in RFC 2243
  */
 std::string escapeRfc2253(StringData str);
+
+/**
+ * Platform neutral TLS version enum
+ */
+enum class TLSVersion {
+    kUnknown,
+    kTLS10,
+    kTLS11,
+    kTLS12,
+};
+
+/**
+ * Map SSL version to platform-neutral enum.
+ */
+StatusWith<TLSVersion> mapTLSVersion(SSLConnectionType conn);
+
+/**
+ * Record information about TLS versions and optionally log the TLS version
+ */
+void recordTLSVersion(TLSVersion version, const HostAndPort& hostForLogging);
+
 
 }  // namespace mongo
 #endif  // #ifdef MONGO_CONFIG_SSL

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit e659232178ba8a2ee39e35259f2cb653e85d7d7c [formerly c377f378176e34987babf95db8ed70a12ce44b4f])
Former-commit-id: 573f92bd3567a70f2b6bdc8295a9d230dec1cf04

src/mongo/util/net/ssl_manager.h
@@ -145,7 +145,9 @@ public:
      * a StatusWith instead.
      */
     virtual SSLPeerInfo parseAndValidatePeerCertificateDeprecated(
-        const SSLConnection* conn, const std::string& remoteHost) = 0;
+        const SSLConnection* conn,
+        const std::string& remoteHost,
+        const HostAndPort& hostForLogging) = 0;
 
     /**
      * Gets the SSLConfiguration containing all information about the current SSL setup
@@ -194,7 +196,7 @@ public:
      * X509 authorization will be returned.
      */
     virtual StatusWith<boost::optional<SSLPeerInfo>> parseAndValidatePeerCertificate(
-        SSL* ssl, const std::string& remoteHost) = 0;
+        SSL* ssl, const std::string& remoteHost, const HostAndPort& hostForLogging) = 0;
 };
 
 // Access SSL functions through this instance.
@@ -221,6 +223,27 @@ bool hostNameMatchForX509Certificates(std::string nameToMatch, std::string certH
  * packet if the client has selected a protocol which has been disabled by the server.
  */
 boost::optional<std::array<std::uint8_t, 7>> checkTLSRequest(ConstDataRange cdr);
+
+/**
+ * Platform neutral TLS version enum
+ */
+enum class TLSVersion {
+    kUnknown,
+    kTLS10,
+    kTLS11,
+    kTLS12,
+};
+
+/**
+ * Map SSL version to platform-neutral enum.
+ */
+StatusWith<TLSVersion> mapTLSVersion(SSLConnection* conn);
+
+/**
+ * Record information about TLS versions and optionally log the TLS version
+ */
+void recordTLSVersion(TLSVersion version, const HostAndPort& hostForLogging);
+
 
 }  // namespace mongo
 #endif  // #ifdef MONGO_CONFIG_SSL