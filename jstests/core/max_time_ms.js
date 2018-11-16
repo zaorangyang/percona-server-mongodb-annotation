mongodb/mongo

SERVER-37385 make max_time_ms.js even more robust

jstests/core/max_time_ms.js
@@ -34,10 +34,11 @@ error = assert.throws(function() {
     cursor.itcount();
 }, [], "expected query to abort due to time limit");
 // TODO SERVER-32565: The error should always be MaxTimeMSExpired, but there are rare cases where
-// interrupting javascript execution on the server with a stepdown or timeout causes an
-// InternalError instead, so we also accept that here.
-assert(ErrorCodes.MaxTimeMSExpired == error.code || ErrorCodes.InternalError == error.code,
-       "Failed with error: " + tojson(error));
+// interrupting javascript execution on the server with a stepdown or timeout results in an error
+// code of InternalError or Interrupted instead, so we also accept those here.
+assert.contains(error.code,
+                [ErrorCodes.MaxTimeMSExpired, ErrorCodes.Interrupted, ErrorCodes.InternalError],
+                "Failed with error: " + tojson(error));
 
 //
 // Simple negative test for query: a ~300ms query with a 10s time limit should not hit the time
@@ -89,10 +90,11 @@ error = assert.throws(function() {
     cursor.next();
 }, [], "expected batch 2 (getmore) to abort due to time limit");
 // TODO SERVER-32565: The error should always be MaxTimeMSExpired, but there are rare cases where
-// interrupting javascript execution on the server with a stepdown or timeout causes an
-// InternalError instead, so we also accept that here.
-assert(ErrorCodes.MaxTimeMSExpired == error.code || ErrorCodes.InternalError == error.code,
-       "Failed with error: " + tojson(error));
+// interrupting javascript execution on the server with a stepdown or timeout results in an error
+// code of InternalError or Interrupted instead, so we also accept those here.
+assert.contains(error.code,
+                [ErrorCodes.MaxTimeMSExpired, ErrorCodes.Interrupted, ErrorCodes.InternalError],
+                "Failed with error: " + tojson(error));
 
 //
 // Simple negative test for getmore:
@@ -150,10 +152,11 @@ error = assert.throws(function() {
     cursor.itcount();
 }, [], "expected find() to abort due to time limit");
 // TODO SERVER-32565: The error should always be MaxTimeMSExpired, but there are rare cases where
-// interrupting javascript execution on the server with a stepdown or timeout causes an
-// InternalError instead, so we also accept that here.
-assert(ErrorCodes.MaxTimeMSExpired == error.code || ErrorCodes.InternalError == error.code,
-       "Failed with error: " + tojson(error));
+// interrupting javascript execution on the server with a stepdown or timeout results in an error
+// code of InternalError or Interrupted instead, so we also accept those here.
+assert.contains(error.code,
+                [ErrorCodes.MaxTimeMSExpired, ErrorCodes.Interrupted, ErrorCodes.InternalError],
+                "Failed with error: " + tojson(error));
 
 //
 // Many-batch negative test for getmore:

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit 8c35c1882268fd5d36679c256a29a2feb3e0e6c0 [formerly 5978c283adec32042f66fe6730f88a16e563ef86])
Former-commit-id: 190c87c645c4940f77335c8b90c5ee4f99460bdc

jstests/core/max_time_ms.js
@@ -30,7 +30,12 @@ cursor.maxTimeMS(100);
 error = assert.throws(function() {
     cursor.itcount();
 }, [], "expected query to abort due to time limit");
-assert.eq(ErrorCodes.ExceededTimeLimit, error.code);
+// TODO SERVER-32565: The error should always be ExceededTimeLimit, but there are rare cases where
+// interrupting javascript execution on the server with a stepdown or timeout results in an error
+// code of InternalError or Interrupted instead, so we also accept those here.
+assert.contains(error.code,
+                [ErrorCodes.ExceededTimeLimit, ErrorCodes.Interrupted, ErrorCodes.InternalError],
+                "Failed with error: " + tojson(error));
 
 //
 // Simple negative test for query: a ~300ms query with a 10s time limit should not hit the time
@@ -80,7 +85,12 @@ error = assert.throws(function() {
     cursor.next();
     cursor.next();
 }, [], "expected batch 2 (getmore) to abort due to time limit");
-assert.eq(ErrorCodes.ExceededTimeLimit, error.code);
+// TODO SERVER-32565: The error should always be ExceededTimeLimit, but there are rare cases where
+// interrupting javascript execution on the server with a stepdown or timeout results in an error
+// code of InternalError or Interrupted instead, so we also accept those here.
+assert.contains(error.code,
+                [ErrorCodes.ExceededTimeLimit, ErrorCodes.Interrupted, ErrorCodes.InternalError],
+                "Failed with error: " + tojson(error));
 
 //
 // Simple negative test for getmore:
@@ -136,7 +146,12 @@ cursor.maxTimeMS(6 * 1000);
 error = assert.throws(function() {
     cursor.itcount();
 }, [], "expected find() to abort due to time limit");
-assert.eq(ErrorCodes.ExceededTimeLimit, error.code);
+// TODO SERVER-32565: The error should always be ExceededTimeLimit, but there are rare cases where
+// interrupting javascript execution on the server with a stepdown or timeout results in an error
+// code of InternalError or Interrupted instead, so we also accept those here.
+assert.contains(error.code,
+                [ErrorCodes.ExceededTimeLimit, ErrorCodes.Interrupted, ErrorCodes.InternalError],
+                "Failed with error: " + tojson(error));
 
 //
 // Many-batch negative test for getmore: