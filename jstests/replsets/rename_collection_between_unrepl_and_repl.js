mongodb/mongo

SERVER-29825 Do not allow rename from unreplicated to replicated DB or vice versa

jstests/replsets/rename_collection_between_unrepl_and_repl.js
@@ -1,0 +1,44 @@
+/**
+ * Tests that renameCollection disallows renaming between an unreplicated and a replicated
+ * namespace in both directions. Unreplicated collections are unique to the nodes that own
+ * them, so allowing such renames would introduce consistency risks. This test uses the
+ * 'local' database for unreplicated namespaces.
+ */
+
+(function() {
+    "use strict";
+
+    const name = "rename_collection_between_unrepl_and_repl";
+    const rst = new ReplSetTest({"name": name, "nodes": 1});
+    rst.startSet();
+    rst.initiate();
+    const primary = rst.getPrimary();
+
+    /**
+     * Part 1: Attempt to rename from a replicated to an unreplicated namespace.
+     */
+    let sourceNs = "somedb.replicated";
+    let targetNs = "local.unreplicated";
+
+    // Ensure that the source collection exists.
+    assert.commandWorked(primary.getCollection(sourceNs).insert({"fromRepl": "toUnrepl"}));
+
+    assert.commandFailedWithCode(
+        primary.adminCommand({"renameCollection": sourceNs, "to": targetNs}),
+        ErrorCodes.IllegalOperation);
+
+    /**
+     * Part 2: Attempt to rename from an unreplicated to a replicated namespace.
+     */
+    sourceNs = "local.alsoUnreplicated";
+    targetNs = "somedb.alsoReplicated";
+
+    // Ensure that the source collection exists.
+    assert.commandWorked(primary.getCollection(sourceNs).insert({"fromUnrepl": "toRepl"}));
+
+    assert.commandFailedWithCode(
+        primary.adminCommand({"renameCollection": sourceNs, "to": targetNs}),
+        ErrorCodes.IllegalOperation);
+
+    rst.stopSet();
+})();

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit a6bcd477b0ebb7e320b47131bbb6caef486be03d [formerly f68d6091bad5a2747cc09c743b8c2c7b4e42a65a])
Former-commit-id: 8d53a21c1d6ae557e27dd8f872899539e7f3569b

jstests/replsets/rename_collection_between_unrepl_and_repl.js
@@ -1,0 +1,44 @@
+/**
+ * Tests that renameCollection disallows renaming between an unreplicated and a replicated
+ * namespace in both directions. Unreplicated collections are unique to the nodes that own
+ * them, so allowing such renames would introduce consistency risks. This test uses the
+ * 'local' database for unreplicated namespaces.
+ */
+
+(function() {
+    "use strict";
+
+    const name = "rename_collection_between_unrepl_and_repl";
+    const rst = new ReplSetTest({"name": name, "nodes": 1});
+    rst.startSet();
+    rst.initiate();
+    const primary = rst.getPrimary();
+
+    /**
+     * Part 1: Attempt to rename from a replicated to an unreplicated namespace.
+     */
+    let sourceNs = "somedb.replicated";
+    let targetNs = "local.unreplicated";
+
+    // Ensure that the source collection exists.
+    assert.writeOK(primary.getCollection(sourceNs).insert({"fromRepl": "toUnrepl"}));
+
+    assert.commandFailedWithCode(
+        primary.adminCommand({"renameCollection": sourceNs, "to": targetNs}),
+        ErrorCodes.IllegalOperation);
+
+    /**
+     * Part 2: Attempt to rename from an unreplicated to a replicated namespace.
+     */
+    sourceNs = "local.alsoUnreplicated";
+    targetNs = "somedb.alsoReplicated";
+
+    // Ensure that the source collection exists.
+    assert.writeOK(primary.getCollection(sourceNs).insert({"fromUnrepl": "toRepl"}));
+
+    assert.commandFailedWithCode(
+        primary.adminCommand({"renameCollection": sourceNs, "to": targetNs}),
+        ErrorCodes.IllegalOperation);
+
+    rst.stopSet();
+})();