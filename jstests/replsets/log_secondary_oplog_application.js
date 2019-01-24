mongodb/mongo

SERVER-32146 Log slow oplog entry application

jstests/replsets/log_secondary_oplog_application.js
@@ -1,0 +1,81 @@
+/**
+ * Tests that the server logs ops on the secondary if and only if they are slow to apply.
+ * We should only report ops if they take longer than "slowMS" to apply on a secondary.
+ * We intentionally target CRUD ops in this test, since we know we should be the only ones
+ * issuing them. See below for details on how we simulate quickness and slowness.
+ */
+
+(function() {
+    "use strict";
+    load("jstests/libs/check_log.js");
+
+    let name = "log_secondary_oplog_application";
+    let rst = ReplSetTest({name: name, nodes: 2});
+    rst.startSet();
+
+    let nodes = rst.nodeList();
+    rst.initiate({
+        "_id": name,
+        "members": [{"_id": 0, "host": nodes[0]}, {"_id": 1, "host": nodes[1], "priority": 0}]
+    });
+
+    let primary = rst.getPrimary();
+    let secondary = rst.getSecondary();
+
+    /**
+     * Part 1: Issue a fast op and make sure that we do *not* log it.
+     * We ensure the op is always considered fast by vastly increasing the "slowMS" threshold.
+     */
+
+    // Create collection explicitly so the insert doesn't have to do it.
+    assert.commandWorked(primary.getDB(name).createCollection("fastOp"));
+    rst.awaitReplication();
+
+    // Set "slowMS" to a very high value (in milliseconds).
+    assert.commandWorked(secondary.getDB(name).setProfilingLevel(1, 60 * 60 * 1000));
+
+    // Issue a write and make sure we replicate it.
+    assert.commandWorked(primary.getDB(name)["fastOp"].insert({"fast": "cheetah"}));
+    rst.awaitReplication();
+
+    // The op should not have been logged.
+    assert.throws(function() {
+        checkLog.contains(secondary, "applied op: CRUD", 1 * 1000);
+    });
+
+    /**
+     * Part 2: Issue a slow op and make sure that we *do* log it.
+     * We use a failpoint in SyncTail::syncApply which blocks after we read the time at the start
+     * of the application of the op, and we wait there to simulate slowness.
+     */
+
+    // Create collection explicitly so the insert doesn't have to do it.
+    assert.commandWorked(primary.getDB(name).createCollection("slowOp"));
+    rst.awaitReplication();
+
+    // Set "slowMS" to a low value (in milliseconds).
+    assert.commandWorked(secondary.getDB(name).setProfilingLevel(1, 20));
+
+    // Hang right after taking note of the start time of the application.
+    assert.commandWorked(secondary.adminCommand(
+        {configureFailPoint: "hangAfterRecordingOpApplicationStartTime", mode: "alwaysOn"}));
+
+    // Issue a write and make sure we've hit the failpoint before moving on.
+    assert.commandWorked(primary.getDB(name)["slowOp"].insert({"slow": "sloth"}));
+    checkLog.contains(secondary,
+                      "syncApply - fail point hangAfterRecordingOpApplicationStartTime enabled");
+
+    // Wait for an amount of time safely above the "slowMS" we set.
+    sleep(0.5 * 1000);
+
+    // Disable the failpoint so the op finish can applying.
+    assert.commandWorked(secondary.adminCommand(
+        {configureFailPoint: "hangAfterRecordingOpApplicationStartTime", mode: "off"}));
+
+    // Make sure we log that insert op.
+    rst.awaitReplication();
+    checkLog.contains(secondary, "applied op: CRUD");
+
+    rst.stopSet();
+
+})();

src/mongo/db/repl/sync_tail.cpp
@@ -88,6 +88,7 @@ namespace repl {
 namespace {
 
 MONGO_FAIL_POINT_DEFINE(pauseBatchApplicationBeforeCompletion);
+MONGO_FAIL_POINT_DEFINE(hangAfterRecordingOpApplicationStartTime);
 
 // The oplog entries applied
 Counter64 opsAppliedStats;
@@ -227,6 +228,41 @@ NamespaceStringOrUUID getNsOrUUID(const NamespaceString& nss, const BSONObj& op)
     return nss;
 }
 
+/**
+ * Used for logging a report of ops that take longer than "slowMS" to apply. This is called
+ * right before returning from syncApply, and it returns the same status.
+ */
+Status finishAndLogApply(ClockSource* clockSource,
+                         Status finalStatus,
+                         Date_t applyStartTime,
+                         OpTypeEnum opType,
+                         const BSONObj& op) {
+
+    if (finalStatus.isOK()) {
+        auto applyEndTime = clockSource->now();
+        auto diffMS = durationCount<Milliseconds>(applyEndTime - applyStartTime);
+
+        // This op was slow to apply, so we should log a report of it.
+        if (diffMS > serverGlobalParams.slowMS) {
+
+            StringBuilder s;
+            s << "applied op: ";
+
+            if (opType == OpTypeEnum::kCommand) {
+                s << "command ";
+            } else {
+                s << "CRUD ";
+            }
+
+            s << redact(op);
+            s << ", took " << diffMS << "ms";
+
+            log() << s.str();
+        }
+    }
+    return finalStatus;
+}
+
 }  // namespace
 
 // static
@@ -263,7 +299,20 @@ Status SyncTail::syncApply(OperationContext* opCtx,
         return status;
     };
 
+    auto clockSource = opCtx->getServiceContext()->getFastClockSource();
+    auto applyStartTime = clockSource->now();
+
+    if (MONGO_FAIL_POINT(hangAfterRecordingOpApplicationStartTime)) {
+        log() << "syncApply - fail point hangAfterRecordingOpApplicationStartTime enabled. "
+              << "Blocking until fail point is disabled. ";
+        MONGO_FAIL_POINT_PAUSE_WHILE_SET(hangAfterRecordingOpApplicationStartTime);
+    }
+
     auto opType = OpType_parse(IDLParserErrorContext("syncApply"), op["op"].valuestrsafe());
+
+    auto finishApply = [&](Status status) {
+        return finishAndLogApply(clockSource, status, applyStartTime, opType, op);
+    };
 
     if (opType == OpTypeEnum::kNoop) {
         if (nss.db() == "") {
@@ -271,9 +320,9 @@ Status SyncTail::syncApply(OperationContext* opCtx,
         }
         Lock::DBLock dbLock(opCtx, nss.db(), MODE_X);
         OldClientContext ctx(opCtx, nss.ns());
-        return applyOp(ctx.db());
+        return finishApply(applyOp(ctx.db()));
     } else if (OplogEntry::isCrudOpType(opType)) {
-        return writeConflictRetry(opCtx, "syncApply_CRUD", nss.ns(), [&] {
+        return finishApply(writeConflictRetry(opCtx, "syncApply_CRUD", nss.ns(), [&] {
             // Need to throw instead of returning a status for it to be properly ignored.
             try {
                 AutoGetCollection autoColl(opCtx, getNsOrUUID(nss, op), MODE_IX);
@@ -300,9 +349,9 @@ Status SyncTail::syncApply(OperationContext* opCtx,
                 ex.addContext(str::stream() << "Failed to apply operation: " << redact(op));
                 throw;
             }
-        });
+        }));
     } else if (opType == OpTypeEnum::kCommand) {
-        return writeConflictRetry(opCtx, "syncApply_command", nss.ns(), [&] {
+        return finishApply(writeConflictRetry(opCtx, "syncApply_command", nss.ns(), [&] {
             // A command may need a global write lock. so we will conservatively go
             // ahead and grab one for non-transaction commands.
             // Transactions have to acquire the same locks on secondaries as on primary.
@@ -322,7 +371,7 @@ Status SyncTail::syncApply(OperationContext* opCtx,
             Status status = applyCommand_inlock(opCtx, op, entry, oplogApplicationMode);
             incrementOpsAppliedStats();
             return status;
-        });
+        }));
     }
 
     MONGO_UNREACHABLE;

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit 8f28d4a485bfdf678b5680e0253c70db8e20b350 [formerly 66434ba0a7930efedbe5e06a098ab8e7bb659cfa])
Former-commit-id: afb213ced669a328c90e0a0fb0b7e06bf92c0ccb

jstests/replsets/log_secondary_oplog_application.js
@@ -1,0 +1,81 @@
+/**
+ * Tests that the server logs ops on the secondary if and only if they are slow to apply.
+ * We should only report ops if they take longer than "slowMS" to apply on a secondary.
+ * We intentionally target CRUD ops in this test, since we know we should be the only ones
+ * issuing them. See below for details on how we simulate quickness and slowness.
+ */
+
+(function() {
+    "use strict";
+    load("jstests/libs/check_log.js");
+
+    let name = "log_secondary_oplog_application";
+    let rst = ReplSetTest({name: name, nodes: 2});
+    rst.startSet();
+
+    let nodes = rst.nodeList();
+    rst.initiate({
+        "_id": name,
+        "members": [{"_id": 0, "host": nodes[0]}, {"_id": 1, "host": nodes[1], "priority": 0}]
+    });
+
+    let primary = rst.getPrimary();
+    let secondary = rst.getSecondary();
+
+    /**
+     * Part 1: Issue a fast op and make sure that we do *not* log it.
+     * We ensure the op is always considered fast by vastly increasing the "slowMS" threshold.
+     */
+
+    // Create collection explicitly so the insert doesn't have to do it.
+    assert.commandWorked(primary.getDB(name).createCollection("fastOp"));
+    rst.awaitReplication();
+
+    // Set "slowMS" to a very high value (in milliseconds).
+    assert.commandWorked(secondary.getDB(name).setProfilingLevel(1, 60 * 60 * 1000));
+
+    // Issue a write and make sure we replicate it.
+    assert.writeOK(primary.getDB(name)["fastOp"].insert({"fast": "cheetah"}));
+    rst.awaitReplication();
+
+    // The op should not have been logged.
+    assert.throws(function() {
+        checkLog.contains(secondary, "applied op: CRUD", 1 * 1000);
+    });
+
+    /**
+     * Part 2: Issue a slow op and make sure that we *do* log it.
+     * We use a failpoint in SyncTail::syncApply which blocks after we read the time at the start
+     * of the application of the op, and we wait there to simulate slowness.
+     */
+
+    // Create collection explicitly so the insert doesn't have to do it.
+    assert.commandWorked(primary.getDB(name).createCollection("slowOp"));
+    rst.awaitReplication();
+
+    // Set "slowMS" to a low value (in milliseconds).
+    assert.commandWorked(secondary.getDB(name).setProfilingLevel(1, 20));
+
+    // Hang right after taking note of the start time of the application.
+    assert.commandWorked(secondary.adminCommand(
+        {configureFailPoint: "hangAfterRecordingOpApplicationStartTime", mode: "alwaysOn"}));
+
+    // Issue a write and make sure we've hit the failpoint before moving on.
+    assert.writeOK(primary.getDB(name)["slowOp"].insert({"slow": "sloth"}));
+    checkLog.contains(secondary,
+                      "syncApply - fail point hangAfterRecordingOpApplicationStartTime enabled");
+
+    // Wait for an amount of time safely above the "slowMS" we set.
+    sleep(0.5 * 1000);
+
+    // Disable the failpoint so the op finish can applying.
+    assert.commandWorked(secondary.adminCommand(
+        {configureFailPoint: "hangAfterRecordingOpApplicationStartTime", mode: "off"}));
+
+    // Make sure we log that insert op.
+    rst.awaitReplication();
+    checkLog.contains(secondary, "applied op: CRUD");
+
+    rst.stopSet();
+
+})();

src/mongo/db/repl/sync_tail.cpp
@@ -94,6 +94,8 @@ AtomicInt32 SyncTail::replBatchLimitOperations{5 * 1000};
 
 namespace {
 
+MONGO_FP_DECLARE(hangAfterRecordingOpApplicationStartTime);
+
 /**
  * This variable determines the number of writer threads SyncTail will have. It has a default value,
  * which varies based on architecture and can be overridden using the "replWriterThreadCount" server
@@ -316,6 +318,41 @@ bool SyncTail::peek(OperationContext* opCtx, BSONObj* op) {
     return _networkQueue->peek(opCtx, op);
 }
 
+/**
+ * Used for logging a report of ops that take longer than "slowMS" to apply. This is called
+ * right before returning from syncApply, and it returns the same status.
+ */
+Status finishAndLogApply(ClockSource* clockSource,
+                         Status finalStatus,
+                         Date_t applyStartTime,
+                         const char* opType,
+                         const BSONObj& op) {
+
+    if (finalStatus.isOK()) {
+        auto applyEndTime = clockSource->now();
+        auto diffMS = durationCount<Milliseconds>(applyEndTime - applyStartTime);
+
+        // This op was slow to apply, so we should log a report of it.
+        if (diffMS > serverGlobalParams.slowMS) {
+
+            StringBuilder s;
+            s << "applied op: ";
+
+            if (opType[0] == 'c') {
+                s << "command ";
+            } else {
+                s << "CRUD ";
+            }
+
+            s << redact(op);
+            s << ", took " << diffMS << "ms";
+
+            log() << s.str();
+        }
+    }
+    return finalStatus;
+}
+
 // static
 Status SyncTail::syncApply(OperationContext* opCtx,
                            const BSONObj& op,
@@ -353,6 +390,19 @@ Status SyncTail::syncApply(OperationContext* opCtx,
         return status;
     };
 
+    auto clockSource = opCtx->getServiceContext()->getFastClockSource();
+    auto applyStartTime = clockSource->now();
+
+    if (MONGO_FAIL_POINT(hangAfterRecordingOpApplicationStartTime)) {
+        log() << "syncApply - fail point hangAfterRecordingOpApplicationStartTime enabled. "
+              << "Blocking until fail point is disabled. ";
+        MONGO_FAIL_POINT_PAUSE_WHILE_SET(hangAfterRecordingOpApplicationStartTime);
+    }
+
+    auto finishApply = [&](Status status) {
+        return finishAndLogApply(clockSource, status, applyStartTime, opType, op);
+    };
+
     bool isNoOp = opType[0] == 'n';
     if (isNoOp || (opType[0] == 'i' && nss.isSystemDotIndexes())) {
         if (isNoOp && nss.db() == "") {
@@ -361,11 +411,11 @@ Status SyncTail::syncApply(OperationContext* opCtx,
         }
         Lock::DBLock dbLock(opCtx, nss.db(), MODE_X);
         OldClientContext ctx(opCtx, nss.ns());
-        return applyOp(ctx.db());
+        return finishApply(applyOp(ctx.db()));
     }
 
     if (isCrudOpType(opType)) {
-        return writeConflictRetry(opCtx, "syncApply_CRUD", nss.ns(), [&] {
+        return finishApply(writeConflictRetry(opCtx, "syncApply_CRUD", nss.ns(), [&] {
             // DB lock always acquires the global lock
             Lock::DBLock dbLock(opCtx, nss.db(), MODE_IX);
             auto ui = op["ui"];
@@ -399,11 +449,11 @@ Status SyncTail::syncApply(OperationContext* opCtx,
 
             OldClientContext ctx(opCtx, actualNss.ns(), db, /*justCreated*/ false);
             return applyOp(ctx.db());
-        });
+        }));
     }
 
     if (opType[0] == 'c') {
-        return writeConflictRetry(opCtx, "syncApply_command", nss.ns(), [&] {
+        return finishApply(writeConflictRetry(opCtx, "syncApply_command", nss.ns(), [&] {
             // a command may need a global write lock. so we will conservatively go
             // ahead and grab one here. suboptimal. :-(
             Lock::GlobalWrite globalWriteLock(opCtx);
@@ -412,7 +462,7 @@ Status SyncTail::syncApply(OperationContext* opCtx,
             Status status = applyCommandInLock(opCtx, op, oplogApplicationMode);
             incrementOpsAppliedStats();
             return status;
-        });
+        }));
     }
 
     // unknown opType