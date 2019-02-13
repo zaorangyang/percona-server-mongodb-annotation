mongodb/mongo

SERVER-9043/SERVER-31156 Link flushRouterConfig on MongoD and make it accept a namespace argument

src/mongo/s/catalog_cache.cpp
@@ -423,6 +423,17 @@ void CatalogCache::invalidateShardedCollection(const NamespaceString& nss) {
     itDb->second[nss.ns()]->needsRefresh = true;
 }
 
+void CatalogCache::purgeCollection(const NamespaceString& nss) {
+    stdx::lock_guard<stdx::mutex> lg(_mutex);
+
+    auto itDb = _collectionsByDb.find(nss.db());
+    if (itDb == _collectionsByDb.end()) {
+        return;
+    }
+
+    itDb->second.erase(nss.ns());
+}
+
 void CatalogCache::purgeDatabase(StringData dbName) {
     stdx::lock_guard<stdx::mutex> lg(_mutex);
     _databases.erase(dbName);

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit 44403d274f5cf987fc0858e2a31605266b5664b8 [formerly e4f26d25632f94a6577028ccefd32069550628b6])
(cherry picked from commit 98beea6668d80446449e19a5a57817522dd95b53 [formerly dbcfdd8a27453e2ff552bcaa6679aac1cbc1d428])
Former-commit-id: 52b87981917afaa4b93a73218404690a69036016

src/mongo/s/catalog_cache.cpp
@@ -274,6 +274,17 @@ void CatalogCache::invalidateShardedCollection(const NamespaceString& nss) {
     it->second->collections[nss.ns()].needsRefresh = true;
 }
 
+void CatalogCache::purgeCollection(const NamespaceString& nss) {
+    stdx::lock_guard<stdx::mutex> lg(_mutex);
+
+    auto itDb = _databases.find(nss.db());
+    if (itDb == _databases.end()) {
+        return;
+    }
+
+    itDb->second->collections.erase(nss.ns());
+}
+
 void CatalogCache::purgeDatabase(StringData dbName) {
     stdx::lock_guard<stdx::mutex> lg(_mutex);
     _databases.erase(dbName);