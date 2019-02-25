mongodb/mongo

SERVER-35732 Fix dbStats.fsUsedSize behavior for database with hyphenated names

src/mongo/db/catalog/database_impl.cpp
@@ -448,11 +448,8 @@ void DatabaseImpl::getStats(OperationContext* opCtx, BSONObjBuilder* output, dou
     _dbEntry->appendExtraStats(opCtx, output, scale);
 
     if (!opCtx->getServiceContext()->getStorageEngine()->isEphemeral()) {
-        boost::filesystem::path dbpath(storageGlobalParams.dbpath);
-        if (storageGlobalParams.directoryperdb) {
-            dbpath /= _name;
-        }
-
+        boost::filesystem::path dbpath(
+            opCtx->getServiceContext()->getStorageEngine()->getFilesystemPathForDb(_name));
         boost::system::error_code ec;
         boost::filesystem::space_info spaceInfo = boost::filesystem::space(dbpath, ec);
         if (!ec) {

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit 9209dcca96a10529ea3e5f7de7cba4cf59e335a1 [formerly e0be8ba5aa04df21f6a9a42578a349a4c303776d])
(cherry picked from commit cdf15b6db2e30f51c785c8e2c56472e793a7db69)
Former-commit-id: b4339db12bf57ffee5b84a95c6919dbd35fe31c9

src/mongo/db/catalog/database_impl.cpp
@@ -414,11 +414,8 @@ void DatabaseImpl::getStats(OperationContext* opCtx, BSONObjBuilder* output, dou
     _dbEntry->appendExtraStats(opCtx, output, scale);
 
     if (!opCtx->getServiceContext()->getGlobalStorageEngine()->isEphemeral()) {
-        boost::filesystem::path dbpath(storageGlobalParams.dbpath);
-        if (storageGlobalParams.directoryperdb) {
-            dbpath /= _name;
-        }
-
+        boost::filesystem::path dbpath(
+            opCtx->getServiceContext()->getGlobalStorageEngine()->getFilesystemPathForDb(_name));
         boost::system::error_code ec;
         boost::filesystem::space_info spaceInfo = boost::filesystem::space(dbpath, ec);
         if (!ec) {