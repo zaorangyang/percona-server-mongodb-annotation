mongodb/mongo

SERVER-38408 Return postBatchResumeToken with each mongoD change stream batch

src/mongo/util/uuid.cpp
@@ -89,6 +89,10 @@ UUID UUID::parse(const BSONObj& obj) {
     return res.getValue();
 }
 
+UUID UUID::nil() {
+    return UUID{};
+}
+
 bool UUID::isUUIDString(const std::string& s) {
     return std::regex_match(s, uuidRegex);
 }

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit c70d3f0098e82748dd5b75e060a066dd96e9413e [formerly fc5bc0947ceedee3b61b2d922cabd3e5df7ec07c])
(cherry picked from commit 69bedac51a427979c7b9022ac3921e7f95e7fbff [formerly 7f70c1213b2cc79591ea1d11f9724d057886a0fd])
Former-commit-id: 296dfba8bd13e5c420b8ccfb51878e017eac2f5a

src/mongo/util/uuid.cpp
@@ -89,6 +89,10 @@ UUID UUID::parse(const BSONObj& obj) {
     return res.getValue();
 }
 
+UUID UUID::makeDefaultForChangeStream() {
+    return UUID{};
+}
+
 bool UUID::isUUIDString(const std::string& s) {
     return std::regex_match(s, uuidRegex);
 }