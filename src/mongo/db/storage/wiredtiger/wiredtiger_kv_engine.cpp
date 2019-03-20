d975556f28d15e57cfd76cb1f4a55f8d5dde5477
Igor Solodovnikov <igor.solodovnikov@percona.com>
2019-03-20 15:59:43 +0200

    PSMDB-322 generate key on first start if necessary
    (cherry picked from commit 7a7460da228a32b045ae67d6072d1d60341ba25a [formerly b74d8166c4e8307545aa3625903b57396284641f])
    
    
    Former-commit-id: b5bc0fe6145e92cc61c8316f65b93cea7c425d30

--- src/mongo/db/storage/wiredtiger/wiredtiger_kv_engine.cpp
+++ src/mongo/db/storage/wiredtiger/wiredtiger_kv_engine.cpp
@@ -626,6 +626,7 @@ WiredTigerKVEngine::WiredTigerKVEngine(const std::string& canonicalName,
 
     if (encryptionGlobalParams.enableEncryption) {
         namespace fs = boost::filesystem;
+        bool just_created{false};
         fs::path keyDBPath = path;
         keyDBPath /= keydbDir;
         if (!fs::exists(keyDBPath)) {
@@ -634,6 +635,7 @@ WiredTigerKVEngine::WiredTigerKVEngine(const std::string& canonicalName,
             if (!fs::exists(betaKeyDBPath)) {
                 try {
                     fs::create_directory(keyDBPath);
+                    just_created = true;
                 } catch (std::exception& e) {
                     log() << "error creating KeyDB dir " << keyDBPath.string() << ' ' << e.what();
                     throw;
@@ -670,7 +672,7 @@ WiredTigerKVEngine::WiredTigerKVEngine(const std::string& canonicalName,
                 }
             }
         }
-        auto encryptionKeyDB = stdx::make_unique<EncryptionKeyDB>(keyDBPath.string());
+        auto encryptionKeyDB = stdx::make_unique<EncryptionKeyDB>(just_created, keyDBPath.string());
         encryptionKeyDB->init();
         // do master key rotation if necessary
         if (encryptionGlobalParams.vaultRotateMasterKey) {
