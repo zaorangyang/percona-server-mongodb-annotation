mongodb/mongo

SERVER-39820 Include client IP in log message for successful authentication

src/mongo/db/commands/authentication_commands.cpp
@@ -285,8 +285,7 @@ bool CmdAuthenticate::run(OperationContext* opCtx,
     if (!status.isOK()) {
         if (!serverGlobalParams.quiet.load()) {
             auto const client = opCtx->getClient();
-            log() << "Failed to authenticate " << user
-                  << (client->hasRemote() ? (" from client " + client->getRemote().toString()) : "")
+            log() << "Failed to authenticate " << user << " from client " << client->getRemote()
                   << " with mechanism " << mechanism << ": " << status;
         }
         sleepmillis(saslGlobalParams.authFailedDelay.load());
@@ -299,6 +298,12 @@ bool CmdAuthenticate::run(OperationContext* opCtx,
         }
         return false;
     }
+
+    if (!serverGlobalParams.quiet.load()) {
+        log() << "Successfully authenticated as principal " << user.getUser() << " on "
+              << user.getDB() << " from client " << opCtx->getClient()->session()->remote();
+    }
+
     result.append("dbname", user.getDB());
     result.append("user", user.getUser());
     return true;

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit 65d00952955273d150e1ba84f83f346ee16a07a3 [formerly 0a847ef8453015e8b622595692b2fde0488486a6])
(cherry picked from commit 4a515ef5692d48efffb74c4fe9e1a297bd063aa3)
Former-commit-id: 51add9e4cdaf3ded4c0d0ff3666cb7927367c9c3

src/mongo/db/commands/authentication_commands.cpp
@@ -187,9 +187,10 @@ bool CmdAuthenticate::run(OperationContext* txn,
     Status status = _authenticate(txn, mechanism, user, cmdObj);
     audit::logAuthentication(Client::getCurrent(), mechanism, user, status.code());
     if (!status.isOK()) {
-        if (!serverGlobalParams.quiet) {
-            log() << "Failed to authenticate " << user << " with mechanism " << mechanism << ": "
-                  << status;
+        if (!serverGlobalParams.quiet.load()) {
+            auto const client = txn->getClient();
+            log() << "Failed to authenticate " << user << " from client " << client->getRemote()
+                  << " with mechanism " << mechanism << ": " << status;
         }
         if (status.code() == ErrorCodes::AuthenticationFailed) {
             // Statuses with code AuthenticationFailed may contain messages we do not wish to
@@ -201,6 +202,12 @@ bool CmdAuthenticate::run(OperationContext* txn,
         sleepmillis(saslGlobalParams.authFailedDelay);
         return false;
     }
+
+    if (!serverGlobalParams.quiet.load()) {
+        log() << "Successfully authenticated as principal " << user.getUser() << " on "
+              << user.getDB() << " from client " << txn->getClient()->session()->remote();
+    }
+
     result.append("dbname", user.getDB());
     result.append("user", user.getUser());
     return true;