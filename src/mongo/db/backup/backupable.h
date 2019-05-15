9e9a768aad6ebaf6e497c0a00af1c9b8ef730433
Igor Solodovnikov <igor.solodovnikov@percona.com>
2019-05-15 18:42:07 +0300

    PSMDB-371 extend createBackup command to support S3-compatible storage
    (cherry picked from commit 70796d1650f79efc461e88a2cbfeabc178e4258d)
    
    
    Former-commit-id: 28cf21c704f841ddb15a2ae22782de287cc67f36

--- src/mongo/db/backup/backupable.h
+++ src/mongo/db/backup/backupable.h
@@ -41,6 +41,16 @@ namespace mongo {
 
 namespace percona {
 
+struct S3BackupParameters {
+    std::string profile;  // empty value means default profile
+    std::string region;  // empty value means default region (US_EAST_1)
+    std::string endpoint;  // endpoint override, for example  "127.0.0.1:9000"
+    std::string scheme{"HTTPS"};  // HTTP/HTTPS, by default HTTPS
+    bool useVirtualAddressing{true};  // true by default
+    std::string bucket;  // S3 bucket name
+    std::string path;  // path inside bucket (may be empty)
+};
+
 /**
  * The interface which provides the ability to perform hot
  * backups of the storage engine.
@@ -56,6 +66,16 @@ struct Backupable {
     virtual mongo::Status hotBackup(mongo::OperationContext* opCtx, const std::string& path) {
         return mongo::Status(mongo::ErrorCodes::IllegalOperation,
                              "This engine doesn't support hot backup.");
+    }
+
+    /**
+     * Perform hot backup to S3-compatible storage.
+     * @param s3params parameters of server connection and backup location.
+     * @return Status code of the operation.
+     */
+    virtual mongo::Status hotBackup(mongo::OperationContext* opCtx, const S3BackupParameters& s3params) {
+        return mongo::Status(mongo::ErrorCodes::IllegalOperation,
+                             "This engine doesn't support hot backup to S3-compatible storage.");
     }
 };
 
--- src/mongo/db/storage/kv/kv_storage_engine.cpp
+++ src/mongo/db/storage/kv/kv_storage_engine.cpp
@@ -84,6 +84,10 @@ Status KVStorageEngine::hotBackup(OperationContext* opCtx, const std::string& pa
     return _engine->hotBackup(opCtx, path);
 }
 
+Status KVStorageEngine::hotBackup(OperationContext* opCtx, const percona::S3BackupParameters& s3params) {
+    return _engine->hotBackup(opCtx, s3params);
+}
+
 void KVStorageEngine::keydbDropDatabase(const std::string& db) {
     _engine->keydbDropDatabase(db);
 }
