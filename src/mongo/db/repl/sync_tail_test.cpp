mongodb/mongo

SERVER-36004 SessionUpdateTracker should ignore no-op entries for pre/post image oplogs

src/mongo/db/repl/sync_tail_test.cpp
@@ -1603,6 +1603,34 @@ public:
                                 boost::none);   // post-image optime
     }
 
+    /**
+     * Creates an OplogEntry with given parameters and preset defaults for this test suite.
+     */
+    repl::OplogEntry makeOplogEntryForMigrate(const NamespaceString& ns,
+                                              repl::OpTime opTime,
+                                              repl::OpTypeEnum opType,
+                                              BSONObj object,
+                                              boost::optional<BSONObj> object2,
+                                              const OperationSessionInfo& sessionInfo,
+                                              Date_t wallClockTime) {
+        return repl::OplogEntry(opTime,         // optime
+                                boost::none,    // hash
+                                opType,         // opType
+                                ns,             // namespace
+                                boost::none,    // uuid
+                                true,           // fromMigrate
+                                0,              // version
+                                object,         // o
+                                object2,        // o2
+                                sessionInfo,    // sessionInfo
+                                boost::none,    // false
+                                wallClockTime,  // wall clock time
+                                boost::none,    // statement id
+                                boost::none,    // optime of previous write within same transaction
+                                boost::none,    // pre-image optime
+                                boost::none);   // post-image optime
+    }
+
     void checkTxnTable(const OperationSessionInfo& sessionInfo,
                        const repl::OpTime& expectedOpTime,
                        Date_t expectedWallClock) {
@@ -1851,6 +1879,91 @@ TEST_F(SyncTailTxnTableTest, MultiApplyUpdatesTheTransactionTable) {
         client.findOne(NamespaceString::kSessionTransactionsTableNamespace.ns(),
                        BSON(SessionTxnRecord::kSessionIdFieldName << lsidNoTxn.toBSON()));
     ASSERT_TRUE(resultNoTxn.isEmpty());
+}
+
+TEST_F(SyncTailTxnTableTest, SessionMigrationNoOpEntriesShouldUpdateTxnTable) {
+    const auto insertLsid = makeLogicalSessionIdForTest();
+    OperationSessionInfo insertSessionInfo;
+    insertSessionInfo.setSessionId(insertLsid);
+    insertSessionInfo.setTxnNumber(3);
+    auto date = Date_t::now();
+
+    auto innerOplog = makeOplogEntry(nss(),
+                                     {Timestamp(10, 10), 1},
+                                     repl::OpTypeEnum::kInsert,
+                                     BSON("_id" << 1),
+                                     boost::none,
+                                     insertSessionInfo,
+                                     date);
+
+    auto outerInsertDate = Date_t::now();
+    auto insertOplog = makeOplogEntryForMigrate(nss(),
+                                                {Timestamp(40, 0), 1},
+                                                repl::OpTypeEnum::kNoop,
+                                                BSON("$sessionMigrateInfo" << 1),
+                                                innerOplog.toBSON(),
+                                                insertSessionInfo,
+                                                outerInsertDate);
+
+    auto writerPool = OplogApplier::makeWriterPool();
+    SyncTail syncTail(
+        nullptr, getConsistencyMarkers(), getStorageInterface(), multiSyncApply, writerPool.get());
+    ASSERT_OK(syncTail.multiApply(_opCtx.get(), {insertOplog}));
+
+    checkTxnTable(insertSessionInfo, {Timestamp(40, 0), 1}, outerInsertDate);
+}
+
+TEST_F(SyncTailTxnTableTest, PreImageNoOpEntriesShouldNotUpdateTxnTable) {
+    const auto preImageLsid = makeLogicalSessionIdForTest();
+    OperationSessionInfo preImageSessionInfo;
+    preImageSessionInfo.setSessionId(preImageLsid);
+    preImageSessionInfo.setTxnNumber(3);
+    auto preImageDate = Date_t::now();
+
+    auto preImageOplog = makeOplogEntryForMigrate(nss(),
+                                                  {Timestamp(30, 0), 1},
+                                                  repl::OpTypeEnum::kNoop,
+                                                  BSON("_id" << 1),
+                                                  boost::none,
+                                                  preImageSessionInfo,
+                                                  preImageDate);
+
+    auto writerPool = OplogApplier::makeWriterPool();
+    SyncTail syncTail(
+        nullptr, getConsistencyMarkers(), getStorageInterface(), multiSyncApply, writerPool.get());
+    ASSERT_OK(syncTail.multiApply(_opCtx.get(), {preImageOplog}));
+
+    DBDirectClient client(_opCtx.get());
+    auto result = client.findOne(NamespaceString::kSessionTransactionsTableNamespace.ns(),
+                                 {BSON(SessionTxnRecord::kSessionIdFieldName
+                                       << preImageSessionInfo.getSessionId()->toBSON())});
+    ASSERT_TRUE(result.isEmpty());
+}
+
+TEST_F(SyncTailTxnTableTest, NonMigrateNoOpEntriesShouldNotUpdateTxnTable) {
+    const auto lsid = makeLogicalSessionIdForTest();
+    OperationSessionInfo sessionInfo;
+    sessionInfo.setSessionId(lsid);
+    sessionInfo.setTxnNumber(3);
+
+    auto oplog = makeOplogEntry(nss(),
+                                {Timestamp(30, 0), 1},
+                                repl::OpTypeEnum::kNoop,
+                                BSON("_id" << 1),
+                                boost::none,
+                                sessionInfo,
+                                Date_t::now());
+
+    auto writerPool = OplogApplier::makeWriterPool();
+    SyncTail syncTail(
+        nullptr, getConsistencyMarkers(), getStorageInterface(), multiSyncApply, writerPool.get());
+    ASSERT_OK(syncTail.multiApply(_opCtx.get(), {oplog}));
+
+    DBDirectClient client(_opCtx.get());
+    auto result = client.findOne(
+        NamespaceString::kSessionTransactionsTableNamespace.ns(),
+        {BSON(SessionTxnRecord::kSessionIdFieldName << sessionInfo.getSessionId()->toBSON())});
+    ASSERT_TRUE(result.isEmpty());
 }
 
 TEST_F(IdempotencyTest, EmptyCappedNamespaceNotFound) {

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit ed903235b471eaff528dd060e918e8936a360f6d [formerly 9638317daa5300efdd43acfa4688e5c739beff4d])
Former-commit-id: 39f932b0a4f580f7c9dcefb8b3a47335fdb4539f

src/mongo/db/repl/sync_tail_test.cpp
@@ -1704,6 +1704,34 @@ public:
                                 boost::none);   // post-image optime
     }
 
+    /**
+     * Creates an OplogEntry with given parameters and preset defaults for this test suite.
+     */
+    repl::OplogEntry makeOplogEntryForMigrate(const NamespaceString& ns,
+                                              repl::OpTime opTime,
+                                              repl::OpTypeEnum opType,
+                                              BSONObj object,
+                                              boost::optional<BSONObj> object2,
+                                              const OperationSessionInfo& sessionInfo,
+                                              Date_t wallClockTime) {
+        return repl::OplogEntry(opTime,         // optime
+                                1LL,            // hash
+                                opType,         // opType
+                                ns,             // namespace
+                                boost::none,    // uuid
+                                true,           // fromMigrate
+                                0,              // version
+                                object,         // o
+                                object2,        // o2
+                                sessionInfo,    // sessionInfo
+                                boost::none,    // false
+                                wallClockTime,  // wall clock time
+                                boost::none,    // statement id
+                                boost::none,    // optime of previous write within same transaction
+                                boost::none,    // pre-image optime
+                                boost::none);   // post-image optime
+    }
+
     void checkTxnTable(const OperationSessionInfo& sessionInfo,
                        const repl::OpTime& expectedOpTime,
                        Date_t expectedWallClock) {
@@ -1952,6 +1980,91 @@ TEST_F(SyncTailTxnTableTest, MultiApplyUpdatesTheTransactionTable) {
         client.findOne(NamespaceString::kSessionTransactionsTableNamespace.ns(),
                        BSON(SessionTxnRecord::kSessionIdFieldName << lsidNoTxn.toBSON()));
     ASSERT_TRUE(resultNoTxn.isEmpty());
+}
+
+TEST_F(SyncTailTxnTableTest, SessionMigrationNoOpEntriesShouldUpdateTxnTable) {
+    const auto insertLsid = makeLogicalSessionIdForTest();
+    OperationSessionInfo insertSessionInfo;
+    insertSessionInfo.setSessionId(insertLsid);
+    insertSessionInfo.setTxnNumber(3);
+    auto date = Date_t::now();
+
+    auto innerOplog = makeOplogEntry(nss(),
+                                     {Timestamp(10, 10), 1},
+                                     repl::OpTypeEnum::kInsert,
+                                     BSON("_id" << 1),
+                                     boost::none,
+                                     insertSessionInfo,
+                                     date);
+
+    auto outerInsertDate = Date_t::now();
+    auto insertOplog = makeOplogEntryForMigrate(nss(),
+                                                {Timestamp(40, 0), 1},
+                                                repl::OpTypeEnum::kNoop,
+                                                BSON("$sessionMigrateInfo" << 1),
+                                                innerOplog.toBSON(),
+                                                insertSessionInfo,
+                                                outerInsertDate);
+
+    auto writerPool = SyncTail::makeWriterPool();
+    SyncTail syncTail(
+        nullptr, getConsistencyMarkers(), getStorageInterface(), multiSyncApply, writerPool.get());
+    ASSERT_OK(syncTail.multiApply(_opCtx.get(), {insertOplog}));
+
+    checkTxnTable(insertSessionInfo, {Timestamp(40, 0), 1}, outerInsertDate);
+}
+
+TEST_F(SyncTailTxnTableTest, PreImageNoOpEntriesShouldNotUpdateTxnTable) {
+    const auto preImageLsid = makeLogicalSessionIdForTest();
+    OperationSessionInfo preImageSessionInfo;
+    preImageSessionInfo.setSessionId(preImageLsid);
+    preImageSessionInfo.setTxnNumber(3);
+    auto preImageDate = Date_t::now();
+
+    auto preImageOplog = makeOplogEntryForMigrate(nss(),
+                                                  {Timestamp(30, 0), 1},
+                                                  repl::OpTypeEnum::kNoop,
+                                                  BSON("_id" << 1),
+                                                  boost::none,
+                                                  preImageSessionInfo,
+                                                  preImageDate);
+
+    auto writerPool = SyncTail::makeWriterPool();
+    SyncTail syncTail(
+        nullptr, getConsistencyMarkers(), getStorageInterface(), multiSyncApply, writerPool.get());
+    ASSERT_OK(syncTail.multiApply(_opCtx.get(), {preImageOplog}));
+
+    DBDirectClient client(_opCtx.get());
+    auto result = client.findOne(NamespaceString::kSessionTransactionsTableNamespace.ns(),
+                                 {BSON(SessionTxnRecord::kSessionIdFieldName
+                                       << preImageSessionInfo.getSessionId()->toBSON())});
+    ASSERT_TRUE(result.isEmpty());
+}
+
+TEST_F(SyncTailTxnTableTest, NonMigrateNoOpEntriesShouldNotUpdateTxnTable) {
+    const auto lsid = makeLogicalSessionIdForTest();
+    OperationSessionInfo sessionInfo;
+    sessionInfo.setSessionId(lsid);
+    sessionInfo.setTxnNumber(3);
+
+    auto oplog = makeOplogEntry(nss(),
+                                {Timestamp(30, 0), 1},
+                                repl::OpTypeEnum::kNoop,
+                                BSON("_id" << 1),
+                                boost::none,
+                                sessionInfo,
+                                Date_t::now());
+
+    auto writerPool = SyncTail::makeWriterPool();
+    SyncTail syncTail(
+        nullptr, getConsistencyMarkers(), getStorageInterface(), multiSyncApply, writerPool.get());
+    ASSERT_OK(syncTail.multiApply(_opCtx.get(), {oplog}));
+
+    DBDirectClient client(_opCtx.get());
+    auto result = client.findOne(
+        NamespaceString::kSessionTransactionsTableNamespace.ns(),
+        {BSON(SessionTxnRecord::kSessionIdFieldName << sessionInfo.getSessionId()->toBSON())});
+    ASSERT_TRUE(result.isEmpty());
 }
 
 TEST_F(IdempotencyTest, EmptyCappedNamespaceNotFound) {