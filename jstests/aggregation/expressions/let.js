mongodb/mongo

SERVER-41065 Make agg evaluate() thread safe by passing 'Variables' as a parameter

jstests/aggregation/expressions/let.js
@@ -1,0 +1,132 @@
+/**
+ * Basic integration tests for the $let expression.
+ */
+(function() {
+    "use strict";
+
+    load("jstests/aggregation/extras/utils.js");  // For assertErrorCode.
+
+    let coll = db.agg_expr_let;
+    coll.drop();
+    assert.commandWorked(coll.insert({zero: 0, one: 1, two: 2, three: 3, nested: {four: 4}}));
+
+    function testExpr(expression, output) {
+        const res = coll.aggregate([{$project: {output: expression}}]).toArray();
+        assert.eq(res.length, 1, tojson(res));
+        assert.eq(res[0].output, output, tojson(res));
+
+        // Test in group:
+        const result = coll.aggregate({$group: {_id: 0, res: {$sum: expression}}}).toArray();
+        assert.eq(result, [{_id: 0, res: output}]);
+    }
+
+    // Basic tests.
+    testExpr('$two', 2);
+    testExpr('$$CURRENT.two', 2);
+    testExpr('$$ROOT.two', 2);
+
+    // Using sub expressions.
+    testExpr({$add: ['$two', '$$CURRENT.three']}, 5);
+    testExpr({$add: ['$$CURRENT.two', '$$ROOT.nested.four']}, 6);
+
+    // Verify that the variables defined in $let work.
+    testExpr({$let: {vars: {a: 10}, in : '$$a'}}, 10);
+    testExpr({$let: {vars: {a: '$zero'}, in : '$$a'}}, 0);
+    testExpr({$let: {vars: {a: {$add: ['$one', '$two']}, b: 10}, in : {$multiply: ['$$a', '$$b']}}},
+             30);
+
+    // Verify that the outer level variable works in inner level $let.
+    testExpr({
+        $let: {
+            vars: {var1: 1},
+            in : {$let: {vars: {var2: "$$var1"}, in : {$sum: ["$$var1", "$$var2"]}}}
+        }
+    },
+             2);
+
+    // Verify that the outer level variables get overwritten by inner level variables.
+    testExpr({
+        $let: {
+            vars: {var1: "$one"},
+            in : {$let: {vars: {var2: "$$var1", var1: 3}, in : {$sum: ["$$var2", "$$var1"]}}}
+        }
+    },
+             4);
+
+    // $let changing CURRENT
+    testExpr({$let: {vars: {CURRENT: '$$ROOT.nested'}, in : {$multiply: ['$four', '$$ROOT.two']}}},
+             8);
+    testExpr({
+        $let: {
+            vars: {CURRENT: '$$CURRENT.nested'},  // using original value of CURRENT
+            in : {$multiply: ['$four', '$$ROOT.two']}
+        }
+    },
+             8);
+    testExpr({
+        $let: {
+            vars: {CURRENT: '$nested'},  // same as last
+            in : {$multiply: ['$four', '$$ROOT.two']}
+        }
+    },
+             8);
+    testExpr({
+        $let: {
+            vars: {CURRENT: {$const: {ten: 10}}},  // "artificial" object
+            in : {$multiply: ['$ten', '$$ROOT.two']}
+        }
+    },
+             20);
+    testExpr({
+        $let: {
+            vars: {CURRENT: '$three'},  // sets current to the number 3 (not an object)
+            in : {$multiply: ['$$CURRENT', '$$ROOT.two']}
+        }
+    },
+             6);
+
+    // Swapping with $let (ensures there is no ordering dependency in vars).
+    testExpr({
+        $let: {
+            vars: {x: 6, y: 10},
+            in : {
+                $let: {
+                    vars: {x: '$$y', y: '$$x'},  // now {x:10, y:6}
+                    in : {$subtract: ['$$x', '$$y']}
+                }
+            }
+        }
+    },            // Not commutative!
+             4);  // 10-6 not 6-10 or 6-6
+
+    // Unicode is allowed.
+    testExpr({$let: {vars: {'日本語': 10}, in : '$$日本語'}},
+             10);  // Japanese for "Japanese language".
+
+    // Can use ROOT and CURRENT directly with no subfield (SERVER-5916).
+    coll.drop();
+    coll.insert({_id: 'obj'});
+    assert.eq(coll.aggregate({$project: {_id: 0, obj: '$$ROOT'}}).toArray(), [{obj: {_id: 'obj'}}]);
+    assert.eq(coll.aggregate({$project: {_id: 0, obj: '$$CURRENT'}}).toArray(),
+              [{obj: {_id: 'obj'}}]);
+    assert.eq(coll.aggregate({$group: {_id: 0, objs: {$push: '$$ROOT'}}}).toArray(),
+              [{_id: 0, objs: [{_id: 'obj'}]}]);
+    assert.eq(coll.aggregate({$group: {_id: 0, objs: {$push: '$$CURRENT'}}}).toArray(),
+              [{_id: 0, objs: [{_id: 'obj'}]}]);
+
+    // Check name validity checks.
+    assertErrorCode(coll, {$project: {a: {$let: {vars: {ROOT: 1}, in : '$$ROOT'}}}}, 16867);
+    assertErrorCode(coll, {$project: {a: {$let: {vars: {FOO: 1}, in : '$$FOO'}}}}, 16867);
+    assertErrorCode(coll, {$project: {a: {$let: {vars: {_underbar: 1}, in : '$$FOO'}}}}, 16867);
+    assertErrorCode(coll, {$project: {a: {$let: {vars: {'a.b': 1}, in : '$$FOO'}}}}, 16868);
+    assertErrorCode(coll, {$project: {a: {$let: {vars: {'a b': 1}, in : '$$FOO'}}}}, 16868);
+    assertErrorCode(coll, {$project: {a: '$$_underbar'}}, 16870);
+    assertErrorCode(coll, {$project: {a: '$$with spaces'}}, 16871);
+
+    // Verify that variables defined in '$let' cannot be used to initialize other variables.
+    assertErrorCode(
+        coll,
+        [{$project: {output: {$let: {vars: {var1: "$one", var2: "$$var1"}, in : "$$var1"}}}}],
+        17276);
+
+}());

jstests/concurrency/fsm_workloads/schema_validator_with_expr_variables.js
@@ -1,0 +1,80 @@
+/**
+ * Test to verify that the schema validator works correctly in a multi-threaded environment, when
+ * $expr uses expressions which mutate variable values while executing ($let, $map etc).
+ * @tags: [requires_non_retryable_writes]
+ */
+
+"use strict";
+
+var $config = (function() {
+    function setup(db, collName) {
+        for (let i = 0; i < 200; ++i) {
+            assertAlways.commandWorked(
+                db[collName].insert({_id: i, a: i, one: 1, counter: 0, array: [0, i]}));
+        }
+
+        // Add a validator which checks that field 'a' has value 5 and sum of the elements in field
+        // 'array' is 5. The expression is purposefully complex so that it can create a stress on
+        // expressions with variables.
+        assertAlways.commandWorked(db.runCommand({
+            collMod: collName,
+            validator: {
+                $expr: {
+                    $and: [
+                        {
+                          $eq: [
+                              5,
+                              {
+                                $let: {
+                                    vars: {item: {$multiply: ["$a", "$one"]}},
+                                    in : {$multiply: ["$$item", "$one"]}
+                                }
+                              }
+                          ]
+                        },
+                        {
+                          $eq: [
+                              5,
+                              {
+                                $sum: {
+                                    $map:
+                                        {"input": "$array", "as": "item", "in": "$$item"}
+                                }
+                              }
+                          ]
+                        }
+                    ]
+                }
+            }
+        }));
+    }
+
+    const states = {
+        applyValidator: function(db, collName) {
+            assertAlways.commandWorked(db[collName].update({_id: 5}, {$inc: {counter: 1}}));
+            assertAlways.commandFailedWithCode(
+                db[collName].update({_id: 4}, {$set: {a: 4}, $inc: {counter: 1}}),
+                ErrorCodes.DocumentValidationFailure);
+
+            // Update all the documents in the collection.
+            assertAlways.commandWorked(db[collName].update(
+                {}, {$set: {a: 5, array: [2, 3]}, $inc: {counter: 1}}, {multi: true}));
+
+            // Validation fails when elements of 'array' doesn't add up to 5.
+            assertAlways.commandFailedWithCode(
+                db[collName].update({_id: 4}, {$set: {a: 5, array: [2, 2]}}),
+                ErrorCodes.DocumentValidationFailure);
+        }
+    };
+
+    let transitions = {applyValidator: {applyValidator: 1}};
+
+    return {
+        threadCount: 50,
+        iterations: 100,
+        states: states,
+        startState: "applyValidator",
+        transitions: transitions,
+        setup: setup
+    };
+})();

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit 25c00d3a1c8e2b09adc09bb768ae1f93815fc528 [formerly ab9d3aaad1cb9ad42063c1291ea07e321260a3d1])
Former-commit-id: 1e02fdb576d8167562587cd18c5c7132e9675639

jstests/aggregation/expressions/let.js
@@ -1,0 +1,132 @@
+/**
+ * Basic integration tests for the $let expression.
+ */
+(function() {
+    "use strict";
+
+    load("jstests/aggregation/extras/utils.js");  // For assertErrorCode.
+
+    let coll = db.agg_expr_let;
+    coll.drop();
+    assert.writeOK(coll.insert({zero: 0, one: 1, two: 2, three: 3, nested: {four: 4}}));
+
+    function testExpr(expression, output) {
+        const res = coll.aggregate([{$project: {output: expression}}]).toArray();
+        assert.eq(res.length, 1, tojson(res));
+        assert.eq(res[0].output, output, tojson(res));
+
+        // Test in group:
+        const result = coll.aggregate({$group: {_id: 0, res: {$sum: expression}}}).toArray();
+        assert.eq(result, [{_id: 0, res: output}]);
+    }
+
+    // Basic tests.
+    testExpr('$two', 2);
+    testExpr('$$CURRENT.two', 2);
+    testExpr('$$ROOT.two', 2);
+
+    // Using sub expressions.
+    testExpr({$add: ['$two', '$$CURRENT.three']}, 5);
+    testExpr({$add: ['$$CURRENT.two', '$$ROOT.nested.four']}, 6);
+
+    // Verify that the variables defined in $let work.
+    testExpr({$let: {vars: {a: 10}, in : '$$a'}}, 10);
+    testExpr({$let: {vars: {a: '$zero'}, in : '$$a'}}, 0);
+    testExpr({$let: {vars: {a: {$add: ['$one', '$two']}, b: 10}, in : {$multiply: ['$$a', '$$b']}}},
+             30);
+
+    // Verify that the outer level variable works in inner level $let.
+    testExpr({
+        $let: {
+            vars: {var1: 1},
+            in : {$let: {vars: {var2: "$$var1"}, in : {$sum: ["$$var1", "$$var2"]}}}
+        }
+    },
+             2);
+
+    // Verify that the outer level variables get overwritten by inner level variables.
+    testExpr({
+        $let: {
+            vars: {var1: "$one"},
+            in : {$let: {vars: {var2: "$$var1", var1: 3}, in : {$sum: ["$$var2", "$$var1"]}}}
+        }
+    },
+             4);
+
+    // $let changing CURRENT
+    testExpr({$let: {vars: {CURRENT: '$$ROOT.nested'}, in : {$multiply: ['$four', '$$ROOT.two']}}},
+             8);
+    testExpr({
+        $let: {
+            vars: {CURRENT: '$$CURRENT.nested'},  // using original value of CURRENT
+            in : {$multiply: ['$four', '$$ROOT.two']}
+        }
+    },
+             8);
+    testExpr({
+        $let: {
+            vars: {CURRENT: '$nested'},  // same as last
+            in : {$multiply: ['$four', '$$ROOT.two']}
+        }
+    },
+             8);
+    testExpr({
+        $let: {
+            vars: {CURRENT: {$const: {ten: 10}}},  // "artificial" object
+            in : {$multiply: ['$ten', '$$ROOT.two']}
+        }
+    },
+             20);
+    testExpr({
+        $let: {
+            vars: {CURRENT: '$three'},  // sets current to the number 3 (not an object)
+            in : {$multiply: ['$$CURRENT', '$$ROOT.two']}
+        }
+    },
+             6);
+
+    // Swapping with $let (ensures there is no ordering dependency in vars).
+    testExpr({
+        $let: {
+            vars: {x: 6, y: 10},
+            in : {
+                $let: {
+                    vars: {x: '$$y', y: '$$x'},  // now {x:10, y:6}
+                    in : {$subtract: ['$$x', '$$y']}
+                }
+            }
+        }
+    },            // Not commutative!
+             4);  // 10-6 not 6-10 or 6-6
+
+    // Unicode is allowed.
+    testExpr({$let: {vars: {'日本語': 10}, in : '$$日本語'}},
+             10);  // Japanese for "Japanese language".
+
+    // Can use ROOT and CURRENT directly with no subfield (SERVER-5916).
+    coll.drop();
+    coll.insert({_id: 'obj'});
+    assert.eq(coll.aggregate({$project: {_id: 0, obj: '$$ROOT'}}).toArray(), [{obj: {_id: 'obj'}}]);
+    assert.eq(coll.aggregate({$project: {_id: 0, obj: '$$CURRENT'}}).toArray(),
+              [{obj: {_id: 'obj'}}]);
+    assert.eq(coll.aggregate({$group: {_id: 0, objs: {$push: '$$ROOT'}}}).toArray(),
+              [{_id: 0, objs: [{_id: 'obj'}]}]);
+    assert.eq(coll.aggregate({$group: {_id: 0, objs: {$push: '$$CURRENT'}}}).toArray(),
+              [{_id: 0, objs: [{_id: 'obj'}]}]);
+
+    // Check name validity checks.
+    assertErrorCode(coll, {$project: {a: {$let: {vars: {ROOT: 1}, in : '$$ROOT'}}}}, 16867);
+    assertErrorCode(coll, {$project: {a: {$let: {vars: {FOO: 1}, in : '$$FOO'}}}}, 16867);
+    assertErrorCode(coll, {$project: {a: {$let: {vars: {_underbar: 1}, in : '$$FOO'}}}}, 16867);
+    assertErrorCode(coll, {$project: {a: {$let: {vars: {'a.b': 1}, in : '$$FOO'}}}}, 16868);
+    assertErrorCode(coll, {$project: {a: {$let: {vars: {'a b': 1}, in : '$$FOO'}}}}, 16868);
+    assertErrorCode(coll, {$project: {a: '$$_underbar'}}, 16870);
+    assertErrorCode(coll, {$project: {a: '$$with spaces'}}, 16871);
+
+    // Verify that variables defined in '$let' cannot be used to initialize other variables.
+    assertErrorCode(
+        coll,
+        [{$project: {output: {$let: {vars: {var1: "$one", var2: "$$var1"}, in : "$$var1"}}}}],
+        17276);
+
+}());

jstests/concurrency/fsm_workloads/schema_validator_with_expr_variables.js
@@ -1,0 +1,77 @@
+/**
+ * Test to verify that the schema validator works correctly in a multi-threaded environment, when
+ * $expr uses expressions which mutate variable values while executing ($let, $map etc).
+ * @tags: [requires_non_retryable_writes]
+ */
+
+"use strict";
+
+var $config = (function() {
+    function setup(db, collName) {
+        for (let i = 0; i < 200; ++i) {
+            assertAlways.writeOK(
+                db[collName].insert({_id: i, a: i, one: 1, counter: 0, array: [0, i]}));
+        }
+
+        // Add a validator which checks that field 'a' has value 5 and sum of the elements in field
+        // 'array' is 5. The expression is purposefully complex so that it can create a stress on
+        // expressions with variables.
+        assertAlways.commandWorked(db.runCommand({
+            collMod: collName,
+            validator: {
+                $expr: {
+                    $and: [
+                        {
+                          $eq: [
+                              5,
+                              {
+                                $let: {
+                                    vars: {item: {$multiply: ["$a", "$one"]}},
+                                    in : {$multiply: ["$$item", "$one"]}
+                                }
+                              }
+                          ]
+                        },
+                        {
+                          $eq: [
+                              5,
+                              {
+                                $sum: {
+                                    $map:
+                                        {"input": "$array", "as": "item", "in": "$$item"}
+                                }
+                              }
+                          ]
+                        }
+                    ]
+                }
+            }
+        }));
+    }
+
+    const states = {
+        applyValidator: function(db, collName) {
+            assertAlways.writeOK(db[collName].update({_id: 5}, {$inc: {counter: 1}}));
+            assertAlways.writeError(
+                db[collName].update({_id: 4}, {$set: {a: 4}, $inc: {counter: 1}}));
+
+            // Update all the documents in the collection.
+            assertAlways.writeOK(db[collName].update(
+                {}, {$set: {a: 5, array: [2, 3]}, $inc: {counter: 1}}, {multi: true}));
+
+            // Validation fails when elements of 'array' doesn't add up to 5.
+            assertAlways.writeError(db[collName].update({_id: 4}, {$set: {a: 5, array: [2, 2]}}));
+        }
+    };
+
+    let transitions = {applyValidator: {applyValidator: 1}};
+
+    return {
+        threadCount: 50,
+        iterations: 100,
+        states: states,
+        startState: "applyValidator",
+        transitions: transitions,
+        setup: setup
+    };
+})();