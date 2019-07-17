mongodb/mongo

SERVER-40785 Create WaitForMajorityService to allow waiting for write concern asynchronously

src/mongo/db/repl/replication_coordinator_mock.h
@@ -287,9 +287,10 @@ public:
 
     /**
      * Sets the function to generate the return value for calls to awaitReplication().
-     * 'opTime' is the optime passed to awaitReplication().
+     * 'OperationContext' and 'opTime' are the parameters passed to awaitReplication().
      */
-    using AwaitReplicationReturnValueFunction = std::function<StatusAndDuration(const OpTime&)>;
+    using AwaitReplicationReturnValueFunction =
+        std::function<StatusAndDuration(OperationContext*, const OpTime&)>;
     void setAwaitReplicationReturnValueFunction(
         AwaitReplicationReturnValueFunction returnValueFunction);
 
@@ -325,7 +326,8 @@ private:
     OpTime _myLastAppliedOpTime;
     Date_t _myLastAppliedWallTime;
     ReplSetConfig _getConfigReturnValue;
-    AwaitReplicationReturnValueFunction _awaitReplicationReturnValueFunction = [](const OpTime&) {
+    AwaitReplicationReturnValueFunction _awaitReplicationReturnValueFunction = [](OperationContext*,
+                                                                                  const OpTime&) {
         return StatusAndDuration(Status::OK(), Milliseconds(0));
     };
     bool _alwaysAllowWrites = false;

src/mongo/s/sharding_initialization.cpp
@@ -67,6 +67,7 @@
 #include "mongo/s/sharding_task_executor.h"
 #include "mongo/s/sharding_task_executor_pool_controller.h"
 #include "mongo/s/sharding_task_executor_pool_gen.h"
+#include "mongo/stdx/thread.h"
 #include "mongo/util/concurrency/thread_pool.h"
 #include "mongo/util/exit.h"
 #include "mongo/util/log.h"
@@ -100,15 +101,16 @@ std::unique_ptr<ShardingCatalogClient> makeCatalogClient(ServiceContext* service
 
 std::shared_ptr<executor::TaskExecutor> makeShardingFixedTaskExecutor(
     std::unique_ptr<NetworkInterface> net) {
-    auto executor =
-        std::make_unique<ThreadPoolTaskExecutor>(std::make_unique<ThreadPool>([] {
-                                                     ThreadPool::Options opts;
-                                                     opts.poolName = "Sharding-Fixed";
-                                                     opts.maxThreads =
-                                                         ThreadPool::Options::kUnlimited;
-                                                     return opts;
-                                                 }()),
-                                                 std::move(net));
+    auto executor = std::make_unique<ThreadPoolTaskExecutor>(
+        std::make_unique<ThreadPool>([] {
+            ThreadPool::Options opts;
+            opts.poolName = "Sharding-Fixed";
+
+            const auto maxThreads = stdx::thread::hardware_concurrency();
+            opts.maxThreads = maxThreads == 0 ? 16 : 2 * maxThreads;
+            return opts;
+        }()),
+        std::move(net));
 
     return std::make_shared<executor::ShardingTaskExecutor>(std::move(executor));
 }

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit b1e188df10d723337b499c84b30afc01662c48e0 [formerly 2dfbaadb85f32869c75f87117d9f6e98b4e948ea])
Former-commit-id: 31016beb5503c9ecf783826fb3b761ea92bfff7f

src/mongo/db/repl/replication_coordinator_mock.h
@@ -286,9 +286,10 @@ public:
 
     /**
      * Sets the function to generate the return value for calls to awaitReplication().
-     * 'opTime' is the optime passed to awaitReplication().
+     * 'OperationContext' and 'opTime' are the parameters passed to awaitReplication().
      */
-    using AwaitReplicationReturnValueFunction = stdx::function<StatusAndDuration(const OpTime&)>;
+    using AwaitReplicationReturnValueFunction =
+        stdx::function<StatusAndDuration(OperationContext*, const OpTime&)>;
     void setAwaitReplicationReturnValueFunction(
         AwaitReplicationReturnValueFunction returnValueFunction);
 
@@ -324,7 +325,8 @@ private:
     OpTime _myLastAppliedOpTime;
     Date_t _myLastAppliedWallTime;
     ReplSetConfig _getConfigReturnValue;
-    AwaitReplicationReturnValueFunction _awaitReplicationReturnValueFunction = [](const OpTime&) {
+    AwaitReplicationReturnValueFunction _awaitReplicationReturnValueFunction = [](OperationContext*,
+                                                                                  const OpTime&) {
         return StatusAndDuration(Status::OK(), Milliseconds(0));
     };
     bool _alwaysAllowWrites = false;

src/mongo/s/sharding_initialization.cpp
@@ -67,6 +67,7 @@
 #include "mongo/s/sharding_task_executor_pool_controller.h"
 #include "mongo/s/sharding_task_executor_pool_gen.h"
 #include "mongo/stdx/memory.h"
+#include "mongo/stdx/thread.h"
 #include "mongo/util/concurrency/thread_pool.h"
 #include "mongo/util/exit.h"
 #include "mongo/util/log.h"
@@ -100,15 +101,16 @@ std::unique_ptr<ShardingCatalogClient> makeCatalogClient(ServiceContext* service
 
 std::shared_ptr<executor::TaskExecutor> makeShardingFixedTaskExecutor(
     std::unique_ptr<NetworkInterface> net) {
-    auto executor =
-        stdx::make_unique<ThreadPoolTaskExecutor>(stdx::make_unique<ThreadPool>([] {
-                                                      ThreadPool::Options opts;
-                                                      opts.poolName = "Sharding-Fixed";
-                                                      opts.maxThreads =
-                                                          ThreadPool::Options::kUnlimited;
-                                                      return opts;
-                                                  }()),
-                                                  std::move(net));
+    auto executor = stdx::make_unique<ThreadPoolTaskExecutor>(
+        stdx::make_unique<ThreadPool>([] {
+            ThreadPool::Options opts;
+            opts.poolName = "Sharding-Fixed";
+
+            const auto maxThreads = stdx::thread::hardware_concurrency();
+            opts.maxThreads = maxThreads == 0 ? 16 : 2 * maxThreads;
+            return opts;
+        }()),
+        std::move(net));
 
     return std::make_shared<executor::ShardingTaskExecutor>(std::move(executor));
 }