mongodb/mongo

SERVER-40785 Change sharding fixed and arbitrary executors from unique_ptr to shared_ptr

src/mongo/s/sharding_mongod_test_fixture.cpp
@@ -171,13 +171,13 @@ std::unique_ptr<executor::TaskExecutorPool> ShardingMongodTestFixture::makeTaskE
     // note that the ThreadPoolMock uses the NetworkInterfaceMock's threads to run tasks, which is
     // again just the (single) thread the unit test is running on. Therefore, all tasks, local and
     // remote, must be carried out synchronously by the test thread.
-    auto fixedTaskExecutor = makeThreadPoolTestExecutor(std::move(netForFixedTaskExecutor));
+    auto fixedTaskExecutor = makeSharedThreadPoolTestExecutor(std::move(netForFixedTaskExecutor));
     _networkTestEnv = std::make_unique<NetworkTestEnv>(fixedTaskExecutor.get(), _mockNetwork);
 
     // Set up (one) TaskExecutor for the set of arbitrary TaskExecutors.
-    std::vector<std::unique_ptr<executor::TaskExecutor>> arbitraryExecutorsForExecutorPool;
+    std::vector<std::shared_ptr<executor::TaskExecutor>> arbitraryExecutorsForExecutorPool;
     arbitraryExecutorsForExecutorPool.emplace_back(
-        makeThreadPoolTestExecutor(std::make_unique<executor::NetworkInterfaceMock>()));
+        makeSharedThreadPoolTestExecutor(std::make_unique<executor::NetworkInterfaceMock>()));
 
     // Set up the TaskExecutorPool with the fixed TaskExecutor and set of arbitrary TaskExecutors.
     auto executorPool = std::make_unique<executor::TaskExecutorPool>();
@@ -336,7 +336,7 @@ void ShardingMongodTestFixture::shutdownExecutorPool() {
     _executorPoolShutDown = true;
 }
 
-executor::TaskExecutor* ShardingMongodTestFixture::executor() const {
+std::shared_ptr<executor::TaskExecutor> ShardingMongodTestFixture::executor() const {
     invariant(Grid::get(operationContext())->getExecutorPool());
     return Grid::get(operationContext())->getExecutorPool()->getFixedExecutor();
 }

src/mongo/s/sharding_router_test_fixture.cpp
@@ -118,9 +118,8 @@ ShardingTestFixture::ShardingTestFixture() {
     auto fixedNet = std::make_unique<executor::NetworkInterfaceMock>();
     fixedNet->setEgressMetadataHook(makeMetadataHookList());
     _mockNetwork = fixedNet.get();
-    auto fixedExec = makeShardingTestExecutor(std::move(fixedNet));
-    _networkTestEnv = std::make_unique<NetworkTestEnv>(fixedExec.get(), _mockNetwork);
-    _executor = fixedExec.get();
+    _fixedExecutor = makeShardingTestExecutor(std::move(fixedNet));
+    _networkTestEnv = std::make_unique<NetworkTestEnv>(_fixedExecutor.get(), _mockNetwork);
 
     auto netForPool = std::make_unique<executor::NetworkInterfaceMock>();
     netForPool->setEgressMetadataHook(makeMetadataHookList());
@@ -128,11 +127,11 @@ ShardingTestFixture::ShardingTestFixture() {
     auto execForPool = makeShardingTestExecutor(std::move(netForPool));
     _networkTestEnvForPool =
         std::make_unique<NetworkTestEnv>(execForPool.get(), _mockNetworkForPool);
-    std::vector<std::unique_ptr<executor::TaskExecutor>> executorsForPool;
+    std::vector<std::shared_ptr<executor::TaskExecutor>> executorsForPool;
     executorsForPool.emplace_back(std::move(execForPool));
 
     auto executorPool = std::make_unique<executor::TaskExecutorPool>();
-    executorPool->addExecutors(std::move(executorsForPool), std::move(fixedExec));
+    executorPool->addExecutors(std::move(executorsForPool), _fixedExecutor);
 
     auto uniqueDistLockManager = std::make_unique<DistLockManagerMock>(nullptr);
     _distLockManager = uniqueDistLockManager.get();
@@ -193,8 +192,8 @@ ShardingTestFixture::~ShardingTestFixture() {
 }
 
 void ShardingTestFixture::shutdownExecutor() {
-    if (_executor)
-        _executor->shutdown();
+    if (_fixedExecutor)
+        _fixedExecutor->shutdown();
 }
 
 ShardingCatalogClient* ShardingTestFixture::catalogClient() const {
@@ -217,10 +216,10 @@ RemoteCommandTargeterMock* ShardingTestFixture::configTargeter() const {
     return _configTargeter;
 }
 
-executor::TaskExecutor* ShardingTestFixture::executor() const {
-    invariant(_executor);
+std::shared_ptr<executor::TaskExecutor> ShardingTestFixture::executor() const {
+    invariant(_fixedExecutor);
 
-    return _executor;
+    return _fixedExecutor;
 }
 
 DistLockManagerMock* ShardingTestFixture::distLock() const {

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit 254f03cf55e1027bb6fad0643b0cf84762efa8c7 [formerly d15b1f2e036e262f8ea976e04780aa366fa20ad4])
Former-commit-id: ed935ffffae8dd373063da350e4cb61547e20688

src/mongo/s/sharding_mongod_test_fixture.cpp
@@ -171,13 +171,13 @@ std::unique_ptr<executor::TaskExecutorPool> ShardingMongodTestFixture::makeTaskE
     // note that the ThreadPoolMock uses the NetworkInterfaceMock's threads to run tasks, which is
     // again just the (single) thread the unit test is running on. Therefore, all tasks, local and
     // remote, must be carried out synchronously by the test thread.
-    auto fixedTaskExecutor = makeThreadPoolTestExecutor(std::move(netForFixedTaskExecutor));
+    auto fixedTaskExecutor = makeSharedThreadPoolTestExecutor(std::move(netForFixedTaskExecutor));
     _networkTestEnv = stdx::make_unique<NetworkTestEnv>(fixedTaskExecutor.get(), _mockNetwork);
 
     // Set up (one) TaskExecutor for the set of arbitrary TaskExecutors.
-    std::vector<std::unique_ptr<executor::TaskExecutor>> arbitraryExecutorsForExecutorPool;
+    std::vector<std::shared_ptr<executor::TaskExecutor>> arbitraryExecutorsForExecutorPool;
     arbitraryExecutorsForExecutorPool.emplace_back(
-        makeThreadPoolTestExecutor(stdx::make_unique<executor::NetworkInterfaceMock>()));
+        makeSharedThreadPoolTestExecutor(stdx::make_unique<executor::NetworkInterfaceMock>()));
 
     // Set up the TaskExecutorPool with the fixed TaskExecutor and set of arbitrary TaskExecutors.
     auto executorPool = stdx::make_unique<executor::TaskExecutorPool>();
@@ -338,7 +338,7 @@ void ShardingMongodTestFixture::shutdownExecutorPool() {
     _executorPoolShutDown = true;
 }
 
-executor::TaskExecutor* ShardingMongodTestFixture::executor() const {
+std::shared_ptr<executor::TaskExecutor> ShardingMongodTestFixture::executor() const {
     invariant(Grid::get(operationContext())->getExecutorPool());
     return Grid::get(operationContext())->getExecutorPool()->getFixedExecutor();
 }

src/mongo/s/sharding_router_test_fixture.cpp
@@ -118,9 +118,8 @@ ShardingTestFixture::ShardingTestFixture() {
     auto fixedNet = stdx::make_unique<executor::NetworkInterfaceMock>();
     fixedNet->setEgressMetadataHook(makeMetadataHookList());
     _mockNetwork = fixedNet.get();
-    auto fixedExec = makeShardingTestExecutor(std::move(fixedNet));
-    _networkTestEnv = stdx::make_unique<NetworkTestEnv>(fixedExec.get(), _mockNetwork);
-    _executor = fixedExec.get();
+    _fixedExecutor = makeShardingTestExecutor(std::move(fixedNet));
+    _networkTestEnv = stdx::make_unique<NetworkTestEnv>(_fixedExecutor.get(), _mockNetwork);
 
     auto netForPool = stdx::make_unique<executor::NetworkInterfaceMock>();
     netForPool->setEgressMetadataHook(makeMetadataHookList());
@@ -128,11 +127,11 @@ ShardingTestFixture::ShardingTestFixture() {
     auto execForPool = makeShardingTestExecutor(std::move(netForPool));
     _networkTestEnvForPool =
         stdx::make_unique<NetworkTestEnv>(execForPool.get(), _mockNetworkForPool);
-    std::vector<std::unique_ptr<executor::TaskExecutor>> executorsForPool;
+    std::vector<std::shared_ptr<executor::TaskExecutor>> executorsForPool;
     executorsForPool.emplace_back(std::move(execForPool));
 
     auto executorPool = stdx::make_unique<executor::TaskExecutorPool>();
-    executorPool->addExecutors(std::move(executorsForPool), std::move(fixedExec));
+    executorPool->addExecutors(std::move(executorsForPool), _fixedExecutor);
 
     auto uniqueDistLockManager = stdx::make_unique<DistLockManagerMock>(nullptr);
     _distLockManager = uniqueDistLockManager.get();
@@ -195,8 +194,8 @@ ShardingTestFixture::~ShardingTestFixture() {
 }
 
 void ShardingTestFixture::shutdownExecutor() {
-    if (_executor)
-        _executor->shutdown();
+    if (_fixedExecutor)
+        _fixedExecutor->shutdown();
 }
 
 ShardingCatalogClient* ShardingTestFixture::catalogClient() const {
@@ -219,10 +218,10 @@ RemoteCommandTargeterMock* ShardingTestFixture::configTargeter() const {
     return _configTargeter;
 }
 
-executor::TaskExecutor* ShardingTestFixture::executor() const {
-    invariant(_executor);
+std::shared_ptr<executor::TaskExecutor> ShardingTestFixture::executor() const {
+    invariant(_fixedExecutor);
 
-    return _executor;
+    return _fixedExecutor;
 }
 
 DistLockManagerMock* ShardingTestFixture::distLock() const {