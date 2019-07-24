cc3e860d23c84e765123c4cdfcfbd4c934bb3b24
Tomislav Plavcic <tplavcic@gmail.com>
2019-07-24 16:50:02 +0200

    Fix python3 path in libs/python.js and ssl/ssl_ECDHE_suites.js (#462)
    (cherry picked from commit 17af9dc5275d2ae4c3a0570f3f6cacce930702a1 [formerly 7103d52911daef84e2a4da3bfe44d48f5a1c1a64])
    
    
    Former-commit-id: a4127579ee627c924f79cbb07fae78f83ba0f004

--- jstests/ssl/ssl_ECDHE_suites.js
+++ jstests/ssl/ssl_ECDHE_suites.js
@@ -37,7 +37,7 @@ const mongod = MongoRunner.runMongod(x509_options);
 // Use new toolchain python, if it exists
 let python_binary = '/opt/mongodbtoolchain/v3/bin/python3';
 if (runProgram('/bin/sh', '-c', 'ls ' + python_binary) !== 0) {
-    python_binary = '/opt/mongodbtoolchain/v3/bin/python3';
+    python_binary = '/usr/bin/python3';
 }
 
 // Run the tls cipher suite enumerator
@@ -75,7 +75,6 @@ suites.slice(suites.indexOf('tls1_1')).forEach(tlsVersion => {
         if (cipher.startsWith('ECDHE')) {
             hasECDHE = true;
         }
-
         if (cipher.startsWith('DHE')) {
             hasDHE = true;
         }
