mongodb/mongo

SERVER-42067 Ensure key Ordering offset does not exceed the maximum number of compound index keys

src/mongo/db/storage/key_string_test.cpp
@@ -212,6 +212,20 @@ TEST(TypeBitsTest, AppendLotsOfZeroTypeBits) {
     ASSERT(!typeBits.isLongEncoding());
 }
 
+TEST_F(KeyStringBuilderTest, TooManyElementsInCompoundKey) {
+    // Construct an illegal KeyString with more than the limit of 32 elements in a compound index
+    // key. Encode 33 kBoolTrue ('o') values.
+    const char* data = "ooooooooooooooooooooooooooooooooo";
+    const size_t size = 33;
+
+    KeyString::Builder ks(KeyString::Version::V1);
+    ks.resetFromBuffer(data, size);
+
+    ASSERT_THROWS_CODE(KeyString::toBsonSafe(data, size, ALL_ASCENDING, ks.getTypeBits()),
+                       AssertionException,
+                       ErrorCodes::Overflow);
+}
+
 TEST_F(KeyStringBuilderTest, Simple1) {
     BSONObj a = BSON("" << 5);
     BSONObj b = BSON("" << 6);

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit 8008c0886c9ec70f272231e4187a8c424d1f0485 [formerly fe4ca9897f8b8e91dc82fc15250e287da68cc83f])
Former-commit-id: 37d64e82e8a97475ef49fa94a9cd610d35cfda17

src/mongo/db/storage/key_string_test.cpp
@@ -202,6 +202,20 @@ TEST(TypeBitsTest, AppendLotsOfZeroTypeBits) {
     ASSERT(!typeBits.isLongEncoding());
 }
 
+TEST_F(KeyStringTest, TooManyElementsInCompoundKey) {
+    // Construct an illegal KeyString with more than the limit of 32 elements in a compound index
+    // key. Encode 33 kBoolTrue ('o') values.
+    const char* data = "ooooooooooooooooooooooooooooooooo";
+    const size_t size = 33;
+
+    KeyString ks(KeyString::Version::V1);
+    ks.resetFromBuffer(data, size);
+
+    ASSERT_THROWS_CODE(KeyString::toBsonSafe(data, size, ALL_ASCENDING, ks.getTypeBits()),
+                       AssertionException,
+                       ErrorCodes::Overflow);
+}
+
 TEST_F(KeyStringTest, Simple1) {
     BSONObj a = BSON("" << 5);
     BSONObj b = BSON("" << 6);