mongodb/mongo

SERVER-42069 uassert when decimal positive infinity is encoded in KeyString V0

src/mongo/db/storage/key_string_test.cpp
@@ -619,6 +619,23 @@ TEST_F(KeyStringBuilderTest, DoubleInvalidIntegerPartV0) {
         31209);
 }
 
+TEST_F(KeyStringBuilderTest, InvalidInfinityDecimalV0) {
+    // Encode a Decimal positive infinity in a V1 keystring.
+    mongo::KeyString::Builder ks(
+        mongo::KeyString::Version::V1, BSON("" << Decimal128::kPositiveInfinity), ALL_ASCENDING);
+
+    // Construct V0 type bits that indicate a NumberDecimal has been encoded.
+    mongo::KeyString::TypeBits tb(mongo::KeyString::Version::V0);
+    tb.appendNumberDecimal();
+
+    // The conversion to BSON will fail because Decimal positive infinity cannot be encoded with V0
+    // type bits.
+    ASSERT_THROWS_CODE(
+        mongo::KeyString::toBsonSafe(ks.getBuffer(), ks.getSize(), ALL_ASCENDING, tb),
+        AssertionException,
+        31231);
+}
+
 TEST_F(KeyStringBuilderTest, LotsOfNumbers1) {
     for (int i = 0; i < 64; i++) {
         int64_t x = 1LL << i;

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit 3151f38f0f4081f5c93dc23a90e6ccc6fdf7d0b9 [formerly dd829219438e556f4635eb5b6a31bc1158b10155])
Former-commit-id: f5bfa12aa3618cd0dd316223fba05c092607cf4c

src/mongo/db/storage/key_string_test.cpp
@@ -492,6 +492,23 @@ TEST_F(KeyStringTest, DoubleInvalidIntegerPartV0) {
         31209);
 }
 
+TEST_F(KeyStringTest, InvalidInfinityDecimalV0) {
+    // Encode a Decimal positive infinity in a V1 keystring.
+    mongo::KeyString ks(
+        mongo::KeyString::Version::V1, BSON("" << Decimal128::kPositiveInfinity), ALL_ASCENDING);
+
+    // Construct V0 type bits that indicate a NumberDecimal has been encoded.
+    mongo::KeyString::TypeBits tb(mongo::KeyString::Version::V0);
+    tb.appendNumberDecimal();
+
+    // The conversion to BSON will fail because Decimal positive infinity cannot be encoded with V0
+    // type bits.
+    ASSERT_THROWS_CODE(
+        mongo::KeyString::toBsonSafe(ks.getBuffer(), ks.getSize(), ALL_ASCENDING, tb),
+        AssertionException,
+        31231);
+}
+
 TEST_F(KeyStringTest, LotsOfNumbers1) {
     for (int i = 0; i < 64; i++) {
         int64_t x = 1LL << i;