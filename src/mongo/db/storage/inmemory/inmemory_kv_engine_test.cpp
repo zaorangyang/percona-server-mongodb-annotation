c60abae83394f9126b9aa0f0a18e46c97b42954d
Igor Solodovnikov <igor.solodovnikov@percona.com>
2019-08-14 13:58:29 +0300

    fix call to WiredTigerKVENgine constructor in inMemory code
    Former-commit-id: cb5dd088aa1027050d59bb4bade8d57bb56bd0a8

--- src/mongo/db/storage/inmemory/inmemory_kv_engine_test.cpp
+++ src/mongo/db/storage/inmemory/inmemory_kv_engine_test.cpp
@@ -59,7 +59,7 @@ public:
             "log=(enabled=false),"
             "file_manager=(close_idle_time=0),"
             "checkpoint=(wait=0,log_size=0)",
-            100, false, true, false, readOnly));
+            100, 0, false, true, false, readOnly));
         repl::ReplicationCoordinator::set(
             getGlobalServiceContext(),
             std::unique_ptr<repl::ReplicationCoordinator>(new repl::ReplicationCoordinatorMock(
