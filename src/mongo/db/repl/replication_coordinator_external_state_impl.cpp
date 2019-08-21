mongodb/mongo

SERVER-41788 put oplog application mode on applier options

src/mongo/db/repl/replication_coordinator_external_state_impl.cpp
@@ -226,14 +226,15 @@ void ReplicationCoordinatorExternalStateImpl::startSteadyStateReplication(
     // interface. During steady state replication, there is no need to log details on every batch
     // we apply (recovery); or track missing documents that are fetched from the sync source
     // (initial sync).
-    _oplogApplier = std::make_unique<OplogApplierImpl>(_oplogApplierTaskExecutor.get(),
-                                                       _oplogBuffer.get(),
-                                                       &noopOplogApplierObserver,
-                                                       replCoord,
-                                                       _replicationProcess->getConsistencyMarkers(),
-                                                       _storageInterface,
-                                                       OplogApplier::Options(),
-                                                       _writerPool.get());
+    _oplogApplier = std::make_unique<OplogApplierImpl>(
+        _oplogApplierTaskExecutor.get(),
+        _oplogBuffer.get(),
+        &noopOplogApplierObserver,
+        replCoord,
+        _replicationProcess->getConsistencyMarkers(),
+        _storageInterface,
+        OplogApplier::Options(OplogApplication::Mode::kSecondary),
+        _writerPool.get());
 
     invariant(!_bgSync);
     _bgSync =

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit 2d42c7f8d32ee0e04a0c94f4e6a1836dbed45bf6 [formerly 87862e515e8b4954d832f99ebe064fafb6cbbc2e])
Former-commit-id: 93bc8fc6fbfac0eb29e1c10e84a1c7cc6cbf8168

src/mongo/db/repl/replication_coordinator_external_state_impl.cpp
@@ -226,15 +226,15 @@ void ReplicationCoordinatorExternalStateImpl::startSteadyStateReplication(
     // interface. During steady state replication, there is no need to log details on every batch
     // we apply (recovery); or track missing documents that are fetched from the sync source
     // (initial sync).
-    _oplogApplier =
-        stdx::make_unique<OplogApplierImpl>(_oplogApplierTaskExecutor.get(),
-                                            _oplogBuffer.get(),
-                                            &noopOplogApplierObserver,
-                                            replCoord,
-                                            _replicationProcess->getConsistencyMarkers(),
-                                            _storageInterface,
-                                            OplogApplier::Options(),
-                                            _writerPool.get());
+    _oplogApplier = stdx::make_unique<OplogApplierImpl>(
+        _oplogApplierTaskExecutor.get(),
+        _oplogBuffer.get(),
+        &noopOplogApplierObserver,
+        replCoord,
+        _replicationProcess->getConsistencyMarkers(),
+        _storageInterface,
+        OplogApplier::Options(OplogApplication::Mode::kSecondary),
+        _writerPool.get());
 
     invariant(!_bgSync);
     _bgSync =