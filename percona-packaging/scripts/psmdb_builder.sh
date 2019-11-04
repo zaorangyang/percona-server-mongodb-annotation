f04afff0f25a1ab5a7a79e4e64d6fec3889f2b77
Oleksandr Miroshnychenko <alex.miroshnychenko@percona.com>
2019-11-04 15:26:14 +0200

    PSMDB-378 support build with AWS-SDK for v3.6
    Former-commit-id: 5e923f1f5d3142c60c7fc5c24eb6db40ce4f2f5c

--- percona-packaging/scripts/psmdb_builder.sh
+++ percona-packaging/scripts/psmdb_builder.sh
@@ -186,6 +186,15 @@ get_sources(){
     sed -i 's:/bin/sh:/bin/sh -x:' percona-server-mongodb/src/third_party/rocksdb/build_tools/build_detect_platform
 
     mv percona-server-mongodb ${PRODUCT}-${PSM_VER}-${PSM_RELEASE}
+    cd ${PRODUCT}-${PSM_VER}-${PSM_RELEASE}
+        git clone https://github.com/aws/aws-sdk-cpp.git
+            cd aws-sdk-cpp
+                git reset --hard
+                git clean -xdf
+                git checkout 1.7.91
+                mkdir build
+                sed -i 's:AWS_EVENT_STREAM_SHA:AWS_EVENT_STREAM_TAG:g' third-party/cmake/BuildAwsEventStream.cmake
+    cd ../../
     tar --owner=0 --group=0 --exclude=.* -czf ${PRODUCT}-${PSM_VER}-${PSM_RELEASE}.tar.gz ${PRODUCT}-${PSM_VER}-${PSM_RELEASE}
     echo "UPLOAD=UPLOAD/experimental/BUILDS/${PRODUCT}-3.6/${PRODUCT}-${PSM_VER}-${PSM_RELEASE}/${PSM_BRANCH}/${REVISION}/${BUILD_ID}" >> percona-server-mongodb-36.properties
     mkdir $WORKDIR/source_tarball
@@ -277,6 +286,34 @@ fix_rules(){
     sed -i 's:release:release --disable-warnings-as-errors :g' debian/rules
 }
 
+aws_sdk_build(){
+    cd $WORKDIR
+        git clone https://github.com/aws/aws-sdk-cpp.git
+        cd aws-sdk-cpp
+            git reset --hard
+            git clean -xdf
+            git checkout 1.7.91
+            mkdir build
+            sed -i 's:AWS_EVENT_STREAM_SHA:AWS_EVENT_STREAM_TAG:g' third-party/cmake/BuildAwsEventStream.cmake
+            cd build
+            CMAKE_CMD="cmake"
+            if [ -f /etc/redhat-release ]; then
+                RHEL=$(rpm --eval %rhel)
+                if [ x"$RHEL" = x6 ]; then
+                    CMAKE_CMD="cmake3"
+                fi
+            fi
+            set_compiler
+            if [ -z "${CC}" -a -z "${CXX}" ]; then
+                ${CMAKE_CMD} .. -DCMAKE_BUILD_TYPE=Release -DBUILD_ONLY="s3" -DBUILD_SHARED_LIBS=OFF -DMINIMIZE_SIZE=ON
+            else
+                ${CMAKE_CMD} CC=${CC} CXX=${CXX} .. -DCMAKE_BUILD_TYPE=Release -DBUILD_ONLY="s3" -DBUILD_SHARED_LIBS=OFF -DMINIMIZE_SIZE=ON
+            fi
+            make -j4
+            make install
+    cd ${WORKDIR}
+}
+
 install_deps() {
     if [ $INSTALL = 0 ]
     then
@@ -300,19 +337,35 @@ install_deps() {
       rm -fr /usr/local/gcc-5.4.0
       RHEL=$(rpm --eval %rhel)
       if [ x"$RHEL" = x6 ]; then
-        yum -y install rpmbuild rpm-build libpcap-devel gcc make cmake gcc-c++ openssl-devel cyrus-sasl-devel snappy-devel zlib-devel bzip2-devel libpcap-devel scons make rpm-build rpmbuild percona-devtoolset-gcc percona-devtoolset-binutils percona-devtoolset-gcc-c++ percona-devtoolset-libstdc++-devel percona-devtoolset-valgrind-devel python27 python27-devel libcurl-devel
+        yum -y install rpmbuild rpm-build libpcap-devel gcc make cmake gcc-c++ openssl-devel cyrus-sasl-devel snappy-devel zlib-devel bzip2-devel libpcap-devel scons make rpm-build rpmbuild percona-devtoolset-gcc percona-devtoolset-binutils percona-devtoolset-gcc-c++ percona-devtoolset-libstdc++-devel percona-devtoolset-valgrind-devel python27 python27-devel libcurl-devel e2fsprogs-devel expat-devel lz4-devel git cmake3
         wget https://bootstrap.pypa.io/get-pip.py
         python2.7 get-pip.py
         rm -rf /usr/bin/python2
         ln -s /usr/bin/python2.7 /usr/bin/python2
+        wget http://curl.haxx.se/download/curl-7.26.0.tar.gz
+        tar -xvzf curl-7.26.0.tar.gz
+        cd curl-7.26.0
+          ./configure
+          make
+          make install
+        cd ../
       else
-        yum -y install rpmbuild rpm-build libpcap-devel gcc make cmake gcc-c++ openssl-devel cyrus-sasl-devel snappy-devel zlib-devel bzip2-devel scons rpmlint rpm-build git python-pip python-devel libopcodes libcurl-devel
+        yum -y install rpmbuild rpm-build libpcap-devel gcc make cmake gcc-c++ openssl-devel cyrus-sasl-devel snappy-devel zlib-devel bzip2-devel scons rpmlint rpm-build git python-pip python-devel libopcodes libcurl-devel e2fsprogs-devel expat-devel lz4-devel
       fi
       install_golang
       install_gcc_54_centos
     else
       export DEBIAN=$(lsb_release -sc)
       export ARCH=$(echo $(uname -m) | sed -e 's:i686:i386:g')
+      if [ x"${DEBIAN}" = xjessie ]; then
+        cat >/etc/apt/sources.list.d/stretch.list <<EOL
+deb http://httpredir.debian.org/debian stretch main
+EOL
+        apt-get -y update
+        DEBIAN_FRONTEND=noninteractive apt-get -y install cmake
+        rm -f /etc/apt/sources.list.d/stretch.list
+        apt-get -y update
+      fi
       INSTALL_LIST="python python-dev valgrind scons liblz4-dev devscripts debhelper debconf libpcap-dev libbz2-dev libsnappy-dev pkg-config zlib1g-dev libzlcore-dev dh-systemd libsasl2-dev gcc g++ cmake curl libcurl4-openssl-dev libssl-dev"
       until apt-get -y install dirmngr; do
         sleep 1
@@ -327,12 +380,14 @@ install_deps() {
         sleep 1
         echo "waiting"
       done
+      apt-get -y install libext2fs-dev || apt-get -y install e2fslibs-dev
       install_golang
       install_gcc_54_deb
       wget https://bootstrap.pypa.io/get-pip.py
       python get-pip.py
       easy_install pip
     fi
+    aws_sdk_build
     return;
 }
 
@@ -471,6 +526,7 @@ build_rpm(){
     echo "CC and CXX should be modified once correct compiller would be installed on Centosed"
     export CC=/usr/local/gcc-5.4.0/bin/gcc-5.4
     export CXX=/usr/local/gcc-5.4.0/bin/g++-5.4
+    export LD_LIBRARY_PATH=/usr/local/gcc-5.4.0/lib:/usr/local/gcc-5.4.0/lib64:$LD_LIBRARY_PATH
     #
     echo "RHEL=${RHEL}" >> percona-server-mongodb-36.properties
     echo "ARCH=${ARCH}" >> percona-server-mongodb-36.properties
@@ -484,6 +540,7 @@ build_rpm(){
         export PATH="/usr/local/go/bin:$PATH:$GOPATH"
         export GOBINPATH="/usr/local/go/bin"
     #fi
+    export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
     rpmbuild --define "_topdir ${WORKDIR}/rpmbuild" --define "dist .$OS_NAME" --rebuild rpmbuild/SRPMS/$SRC_RPM
 
     return_code=$?
@@ -591,7 +648,7 @@ build_deb(){
     sed -i 's|go build|go build -a -x|' mongo-tools/build.sh
     sed -i 's|exit $ec||' mongo-tools/build.sh
     dch -m -D "${DEBIAN}" --force-distribution -v "${VERSION}-${RELEASE}.${DEBIAN}" 'Update distribution'
-
+    export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
     dpkg-buildpackage -rfakeroot -us -uc -b
     mkdir -p $CURDIR/deb
     mkdir -p $WORKDIR/deb
@@ -655,9 +712,10 @@ build_tarball(){
     if [ ${DEBUG} = 1 ]; then
     export CXXFLAGS="${CFLAGS} -Wno-error=deprecated-declarations"
     fi
-    export INSTALLDIR=${WORKDIR}/install
+    export INSTALLDIR=/usr/local
     export PORTABLE=1
     export USE_SSE=1
+    export LD_LIBRARY_PATH=/usr/local/gcc-5.4.0/lib:/usr/local/gcc-5.4.0/lib64:$LD_LIBRARY_PATH
     #
     # RocksDB
     pushd ${PSMDIR}/src/third_party/rocksdb
@@ -681,6 +739,39 @@ build_tarball(){
     # Finally build Percona Server for MongoDB with SCons
     cd ${PSMDIR_ABS}
     pip install --user -r buildscripts/requirements.txt
+    if [ -f /etc/redhat-release ]; then
+        RHEL=$(rpm --eval %rhel)
+        if [ $RHEL = 7 -o $RHEL = 8 ]; then
+            if [ -d aws-sdk-cpp ]; then
+                rm -rf aws-sdk-cpp
+            fi
+            export INSTALLDIR=$PWD/../install
+            export INSTALLDIR_AWS=$PWD/../install_aws
+            git clone https://github.com/aws/aws-sdk-cpp.git
+            cd aws-sdk-cpp
+            git reset --hard
+            git clean -xdf
+            git checkout 1.7.91
+            mkdir build
+            sed -i 's:AWS_EVENT_STREAM_SHA:AWS_EVENT_STREAM_TAG:g' third-party/cmake/BuildAwsEventStream.cmake
+            cd build
+            set_compiler
+            if [ -z "${CC}" -a -z "${CXX}" ]; then
+                cmake .. -DCMAKE_BUILD_TYPE=Release -DBUILD_ONLY="s3" -DBUILD_SHARED_LIBS=OFF -DMINIMIZE_SIZE=ON -DCMAKE_INSTALL_PREFIX="${INSTALLDIR_AWS}"
+            else
+                cmake CC=${CC} CXX=${CXX} .. -DCMAKE_BUILD_TYPE=Release -DBUILD_ONLY="s3" -DBUILD_SHARED_LIBS=OFF -DMINIMIZE_SIZE=ON -DCMAKE_INSTALL_PREFIX="${INSTALLDIR_AWS}"
+            fi
+            make -j4
+            make install
+            mkdir -p ${INSTALLDIR}/include/
+            mkdir -p ${INSTALLDIR}/lib/
+            mv ${INSTALLDIR_AWS}/include/* ${INSTALLDIR}/include/
+            mv ${INSTALLDIR_AWS}/lib*/* ${INSTALLDIR}/lib/
+            cd ../../
+
+        fi
+    fi
+    export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
     if [ ${DEBUG} = 0 ]; then
         buildscripts/scons.py CC=${CC} CXX=${CXX} --disable-warnings-as-errors --release --ssl --opt=on -j$NJOBS --use-sasl-client --wiredtiger --audit --rocksdb --inmemory --hotbackup CPPPATH=${INSTALLDIR}/include LIBPATH=${INSTALLDIR}/lib ${PSM_TARGETS}
     else
