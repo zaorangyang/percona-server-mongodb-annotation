mongodb/mongo

SERVER-43339 Make PBWM respect MaxTimeMS
This only affects the PBWM taken by the Global lock. The PBWM taken by
secondary batch application is still uninterruptible.

src/mongo/db/concurrency/d_concurrency.cpp
@@ -150,7 +150,7 @@ Lock::GlobalLock::GlobalLock(OperationContext* opCtx,
 
     try {
         if (_opCtx->lockState()->shouldConflictWithSecondaryBatchApplication()) {
-            _pbwm.lock(MODE_IS);
+            _pbwm.lock(opCtx, MODE_IS);
         }
         auto unlockPBWM = makeGuard([this] {
             if (_opCtx->lockState()->shouldConflictWithSecondaryBatchApplication()) {
@@ -314,8 +314,12 @@ Lock::ParallelBatchWriterMode::ParallelBatchWriterMode(Locker* lockState)
       _shouldNotConflictBlock(lockState) {}
 
 void Lock::ResourceLock::lock(LockMode mode) {
+    lock(nullptr, mode);
+}
+
+void Lock::ResourceLock::lock(OperationContext* opCtx, LockMode mode) {
     invariant(_result == LOCK_INVALID);
-    _locker->lock(_rid, mode);
+    _locker->lock(opCtx, _rid, mode);
     _result = LOCK_OK;
 }
 

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit 58f9153c94c8e75b26f79052fdcf15c335444d48 [formerly 711d26d8a5c5d64b26dd7dd8d66d489a4918e79f])
 create mode 100644 jstests/noPassthrough/pbwm_max_time_ms.js
Former-commit-id: ad0f72c1e5c452d7869fd829736179092b7ab88f

src/mongo/db/concurrency/d_concurrency.cpp
@@ -172,7 +172,7 @@ void Lock::GlobalLock::_enqueue(LockMode lockMode, Date_t deadline) {
 
     try {
         if (_opCtx->lockState()->shouldConflictWithSecondaryBatchApplication()) {
-            _pbwm.lock(MODE_IS);
+            _pbwm.lock(_opCtx, MODE_IS);
         }
         auto unlockPBWM = makeGuard([this] {
             if (_opCtx->lockState()->shouldConflictWithSecondaryBatchApplication()) {
@@ -308,8 +308,12 @@ Lock::ParallelBatchWriterMode::ParallelBatchWriterMode(Locker* lockState)
       _shouldNotConflictBlock(lockState) {}
 
 void Lock::ResourceLock::lock(LockMode mode) {
+    lock(nullptr, mode);
+}
+
+void Lock::ResourceLock::lock(OperationContext* opCtx, LockMode mode) {
     invariant(_result == LOCK_INVALID);
-    _locker->lock(_rid, mode);
+    _locker->lock(opCtx, _rid, mode);
     _result = LOCK_OK;
 }
 