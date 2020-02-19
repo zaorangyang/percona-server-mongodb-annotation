mongodb/mongo

SERVER-45007 PBWM lock acquisition in GlobalLock supports deadline

src/mongo/db/concurrency/d_concurrency.cpp
@@ -150,7 +150,7 @@ Lock::GlobalLock::GlobalLock(OperationContext* opCtx,
 
     try {
         if (_opCtx->lockState()->shouldConflictWithSecondaryBatchApplication()) {
-            _pbwm.lock(opCtx, MODE_IS);
+            _pbwm.lock(opCtx, MODE_IS, deadline);
         }
         auto unlockPBWM = makeGuard([this] {
             if (_opCtx->lockState()->shouldConflictWithSecondaryBatchApplication()) {

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit d731f5705e1c83d52f83a439d6444eb0169bd6c0 [formerly 73acb91e3989754ffa0ea98dc908362cad76c1ef])
Former-commit-id: 7d6f64a440076a8ff644188eb4d069a5ec7d490b

src/mongo/db/concurrency/d_concurrency.cpp
@@ -172,7 +172,7 @@ void Lock::GlobalLock::_enqueue(LockMode lockMode, Date_t deadline) {
 
     try {
         if (_opCtx->lockState()->shouldConflictWithSecondaryBatchApplication()) {
-            _pbwm.lock(_opCtx, MODE_IS);
+            _pbwm.lock(_opCtx, MODE_IS, deadline);
         }
         auto unlockPBWM = makeGuard([this] {
             if (_opCtx->lockState()->shouldConflictWithSecondaryBatchApplication()) {