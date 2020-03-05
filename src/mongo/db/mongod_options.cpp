6cabd893d76f6110d10e08441e5fc591c3a2d8f5
Igor Solodovnikov <igor.solodovnikov@percona.com>
2020-03-05 19:16:57 +0200

    PSMDB-165 add validation of queryTemplate value
    (cherry picked from commit 343021f21e4040cb90d0111ce7cf5e0c5401f4f9 [formerly 3b849f226d5805a76b1aa65fbfbe8cb3890b0111])
    (cherry picked from commit 0c4f3a24284a4518c3c3060d0f2521ff6722d51c [formerly 19bd4692445c0f84a3f1e79ed305d2d27c3789d5])
    
    
    Former-commit-id: 840d40a750bc250007ed2f80fc3f6097dcb7cf54

--- src/mongo/db/mongod_options.cpp
+++ src/mongo/db/mongod_options.cpp
@@ -1215,10 +1215,6 @@ Status storeMongodOptions(const moe::Environment& params) {
         encryptionGlobalParams.vaultDisableTLS = params["security.vault.disableTLSForTesting"].as<bool>();
     }
 
-    if (params.count("security.ldap.authz.queryTemplate")) {
-        ldapGlobalParams.ldapQueryTemplate = params["security.ldap.authz.queryTemplate"].as<std::string>();
-    }
-
     if (params.count("cpu")) {
         serverGlobalParams.cpu = params["cpu"].as<bool>();
     }
