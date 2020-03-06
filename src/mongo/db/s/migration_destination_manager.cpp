mongodb/mongo

SERVER-45770 add to/from information to moveChunk.to log message

src/mongo/db/s/migration_destination_manager.cpp
@@ -830,7 +830,7 @@ void MigrationDestinationManager::_migrateDriver(OperationContext* outerOpCtx) {
           "sessionId"_attr = *_sessionId);
 
     MoveTimingHelper timing(
-        outerOpCtx, "to", _nss.ns(), _min, _max, 6 /* steps */, &_errmsg, ShardId(), ShardId());
+        outerOpCtx, "to", _nss.ns(), _min, _max, 6 /* steps */, &_errmsg, _toShard, _fromShard);
 
     const auto initialState = getState();
 

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit 0f067b00106f026a2ac2afec5c1931b18581b225 [formerly 742ac9b37b1d8f489e1b259a0a3575f8811edac4])
Conflicts:
	src/mongo/db/s/migration_destination_manager.cpp
Former-commit-id: 25d38642ed3beb47fd731e405f197ca6de820d6e

src/mongo/db/s/migration_destination_manager.cpp
@@ -747,7 +747,7 @@ void MigrationDestinationManager::_migrateDriver(OperationContext* opCtx) {
           << " at epoch " << _epoch.toString() << " with session id " << *_sessionId;
 
     MoveTimingHelper timing(
-        opCtx, "to", _nss.ns(), _min, _max, 6 /* steps */, &_errmsg, ShardId(), ShardId());
+        opCtx, "to", _nss.ns(), _min, _max, 6 /* steps */, &_errmsg, _toShard, _fromShard);
 
     const auto initialState = getState();
 