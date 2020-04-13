mongodb/mongo

SERVER-32871 Invalidate CatalogCache entries when ShardRegistry reload discovers a shard has been removed

src/mongo/db/s/sharding_initialization_mongod.cpp
@@ -426,16 +426,34 @@ void initializeGlobalShardingStateForMongoD(OperationContext* opCtx,
     globalConnPool.addHook(new ShardingConnectionHook(false, makeEgressHooksList(service)));
     shardConnectionPool.addHook(new ShardingConnectionHook(true, makeEgressHooksList(service)));
 
-    uassertStatusOK(initializeGlobalShardingState(
-        opCtx,
-        configCS,
-        distLockProcessId,
-        std::move(shardFactory),
-        std::make_unique<CatalogCache>(CatalogCacheLoader::get(opCtx)),
-        [service] { return makeEgressHooksList(service); },
-        // We only need one task executor here because sharding task executors aren't used for user
-        // queries in mongod.
-        1));
+    auto catalogCache = std::make_unique<CatalogCache>(CatalogCacheLoader::get(opCtx));
+
+    // List of hooks which will be called by the ShardRegistry when it discovers a shard has been
+    // removed.
+    std::vector<ShardRegistry::ShardRemovalHook> shardRemovalHooks = {
+        // Invalidate appropriate entries in the CatalogCache when a shard is removed. It's safe to
+        // capture the CatalogCache pointer since the Grid (and therefore CatalogCache and
+        // ShardRegistry) are never destroyed.
+        [catCache = catalogCache.get()](const ShardId& removedShard) {
+            catCache->invalidateEntriesThatReferenceShard(removedShard);
+        }};
+
+    uassert(ErrorCodes::BadValue,
+            "Unrecognized connection string.",
+            configCS.type() != ConnectionString::INVALID);
+
+    auto shardRegistry = std::make_unique<ShardRegistry>(
+        std::move(shardFactory), configCS, std::move(shardRemovalHooks));
+
+    uassertStatusOK(
+        initializeGlobalShardingState(opCtx,
+                                      distLockProcessId,
+                                      std::move(catalogCache),
+                                      std::move(shardRegistry),
+                                      [service] { return makeEgressHooksList(service); },
+                                      // We only need one task executor here because sharding task
+                                      // executors aren't used for user queries in mongod.
+                                      1));
 
     auto const replCoord = repl::ReplicationCoordinator::get(service);
     if (serverGlobalParams.clusterRole == ClusterRole::ConfigServer &&

src/mongo/s/server.cpp
@@ -371,12 +371,30 @@ Status initializeSharding(OperationContext* opCtx) {
     CatalogCacheLoader::set(opCtx->getServiceContext(),
                             std::make_unique<ConfigServerCatalogCacheLoader>());
 
+    auto catalogCache = std::make_unique<CatalogCache>(CatalogCacheLoader::get(opCtx));
+
+    // List of hooks which will be called by the ShardRegistry when it discovers a shard has been
+    // removed.
+    std::vector<ShardRegistry::ShardRemovalHook> shardRemovalHooks = {
+        // Invalidate appropriate entries in the catalog cache when a shard is removed. It's safe to
+        // capture the catalog cache pointer since the Grid (and therefore CatalogCache and
+        // ShardRegistry) are never destroyed.
+        [catCache = catalogCache.get()](const ShardId& removedShard) {
+            catCache->invalidateEntriesThatReferenceShard(removedShard);
+        }};
+
+    if (mongosGlobalParams.configdbs.type() == ConnectionString::INVALID) {
+        return {ErrorCodes::BadValue, "Unrecognized connection string."};
+    }
+
+    auto shardRegistry = std::make_unique<ShardRegistry>(
+        std::move(shardFactory), mongosGlobalParams.configdbs, std::move(shardRemovalHooks));
+
     Status status = initializeGlobalShardingState(
         opCtx,
-        mongosGlobalParams.configdbs,
         generateDistLockProcessId(opCtx),
-        std::move(shardFactory),
-        std::make_unique<CatalogCache>(CatalogCacheLoader::get(opCtx)),
+        std::move(catalogCache),
+        std::move(shardRegistry),
         [opCtx]() {
             auto hookList = std::make_unique<rpc::EgressMetadataHookList>();
             hookList->addHook(

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit 5d25aebf93cee5e7aff650b852ea23f10f72fbab [formerly 6ea81c883e7297be99884185c908c7ece385caf8])
Former-commit-id: fafc69977611a6ee306f3e8dd2b8bad065556d29

src/mongo/db/s/sharding_initialization_mongod.cpp
@@ -421,16 +421,34 @@ void initializeGlobalShardingStateForMongoD(OperationContext* opCtx,
     globalConnPool.addHook(new ShardingConnectionHook(false, makeEgressHooksList(service)));
     shardConnectionPool.addHook(new ShardingConnectionHook(true, makeEgressHooksList(service)));
 
-    uassertStatusOK(initializeGlobalShardingState(
-        opCtx,
-        configCS,
-        distLockProcessId,
-        std::move(shardFactory),
-        stdx::make_unique<CatalogCache>(CatalogCacheLoader::get(opCtx)),
-        [service] { return makeEgressHooksList(service); },
-        // We only need one task executor here because sharding task executors aren't used for user
-        // queries in mongod.
-        1));
+    auto catalogCache = stdx::make_unique<CatalogCache>(CatalogCacheLoader::get(opCtx));
+
+    // List of hooks which will be called by the ShardRegistry when it discovers a shard has been
+    // removed.
+    std::vector<ShardRegistry::ShardRemovalHook> shardRemovalHooks = {
+        // Invalidate appropriate entries in the CatalogCache when a shard is removed. It's safe to
+        // capture the CatalogCache pointer since the Grid (and therefore CatalogCache and
+        // ShardRegistry) are never destroyed.
+        [catCache = catalogCache.get()](const ShardId& removedShard) {
+            catCache->invalidateEntriesThatReferenceShard(removedShard);
+        }};
+
+    uassert(ErrorCodes::BadValue,
+            "Unrecognized connection string.",
+            configCS.type() != ConnectionString::INVALID);
+
+    auto shardRegistry = stdx::make_unique<ShardRegistry>(
+        std::move(shardFactory), configCS, std::move(shardRemovalHooks));
+
+    uassertStatusOK(
+        initializeGlobalShardingState(opCtx,
+                                      distLockProcessId,
+                                      std::move(catalogCache),
+                                      std::move(shardRegistry),
+                                      [service] { return makeEgressHooksList(service); },
+                                      // We only need one task executor here because sharding task
+                                      // executors aren't used for user queries in mongod.
+                                      1));
 
     auto const replCoord = repl::ReplicationCoordinator::get(service);
     if (serverGlobalParams.clusterRole == ClusterRole::ConfigServer &&

src/mongo/s/server.cpp
@@ -376,12 +376,30 @@ Status initializeSharding(OperationContext* opCtx) {
     CatalogCacheLoader::set(opCtx->getServiceContext(),
                             stdx::make_unique<ConfigServerCatalogCacheLoader>());
 
+    auto catalogCache = stdx::make_unique<CatalogCache>(CatalogCacheLoader::get(opCtx));
+
+    // List of hooks which will be called by the ShardRegistry when it discovers a shard has been
+    // removed.
+    std::vector<ShardRegistry::ShardRemovalHook> shardRemovalHooks = {
+        // Invalidate appropriate entries in the catalog cache when a shard is removed. It's safe to
+        // capture the catalog cache pointer since the Grid (and therefore CatalogCache and
+        // ShardRegistry) are never destroyed.
+        [catCache = catalogCache.get()](const ShardId& removedShard) {
+            catCache->invalidateEntriesThatReferenceShard(removedShard);
+        }};
+
+    if (mongosGlobalParams.configdbs.type() == ConnectionString::INVALID) {
+        return {ErrorCodes::BadValue, "Unrecognized connection string."};
+    }
+
+    auto shardRegistry = stdx::make_unique<ShardRegistry>(
+        std::move(shardFactory), mongosGlobalParams.configdbs, std::move(shardRemovalHooks));
+
     Status status = initializeGlobalShardingState(
         opCtx,
-        mongosGlobalParams.configdbs,
         generateDistLockProcessId(opCtx),
-        std::move(shardFactory),
-        stdx::make_unique<CatalogCache>(CatalogCacheLoader::get(opCtx)),
+        std::move(catalogCache),
+        std::move(shardRegistry),
         [opCtx]() {
             auto hookList = stdx::make_unique<rpc::EgressMetadataHookList>();
             hookList->addHook(