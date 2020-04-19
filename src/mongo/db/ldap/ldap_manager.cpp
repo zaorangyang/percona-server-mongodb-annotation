37f0c94c1b1a2d3c7475e58dd25dc6c035dc4b88
Igor Solodovnikov <igor.solodovnikov@percona.com>
2020-04-19 18:22:16 +0300

    PSMDB-165 initial LDAP manager implementation
    (cherry picked from commit 43e5718607830ff40a7bcdf3bedb991801a1cc06 [formerly 6b80da4a45bd949eed082ecd16c1325e839a4895])
    
    
    Former-commit-id: 84879d2d2ba092e3f6fa8fd4840f6cfbae73f2ee

---
+++ src/mongo/db/ldap/ldap_manager.cpp
@@ -0,0 +1,84 @@
+/*======
+This file is part of Percona Server for MongoDB.
+
+Copyright (C) 2019-present Percona and/or its affiliates. All rights reserved.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the Server Side Public License, version 1,
+    as published by MongoDB, Inc.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    Server Side Public License for more details.
+
+    You should have received a copy of the Server Side Public License
+    along with this program. If not, see
+    <http://www.mongodb.com/licensing/server-side-public-license>.
+
+    As a special exception, the copyright holders give permission to link the
+    code of portions of this program with the OpenSSL library under certain
+    conditions as described in each individual source file and distribute
+    linked combinations including the program with the OpenSSL library. You
+    must comply with the Server Side Public License in all respects for
+    all of the code used other than as permitted herein. If you modify file(s)
+    with this exception, you may extend this exception to your version of the
+    file(s), but you are not obligated to do so. If you do not wish to do so,
+    delete this exception statement from your version. If you delete this
+    exception statement from all source files in the program, then also delete
+    it in the license file.
+======= */
+
+#define MONGO_LOG_DEFAULT_COMPONENT ::mongo::logger::LogComponent::kAccessControl
+
+#include "mongo/db/ldap/ldap_manager.h"
+
+#include "mongo/base/init.h"
+#include "mongo/db/ldap/ldap_manager_impl.h"
+#include "mongo/db/ldap_options.h"
+#include "mongo/util/assert_util.h"
+#include "mongo/util/log.h"
+
+namespace mongo {
+
+std::unique_ptr<LDAPManager> LDAPManager::create() {
+    return std::make_unique<LDAPManagerImpl>();
+}
+
+namespace {
+
+const auto getLDAPManager =
+    ServiceContext::declareDecoration<std::unique_ptr<LDAPManager>>();
+
+}  // namespace
+
+LDAPManager* LDAPManager::get(ServiceContext* service) {
+    return getLDAPManager(service).get();
+}
+
+LDAPManager* LDAPManager::get(ServiceContext& service) {
+    return getLDAPManager(service).get();
+}
+
+void LDAPManager::set(ServiceContext* service,
+                      std::unique_ptr<LDAPManager> authzManager) {
+    getLDAPManager(service) = std::move(authzManager);
+}
+
+MONGO_INITIALIZER_WITH_PREREQUISITES(CreateLDAPManager,
+                                     ("EndStartupOptionStorage"))
+(InitializerContext* context) {
+    if (!ldapGlobalParams.ldapServers->empty()) {
+        auto ldapManager = LDAPManager::create();
+        Status res = ldapManager->initialize();
+        if (!res.isOK()) {
+            error() << "Cannot initialize LDAP server connection";
+            return res;
+        }
+        LDAPManager::set(getGlobalServiceContext(), std::move(ldapManager));
+    }
+    return Status::OK();
+}
+
+}  // namespace mongo
+
