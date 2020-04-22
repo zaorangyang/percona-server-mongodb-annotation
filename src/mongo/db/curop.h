mongodb/mongo

SERVER-40805 Indicate reason for replanning

src/mongo/db/curop.h
@@ -202,7 +202,7 @@ public:
     bool fromMultiPlanner{false};
 
     // True if a replan was triggered during the execution of this operation.
-    bool replanned{false};
+    std::optional<std::string> replanReason;
 
     bool upsert{false};  // true if the update actually did an insert
     bool cursorExhausted{

zaorangyang/percona-server-mongodb-annotation

Former-commit-id: e5341d8f4f0b3f1a397ed3081ef01e80d395cbfb

src/mongo/db/curop.h
@@ -173,7 +173,7 @@ public:
     bool fromMultiPlanner{false};
 
     // True if a replan was triggered during the execution of this operation.
-    bool replanned{false};
+    boost::optional<std::string> replanReason;
 
     bool fastmodinsert{false};  // upsert of an $operation. builds a default object
     bool upsert{false};         // true if the update actually did an insert