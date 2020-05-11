mongodb/mongo

SERVER-45367 Log open cursors for a namespace when submitting tasks to the range deleter

src/mongo/db/s/cleanup_orphaned_cmd.cpp
@@ -112,7 +112,8 @@ CleanupResult cleanupOrphanedData(OperationContext* opCtx,
 
         *stoppedAtKey = targetRange->getMax();
 
-        notifn = css->cleanUpRange(*targetRange, CollectionShardingRuntime::kNow);
+        notifn = css->cleanUpRange(
+            opCtx, autoColl.getCollection(), *targetRange, CollectionShardingRuntime::kNow);
     }
 
     // Sleep waiting for our own deletion. We don't actually care about any others, so there is no

src/mongo/db/s/metadata_manager_test.cpp
@@ -66,6 +66,13 @@ protected:
     void setUp() override {
         ShardServerTestFixture::setUp();
         _manager = std::make_shared<MetadataManager>(getServiceContext(), kNss, executor());
+        _autoColl.emplace(operationContext(), kNss, MODE_IS);
+    }
+
+    void tearDown() override {
+        _autoColl.reset();
+        _manager.reset();
+        ShardServerTestFixture::tearDown();
     }
 
     /**
@@ -154,7 +161,12 @@ protected:
         return CollectionMetadata(std::make_shared<ChunkManager>(rt, boost::none), kThisShard);
     }
 
+    Collection* collection() {
+        return _autoColl->getCollection();
+    }
+
     std::shared_ptr<MetadataManager> _manager;
+    boost::optional<AutoGetCollection> _autoColl;
 };
 
 TEST_F(MetadataManagerTest, InitialMetadataIsUnknown) {
@@ -208,7 +220,7 @@ TEST_F(MetadataManagerTest, AddRangeNotificationsBlockAndYield) {
 
     ChunkRange cr1(BSON("key" << 0), BSON("key" << 10));
 
-    auto notifn1 = _manager->cleanUpRange(cr1, Date_t{});
+    auto notifn1 = _manager->cleanUpRange(operationContext(), collection(), cr1, Date_t{});
     ASSERT_FALSE(notifn1.ready());
     ASSERT_EQ(_manager->numberOfRangesToClean(), 1UL);
 
@@ -253,7 +265,7 @@ TEST_F(MetadataManagerTest, NotificationBlocksUntilDeletion) {
         ASSERT_EQ(_manager->numberOfMetadataSnapshots(), 0UL);
         ASSERT_EQ(_manager->numberOfRangesToClean(), 0UL);
 
-        optNotif = _manager->cleanUpRange(cr1, Date_t{});
+        optNotif = _manager->cleanUpRange(operationContext(), collection(), cr1, Date_t{});
         ASSERT(optNotif);
         ASSERT_EQ(_manager->numberOfMetadataSnapshots(), 0UL);
         ASSERT_EQ(_manager->numberOfRangesToClean(), 1UL);
@@ -291,7 +303,7 @@ TEST_F(MetadataManagerTest, CleanupNotificationsAreSignaledOnDropAndRecreate) {
     _manager->setFilteringMetadata(
         cloneMetadataMinusChunk(*_manager->getActiveMetadata(_manager, boost::none), rangeToClean));
 
-    auto notif = _manager->cleanUpRange(rangeToClean, Date_t{});
+    auto notif = _manager->cleanUpRange(operationContext(), collection(), rangeToClean, Date_t{});
     ASSERT(!notif.ready());
 
     auto optNotif = _manager->trackOrphanedDataCleanup(rangeToClean);
@@ -386,7 +398,7 @@ TEST_F(MetadataManagerTest, RangesToCleanMembership) {
 
     ASSERT_EQ(0UL, _manager->numberOfRangesToClean());
 
-    auto notifn = _manager->cleanUpRange(cr, Date_t{});
+    auto notifn = _manager->cleanUpRange(operationContext(), collection(), cr, Date_t{});
     ASSERT(!notifn.ready());
     ASSERT_EQ(1UL, _manager->numberOfRangesToClean());
 

src/mongo/db/s/migration_source_manager.cpp
@@ -563,7 +563,8 @@ Status MigrationSourceManager::commitChunkMetadataOnConfig(OperationContext* opC
                                                           : CollectionShardingRuntime::kDelayed;
         UninterruptibleLockGuard noInterrupt(opCtx->lockState());
         AutoGetCollection autoColl(opCtx, getNss(), MODE_IS);
-        return CollectionShardingRuntime::get(opCtx, getNss())->cleanUpRange(range, whenToClean);
+        return CollectionShardingRuntime::get(opCtx, getNss())
+            ->cleanUpRange(opCtx, autoColl.getCollection(), range, whenToClean);
     }();
 
     if (!MONGO_FAIL_POINT(doNotRefreshRecipientAfterCommit)) {

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit 99ee0daafbf6f658a6d33bfbabf364c457cc44f3 [formerly 2c7a53f74cd6f588cca2ab968a9263d59c56684c])
Former-commit-id: a434867901150513736daa876b362ab9f7a22195

src/mongo/db/s/cleanup_orphaned_cmd.cpp
@@ -111,7 +111,8 @@ CleanupResult cleanupOrphanedData(OperationContext* opCtx,
 
         *stoppedAtKey = targetRange->getMax();
 
-        notifn = css->cleanUpRange(*targetRange, CollectionShardingState::kNow);
+        notifn = css->cleanUpRange(
+            opCtx, autoColl.getCollection(), *targetRange, CollectionShardingState::kNow);
     }
 
     // Sleep waiting for our own deletion. We don't actually care about any others, so there is no

src/mongo/db/s/metadata_manager_test.cpp
@@ -80,6 +80,13 @@ protected:
         configTargeter()->setFindHostReturnValue(dummyHost);
 
         _manager = std::make_shared<MetadataManager>(getServiceContext(), kNss, executor());
+        _autoColl.emplace(operationContext(), kNss, MODE_IS);
+    }
+
+    void tearDown() override {
+        _autoColl.reset();
+        _manager.reset();
+        ShardingMongodTestFixture::tearDown();
     }
 
     std::shared_ptr<RemoteCommandTargeterMock> configTargeter() const {
@@ -146,7 +153,12 @@ protected:
         return cm2Ptr;
     }
 
+    Collection* collection() {
+        return _autoColl->getCollection();
+    }
+
     std::shared_ptr<MetadataManager> _manager;
+    boost::optional<AutoGetCollection> _autoColl;
 };
 
 // In the following tests, the ranges-to-clean is not drained by the background deleter thread
@@ -172,7 +184,7 @@ TEST_F(MetadataManagerTest, AddRangeNotificationsBlockAndYield) {
     _manager->refreshActiveMetadata(makeEmptyMetadata());
 
     ChunkRange cr1(BSON("key" << 0), BSON("key" << 10));
-    auto notifn1 = _manager->cleanUpRange(cr1, Date_t{});
+    auto notifn1 = _manager->cleanUpRange(operationContext(), collection(), cr1, Date_t{});
     ASSERT_FALSE(notifn1.ready());
     ASSERT_EQ(_manager->numberOfRangesToClean(), 1UL);
     auto optNotifn = _manager->trackOrphanedDataCleanup(cr1);
@@ -218,7 +230,7 @@ TEST_F(MetadataManagerTest, NotificationBlocksUntilDeletion) {
         ASSERT_EQ(_manager->numberOfMetadataSnapshots(), 3UL);
         ASSERT_EQ(_manager->numberOfRangesToClean(), 0UL);  // not yet...
 
-        optNotif = _manager->cleanUpRange(cr1, Date_t{});
+        optNotif = _manager->cleanUpRange(operationContext(), collection(), cr1, Date_t{});
         ASSERT_EQ(_manager->numberOfMetadataSnapshots(), 3UL);
         ASSERT_EQ(_manager->numberOfRangesToClean(), 1UL);
     }  // scm1,2,3 destroyed, refcount of each metadata goes to zero
@@ -316,7 +328,7 @@ TEST_F(MetadataManagerTest, RangesToCleanMembership) {
     ASSERT(_manager->numberOfRangesToClean() == 0UL);
 
     ChunkRange cr1 = ChunkRange(BSON("key" << 0), BSON("key" << 10));
-    auto notifn = _manager->cleanUpRange(cr1, Date_t{});
+    auto notifn = _manager->cleanUpRange(operationContext(), collection(), cr1, Date_t{});
     ASSERT(!notifn.ready());
     ASSERT(_manager->numberOfRangesToClean() == 1UL);
     notifn.abandon();

src/mongo/db/s/migration_source_manager.cpp
@@ -637,7 +637,8 @@ Status MigrationSourceManager::commitChunkMetadataOnConfig(OperationContext* opC
         auto const whenToClean = _args.getWaitForDelete() ? CollectionShardingState::kNow
                                                           : CollectionShardingState::kDelayed;
         AutoGetCollection autoColl(opCtx, getNss(), MODE_IS);
-        return CollectionShardingState::get(opCtx, getNss())->cleanUpRange(range, whenToClean);
+        return CollectionShardingState::get(opCtx, getNss())
+            ->cleanUpRange(opCtx, autoColl.getCollection(), range, whenToClean);
     }();
 
     if (!MONGO_FAIL_POINT(doNotRefreshRecipientAfterCommit)) {