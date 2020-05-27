mongodb/mongo

SERVER-47773 Error consistently when tailable cursors and $near are used together

src/mongo/db/query/canonical_query.cpp
@@ -401,6 +401,12 @@ StatusWith<QueryMetadataBitSet> CanonicalQuery::isValid(MatchExpression* root,
         return Status(ErrorCodes::BadValue, "text and tailable cursor not allowed in same query");
     }
 
+    // NEAR and tailable are incompatible.
+    if (numGeoNear > 0 && request.isTailable()) {
+        return Status(ErrorCodes::BadValue,
+                      "Tailable cursors and geo $near cannot be used together");
+    }
+
     // $natural sort order must agree with hint.
     if (sortNaturalElt) {
         if (!hintObj.isEmpty() && !hintNaturalElt) {

zaorangyang/percona-server-mongodb-annotation

(cherry picked from commit c8ced6df8f620daaa2e539f192f2eef356c63e9c)
(cherry picked from commit 1176bc3ccae5a63796c3771fdc4439b31b73c4b2 [formerly e9c31e791b2e056c016c048d7e21c59e0e2452ea])
Former-commit-id: 444dab325b5351ddd566da1d5365ec8728a06634

src/mongo/db/query/canonical_query.cpp
@@ -409,6 +409,12 @@ Status CanonicalQuery::isValid(MatchExpression* root, const QueryRequest& parsed
         return Status(ErrorCodes::BadValue, "text and tailable cursor not allowed in same query");
     }
 
+    // NEAR and tailable are incompatible.
+    if (numGeoNear > 0 && parsed.isTailable()) {
+        return Status(ErrorCodes::BadValue,
+                      "Tailable cursors and geo $near cannot be used together");
+    }
+
     // $natural sort order must agree with hint.
     if (sortNaturalElt) {
         if (!hintObj.isEmpty() && !hintNaturalElt) {