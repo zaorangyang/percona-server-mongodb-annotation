mongodb/mongo

SERVER-40805 Indicate reason for replanning

src/mongo/db/exec/plan_stats.h
@@ -201,7 +201,7 @@ struct AndSortedStats : public SpecificStats {
 };
 
 struct CachedPlanStats : public SpecificStats {
-    CachedPlanStats() : replanned(false) {}
+    CachedPlanStats() = default;
 
     SpecificStats* clone() const final {
         return new CachedPlanStats(*this);
@@ -211,7 +211,7 @@ struct CachedPlanStats : public SpecificStats {
         return sizeof(*this);
     }
 
-    bool replanned;
+    std::optional<std::string> replanReason;
 };
 
 struct CollectionScanStats : public SpecificStats {

src/mongo/db/query/plan_summary_stats.h
@@ -72,7 +72,7 @@ struct PlanSummaryStats {
     bool fromMultiPlanner = false;
 
     // Was a replan triggered during the execution of this query?
-    bool replanned = false;
+    std::optional<std::string> replanReason;
 };
 
 }  // namespace mongo

zaorangyang/percona-server-mongodb-annotation

Former-commit-id: e3acf12767b9192e24a3b2bbd810e6b11a965ed6

src/mongo/db/exec/plan_stats.h
@@ -214,7 +214,7 @@ struct AndSortedStats : public SpecificStats {
 };
 
 struct CachedPlanStats : public SpecificStats {
-    CachedPlanStats() : replanned(false) {}
+    CachedPlanStats() = default;
 
     SpecificStats* clone() const final {
         return new CachedPlanStats(*this);
@@ -224,7 +224,7 @@ struct CachedPlanStats : public SpecificStats {
         return sizeof(*this);
     }
 
-    bool replanned;
+    boost::optional<std::string> replanReason;
 };
 
 struct CollectionScanStats : public SpecificStats {

src/mongo/db/query/plan_summary_stats.h
@@ -62,7 +62,7 @@ struct PlanSummaryStats {
     bool fromMultiPlanner = false;
 
     // Was a replan triggered during the execution of this query?
-    bool replanned = false;
+    boost::optional<std::string> replanReason;
 };
 
 }  // namespace mongo